afb478fade94502696e42c8e4c56d68321032cb8
Matt Andrews <matt@mattandrews.info>
2017-03-27 16:55:52 +0100

    add lookup

--- data.js
+++ data.js
@@ -1,9 +1,27 @@
 'use strict';
-
 const grants = require('./app/grantnav.json');
 const _ = require('lodash');
+const rp = require('request-promise');
 
-let regions = grants.grants.map(g => g.recipientRegionName).filter(g => g !== undefined);
-let counts = _.countBy(regions);
-// let list = _.sortBy(counts);
-console.log(counts);
+let regionsByName = grants.grants.map(g => g.recipientRegionName).filter(g => g !== undefined);
+let regionsCounts = _.countBy(regionsByName);
+let districtsByName = grants.grants.map(g => g.recipientDistrictName).filter(g => g !== undefined)
+let districtCounts = _.countBy(districtsByName);
+
+let postcode = 'DA7 6NJ';
+rp('http://api.postcodes.io/postcodes/' + encodeURIComponent(postcode)).then(data => {
+    let json = JSON.parse(data);
+    let yourDistrict = json.result.admin_district;
+    console.log('your district is ' + yourDistrict);
+    let match = grants.grants.filter(d => {
+        if (typeof d.recipientDistrictName !== 'undefined') {
+            return d.recipientDistrictName.indexOf(yourDistrict) !== -1;
+        } else {
+            return false;
+        }
+    });
+    console.log(match);
+});
+
+// console.log(regions);
+// console.log(districts);
