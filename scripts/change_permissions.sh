3e725528be79af2981b5cb93b249fcc10ad77d49
Matt Andrews <matt@mattandrews.info>
2017-03-27 14:47:12 +0100

    add nginx conf

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 chmod -R 755 /var/www/biglotteryfund
-rm /etc/nginx/sites-enabled/default
+
+rm /etc/nginx/nginx.conf
 cp /var/www/biglotteryfund/server.conf /etc/nginx/sites-enabled
+cp /var/www/biglotteryfund/nginx.conf /etc/nginx/
