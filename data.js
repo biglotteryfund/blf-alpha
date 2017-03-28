1993b02657a7d3ff0fc630a6d1db94066ae1e31f
Matt Andrews <matt@mattandrews.info>
2017-03-28 14:52:01 +0100

    Parse data for region totals and add map

--- data.js
+++ data.js
@@ -8,20 +8,35 @@ let regionsCounts = _.countBy(regionsByName);
 let districtsByName = grants.grants.map(g => g.recipientDistrictName).filter(g => g !== undefined)
 let districtCounts = _.countBy(districtsByName);
 
-let postcode = 'DA7 6NJ';
-rp('http://api.postcodes.io/postcodes/' + encodeURIComponent(postcode)).then(data => {
-    let json = JSON.parse(data);
-    let yourDistrict = json.result.admin_district;
-    console.log('your district is ' + yourDistrict);
-    let match = grants.grants.filter(d => {
-        if (typeof d.recipientDistrictName !== 'undefined') {
-            return d.recipientDistrictName.indexOf(yourDistrict) !== -1;
-        } else {
-            return false;
-        }
-    });
-    console.log(match);
-});
+let grantsWithDistricts = grants.grants.filter(g => typeof g !== 'undefined');
+let numGrants = _.countBy(grantsWithDistricts.map(g => g.recipientRegionName));
+let grantsByRegion = _.groupBy(grantsWithDistricts, 'recipientRegionName');
+let results = {};
+
+for (let region in grantsByRegion) {
+    let total = _.sumBy(grantsByRegion[region], 'amountAwarded');
+    results[region] = {
+        totalAwarded: total,
+        numGrants: numGrants[region]
+    };
+}
+
+console.log(results);
+
+// let postcode = 'DA7 6NJ';
+// rp('http://api.postcodes.io/postcodes/' + encodeURIComponent(postcode)).then(data => {
+//     let json = JSON.parse(data);
+//     let yourDistrict = json.result.admin_district;
+//     console.log('your district is ' + yourDistrict);
+//     let match = grants.grants.filter(d => {
+//         if (typeof d.recipientDistrictName !== 'undefined') {
+//             return d.recipientDistrictName.indexOf(yourDistrict) !== -1;
+//         } else {
+//             return false;
+//         }
+//     });
+//     console.log(match);
+// });
 
 // console.log(regions);
 // console.log(districts);
