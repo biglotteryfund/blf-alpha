670511bcd20884d6a5ec24768b0c99535d01a418
Matt Andrews <matt@mattandrews.info>
2017-03-29 09:10:18 +0100

    fix sudo

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum install -y nginx
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+apt-get install -y nodejs
