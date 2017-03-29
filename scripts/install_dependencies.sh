03c878354fba67c0df95c03bd5ffa3753c71bbc5
Matt Andrews <matt@mattandrews.info>
2017-03-29 09:20:16 +0100

    add pm2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,3 +3,6 @@ yum install -y nginx
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum install -y nodejs
 
+npm install -g pm2
+pm2 update
+
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo /etc/init.d/httpd stop
 service nginx restart
+
+cd /var/www/biglotteryfund
+npm install
+pm2 start bin/www -n www -i 0
