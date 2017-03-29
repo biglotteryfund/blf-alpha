121505c4a1a613d2199e8da855c5015636262f02
Matt Andrews <matt@mattandrews.info>
2017-03-29 10:39:25 +0100

    update build script

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 chmod -R 755 /var/www/biglotteryfund
 rm /etc/nginx/nginx.conf
+rm /etc/nginx/sites-enabled
+mkdir /etc/nginx/sites-enabled
 cp /var/www/biglotteryfund/server.conf /etc/nginx/sites-enabled
 cp /var/www/biglotteryfund/nginx.conf /etc/nginx/
