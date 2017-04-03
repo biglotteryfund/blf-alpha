51fa7a4440d348636344cf4979e73ececd1e261a
Matt Andrews <matt@mattandrews.info>
2017-04-03 15:17:26 +0100

    Add gulp to build step

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,10 @@
 cd /var/www/biglotteryfund
 npm install
 
+# front-end build
+npm install -g gulp
+gulp build --production
+
 # configure nginx (would be nice to conditionally restart here)
 rm /etc/nginx/sites-enabled/default
 cp /var/www/biglotteryfund/config/server.conf /etc/nginx/sites-enabled
