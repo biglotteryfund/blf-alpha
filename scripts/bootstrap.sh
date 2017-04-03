5e8bad878e957b641a9a46fdc2d4d9e65429e6e5
Matt Andrews <matt@mattandrews.info>
2017-04-03 11:05:13 +0100

    Fix nginx deploy

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,5 +5,6 @@ cd /var/www/biglotteryfund
 npm install
 
 # configure nginx (would be nice to conditionally restart here)
+rm /etc/nginx/sites-enabled/default
 cp /var/www/biglotteryfund/config/server.conf /etc/nginx/sites-enabled
 service nginx restart
