179306066eb657c71809024417993efca4ec465a
Matt Andrews <matt@mattandrews.info>
2017-04-10 16:00:27 +0100

    Only log to a file in production

--- app/logger.js
+++ app/logger.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 const logFilename = 'blf.log';
 const logFilePath = path.join(__dirname, 'logs', logFilename);
-winston.add(winston.transports.File, { filename: logFilePath });
+
+if (process.env.NODE_ENV === 'production') {
+    winston.add(winston.transports.File, {filename: logFilePath});
+}
+
 winston.log('info', 'Logger initialised at ' + logFilePath);
 module.exports = winston;
