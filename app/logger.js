21a24dc97e41a990034d21539f4017ec83492271
Matt Andrews <matt@mattandrews.info>
2017-04-10 15:43:33 +0100

    Add logging to logger (!)

--- app/logger.js
+++ app/logger.js
@@ -5,5 +5,5 @@ const path = require('path');
 const logFilename = 'blf.log';
 const logFilePath = path.join(__dirname, 'logs', logFilename);
 winston.add(winston.transports.File, { filename: logFilePath });
-
+winston.log('info', 'Logger initialised at ' + logFilePath);
 module.exports = winston;
