8233989a74739d08e3de73f4f75fd6a24d56e01a
Matt Andrews <matt@mattandrews.info>
2017-04-19 11:33:48 +0100

    Wrap carousel in a presence check

--- assets/js/modules/carousel.js
+++ assets/js/modules/carousel.js
@@ -2,25 +2,29 @@ const Siema = require('../libs/siema.min');
 
 const init = function (settings) {
 
-    let carousel = new Siema({
-        selector: settings.selector,
-        perPage: settings.perPage || 1,
-        duration: 200,
-        easing: 'ease-out',
-        startIndex: 0,
-        draggable: false,
-        loop: false
-    });
+    let carouselElm = document.querySelector(settings.selector);
 
-    if (settings.nextSelector) {
-        document.querySelector(settings.nextSelector).addEventListener('click', () => carousel.next());
+    if (carouselElm) {
+        let carousel = new Siema({
+            selector: settings.selector,
+            perPage: settings.perPage || 1,
+            duration: 200,
+            easing: 'ease-out',
+            startIndex: 0,
+            draggable: false,
+            loop: false
+        });
+
+        if (settings.nextSelector) {
+            document.querySelector(settings.nextSelector).addEventListener('click', () => carousel.next());
+        }
+
+        if (settings.prevSelector) {
+            document.querySelector(settings.prevSelector).addEventListener('click', () => carousel.prev());
+        }
+
+        return carousel;
     }
-
-    if (settings.prevSelector) {
-        document.querySelector(settings.prevSelector).addEventListener('click', () => carousel.prev());
-    }
-
-    return carousel;
 };
 
 module.exports = {
