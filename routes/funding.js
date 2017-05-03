0a150e0171f47e1a47f3ee2203b0b6cd487f40ff
Matt Andrews <matt@mattandrews.info>
2017-05-03 09:23:47 +0100

    Add a test route for checking live pages work

--- routes/funding.js
+++ routes/funding.js
@@ -1,9 +1,11 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-// const logger = require('../logger');
 
-router.get('/funding-guidance/managing-your-funding', (req, res, next) => {
+router.get([
+        '/funding-guidance/managing-your-funding',
+        '/test'
+    ], (req, res, next) => {
     res.render('pages/funding/guidance/managing-your-funding', {
         title: "Managing your funding"
     });
