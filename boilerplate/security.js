35e0bbdc1aa63e898023104bb8d4cac3762de0c3
Matt Andrews <matt@mattandrews.info>
2017-05-05 21:21:43 +0100

    Fix google issue with CSS

--- boilerplate/security.js
+++ boilerplate/security.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
         contentSecurityPolicy: {
             directives: {
                 defaultSrc: ["'self'", 'fonts.gstatic.com', 'code.ionicframework.com'],
-                styleSrc: ["'self'", 'code.ionicframework.com', 'fonts.googleapis.com'],
+                styleSrc: ["'self'", "'unsafe-inline'", 'code.ionicframework.com', 'fonts.googleapis.com'],
                 connectSrc: ['ws://127.0.0.1:35729/livereload'] // make dev-only?
             }
         },
