e56f89d7bf00731e4931d444b30c388024fb7fe4
Matt Andrews <matt@mattandrews.info>
2017-05-05 12:13:10 +0100

    Tidy up i18n

--- boilerplate/middleware.js
+++ boilerplate/middleware.js
@@ -2,6 +2,7 @@
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const i18n = require('i18n-2');
 // const favicon = require('serve-favicon');
 
 module.exports = function (app) {
@@ -10,4 +11,17 @@ module.exports = function (app) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(cookieParser());
+
+    // internationalisation
+    i18n.expressBind(app, {
+        locales: ['en', 'cy'],
+        cookieName: 'locale',
+        extension: '.json'
+    });
+
+    // set locale from cookie
+    app.use(function(req, res, next) {
+        req.i18n.setLocaleFromCookie();
+        next();
+    });
 };
