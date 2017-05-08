1ae14f9e293d47748add741c67584b7f8876c3ed
Matt Andrews <matt@mattandrews.info>
2017-05-08 12:50:40 +0100

    Add initial structure for material ordering page to allow for configuration in future

--- routes/funding.js
+++ routes/funding.js
@@ -1,9 +1,11 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const materials = require('../config/content/materials.json');
 
 const PATHS = {
     manageFunding: '/funding-guidance/managing-your-funding',
+    freeMaterials: '/funding-guidance/managing-your-funding/ordering-free-materials',
     logoIndex:     '/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos',
     logoDownloads: '/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos/logodownloads'
 };
@@ -24,6 +26,14 @@ router.get(PATHS.logoDownloads, (req, res, next) => {
 router.get(PATHS.manageFunding, (req, res, next) => {
     res.render('pages/funding/guidance/managing-your-funding', {
         title: "Managing your funding"
+    });
+});
+
+// ordering free materials page
+router.get(PATHS.freeMaterials, (req, res, next) => {
+    res.render('pages/funding/guidance/order-free-materials', {
+        title: req.i18n.__("funding.guidance.order-free-materials.title"),
+        materials: materials.categories
     });
 });
 
