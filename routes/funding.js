134ddb3a94f349333684749a21487746ce38f3b7
Matt Andrews <matt@mattandrews.info>
2017-05-17 14:26:08 +0100

    Refactor form logic

--- routes/funding.js
+++ routes/funding.js
@@ -35,27 +35,75 @@ module.exports = (pages) => {
     const freeMaterials = pages.freeMaterials;
     let orderKey = 'orderedMaterials';
 
+    // handle adding/removing items
     router.route(freeMaterials.path + '/item/:id').post((req, res, next) => {
         const validActions = ['increase', 'decrease', 'remove'];
         const action = req.body.action;
-        if (validActions.indexOf(action) !== -1) {
 
-            // @TODO sanitise
-            const id = req.params.id;
-            const code = req.body.code;
+        // @TODO sanitise
+        // get form parameters
+        const id = parseInt(req.params.id);
+        const code = req.body.code;
 
+        // look up the item they're adding
+        const item = materials.items.find(i => i.id === id);
+
+        // is this a valid item/action?
+        if (item && validActions.indexOf(action) !== -1) {
+
+            let maxQuantity = item.maximum;
+            let notAllowedWithItemId = item.notAllowedWithItem;
+
+            // get all their current orders
+            let orders = _.get(req.session, [orderKey], {});
+
+            // how many of the current item do they have?
             const currentItemQuantity = _.get(req.session, [orderKey, code, 'quantity'], 0);
+
+            // store the ID of this item
             _.set(req.session, [orderKey, code, 'id'], id);
 
+            // are they blocked from adding this item?
+            let hasBlockerItem = false;
+
+            if (notAllowedWithItemId) {
+                // check if their current orders contain a blocker
+                for (let code in orders) {
+                    if (orders[code].id === notAllowedWithItemId) {
+                        hasBlockerItem = true;
+                    }
+                }
+            }
+
+            // can they add more of this item?
+            const noSpaceLeft = (currentItemQuantity === maxQuantity);
+
             if (action === 'increase') {
-                _.set(req.session, [orderKey, code, 'quantity'], currentItemQuantity + 1);
+                if (!noSpaceLeft && !hasBlockerItem) {
+                    _.set(req.session, [orderKey, code, 'quantity'], currentItemQuantity + 1);
+                } else {
+                    console.log('cannot add more, either out of space or blocked', noSpaceLeft, hasBlockerItem);
+                }
             } else if (currentItemQuantity > 1 && action === 'decrease') {
                 _.set(req.session, [orderKey, code, 'quantity'], currentItemQuantity - 1);
             } else if (action === 'remove' || (action === 'decrease' && currentItemQuantity === 1)) {
                 _.unset(req.session, [orderKey, code]);
             }
         }
-        res.redirect(req.baseUrl + '/test');
+
+        res.format({
+            html: function () {
+                // res.redirect(req.baseUrl + freeMaterials.path);
+                res.redirect(req.baseUrl + '/test');
+            },
+            json: function () {
+                res.send({
+                    status: 'success',
+                    quantity: _.get(req.session, [orderKey, code, 'quantity'], 0)
+                });
+            }
+        });
+
     });
 
     router.route([freeMaterials.path, '/test'])
@@ -80,77 +128,25 @@ module.exports = (pages) => {
             delete req.session.errors;
         })
         .post((req, res, next) => {
+            req.checkBody('yourName', 'Please provide your name').notEmpty();
+            req.checkBody('yourAddress', 'Please provide your address').notEmpty();
+            req.checkBody('yourNumber', 'Please provide your phone number').notEmpty();
+            req.checkBody('yourEmail', 'Please provide your email address').notEmpty();
+            req.checkBody('yourProjectName', 'Please provide your project name').notEmpty();
+            req.checkBody('yourProjectID', 'Please provide your project ID number').notEmpty();
+            req.checkBody('yourGrantAmount', 'Please provide your grant amount').notEmpty();
 
-            // internal ID
-            let id = req.body.id;
-            // product code
-            let code = req.body.code;
-
-            const currentItemQuantity = _.get(req.session, [orderKey, code, 'quantity'], 0);
-            _.set(req.session, [orderKey, code, 'quantity'], currentItemQuantity + 1);
-            _.set(req.session, [orderKey, code, 'id'], id);
-
-            res.redirect(req.baseUrl + '/test');
-
-            // let itemID = parseInt(req.body.itemID);
-            // let categoryID = parseInt(req.body.categoryID);
-            // let notAllowedWithItem = req.body.notAllowedWithItem;
-            //
-            // // create storage
-            // if (!req.session.quantities) { req.session.quantities = {}; }
-            // if (!req.session.quantities[itemID]) { req.session.quantities[itemID] = 0; }
-            //
-            // // find the item data
-            // let cat = materials.categories.find(c => c.id === categoryID);
-            // let item = cat.items.find(i => i.id === itemID);
-            // let maxQuantity = item.maximum;
-            //
-            // // increment or decrement count
-            // if (req.body.quantity === 'increase') {
-            //     const blockedFromAdding = (notAllowedWithItem && req.session.quantities[notAllowedWithItem]);
-            //     if (!blockedFromAdding && (!maxQuantity || req.session.quantities[itemID] < maxQuantity)) {
-            //         req.session.quantities[itemID] = req.session.quantities[itemID] + 1;
-            //     }
-            // } else {
-            //     if (req.session.quantities[itemID] !== 0) {
-            //         req.session.quantities[itemID] = req.session.quantities[itemID] - 1;
-            //     }
-            // }
-            // res.format({
-            //     html: function () {
-            //         // res.redirect(req.baseUrl + freeMaterials.path);
-            //         res.redirect(req.baseUrl + '/test');
-            //     },
-            //     json: function () {
-            //         res.send({
-            //             status: 'success',
-            //             data: req.session.quantities[itemID],
-            //             all: req.session.quantities
-            //         });
-            //     }
-            // });
+            req.getValidationResult().then((result) => {
+                if (!result.isEmpty()) {
+                    req.session.errors = result.array();
+                    req.session.values = req.body;
+                    // res.redirect(req.baseUrl + freeMaterials.path);
+                    res.redirect(req.baseUrl + '/test');
+                } else {
+                    res.send(req.body);
+                }
+            });
         });
-
-    router.post('/order-materials', (req, res, next) => {
-        req.checkBody('yourName', 'Please provide your name').notEmpty();
-        req.checkBody('yourAddress', 'Please provide your address').notEmpty();
-        req.checkBody('yourNumber', 'Please provide your phone number').notEmpty();
-        req.checkBody('yourEmail', 'Please provide your email address').notEmpty();
-        req.checkBody('yourProjectName', 'Please provide your project name').notEmpty();
-        req.checkBody('yourProjectID', 'Please provide your project ID number').notEmpty();
-        req.checkBody('yourGrantAmount', 'Please provide your grant amount').notEmpty();
-
-        req.getValidationResult().then((result) => {
-            if (!result.isEmpty()) {
-                req.session.errors = result.array();
-                req.session.values = req.body;
-                res.redirect(req.baseUrl + freeMaterials.path);
-            } else {
-                res.send(req.body);
-            }
-        });
-    });
-
 
     return router;
 };
