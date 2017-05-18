344fca862b000d7ee83415037cff0bb89af0fecf
Matt Andrews <matt@mattandrews.info>
2017-05-18 13:04:22 +0100

    Save env vars to profile

--- bin/scripts/env_vars.sh
+++ bin/scripts/env_vars.sh
@@ -6,3 +6,7 @@ SES_PASSWORD=$(aws ssm get-parameters --region eu-west-1 --names ses.auth.passwo
 export SES_PASSWORD=`echo $SES_PASSWORD | sed -e 's/^"//' -e 's/"$//'`
 SES_USER=$(aws ssm get-parameters --region eu-west-1 --names ses.auth.user --with-decryption --query 'Parameters[0].Value')
 export SES_USER=`echo $SES_USER | sed -e 's/^"//' -e 's/"$//'`
+
+rm -f ~/.bash_profile
+echo 'export SES_USER='$SES_USER >> ~/.bash_profile
+echo 'export SES_PASSWORD='$SES_PASSWORD >> ~/.bash_profile
