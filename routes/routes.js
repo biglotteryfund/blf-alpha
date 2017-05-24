6b170faa36f583480f7938e7f1e45bbc5c464324
Matt Andrews <matt@mattandrews.info>
2017-05-24 15:42:51 +0100

    Tweak route syntax

--- routes/routes.js
+++ routes/routes.js
@@ -5,53 +5,53 @@ const handlers = {
 };
 
 const routes = {
-    "sections": {
-        "funding": {
-            "name": "Funding",
-            "path": "/funding",
-            "handler": handlers.funding,
-            "pages": {
-                "logos": {
-                    "name": "Logos",
-                    "path": "/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos/logodownloads",
-                    "template": "pages/funding/guidance/logos",
-                    "lang": "funding.guidance.logos",
-                    "code": 1,
-                    "static": true,
-                    "live": true,
-                    "aliases": [
+    sections: {
+        funding: {
+            name: "Funding",
+            path: "/funding",
+            handler: handlers.funding,
+            pages: {
+                logos: {
+                    name: "Logos",
+                    path: "/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos/logodownloads",
+                    template: "pages/funding/guidance/logos",
+                    lang: "funding.guidance.logos",
+                    code: 1,
+                    static: true,
+                    live: true,
+                    aliases: [
                         "/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos",
                         "/funding-guidance/managing-your-funding/logodownloads"
                     ]
                 },
-                "manageFunding": {
-                    "name": "Managing your funding",
-                    "path": "/funding-guidance/managing-your-funding",
-                    "template": "pages/funding/guidance/managing-your-funding",
-                    "lang": "funding.guidance.managing-your-funding",
-                    "code": 2,
-                    "static": true,
-                    "live": true
+                manageFunding: {
+                    name: "Managing your funding",
+                    path: "/funding-guidance/managing-your-funding",
+                    template: "pages/funding/guidance/managing-your-funding",
+                    lang: "funding.guidance.managing-your-funding",
+                    code: 2,
+                    static: true,
+                    live: true
                 },
-                "freeMaterials": {
-                    "name": "Ordering free materials",
-                    "path": "/funding-guidance/managing-your-funding/ordering-free-materials",
-                    "template": "pages/funding/guidance/order-free-materials",
-                    "lang": "funding.guidance.order-free-materials",
-                    "code": 3,
-                    "live": true,
-                    "aliases": [
+                freeMaterials: {
+                    name: "Ordering free materials",
+                    path: "/funding-guidance/managing-your-funding/ordering-free-materials",
+                    template: "pages/funding/guidance/order-free-materials",
+                    lang: "funding.guidance.order-free-materials",
+                    code: 3,
+                    live: true,
+                    aliases: [
                         '/funding-guidance/managing-your-funding/ordering-free-materials/bilingual-materials-for-use-in-wales'
                     ]
                 },
-                "helpWithPublicity": {
-                    "name": "Help with publicity",
-                    "path": "/funding-guidance/managing-your-funding/help-with-publicity",
-                    "template": "pages/funding/guidance/help-with-publicity",
-                    "lang": "funding.guidance.help-with-publicity",
-                    "code": 12,
-                    "static": true,
-                    "live": true
+                helpWithPublicity: {
+                    name: "Help with publicity",
+                    path: "/funding-guidance/managing-your-funding/help-with-publicity",
+                    template: "pages/funding/guidance/help-with-publicity",
+                    lang: "funding.guidance.help-with-publicity",
+                    code: 12,
+                    static: true,
+                    live: true
                 }
             }
         }
