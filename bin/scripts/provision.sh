dadae8a7ad6343ef4e2c834ea2495d2e6084106e
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-07-13 11:13:11 +0100

    Fix a typo

--- bin/scripts/provision.sh
+++ bin/scripts/provision.sh
@@ -4,9 +4,8 @@
 ###############################################
 # CHANGING THIS FILE REQUIRES RE-UPLOADING IT #
 # AND THEN CLONING THE LAUNCH CONFIGURATIONS. #
-# FUTURE DEPLOYS MAY FAIL IF YOU FORGE THIS!! #
+# FUTURE DEPLOYS MAY FAIL IF YOU FORGET THIS! #
 ###############################################
-
 
 #install codedeploy agent
 apt-get update
