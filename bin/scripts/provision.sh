ea2fdfc9eb8f89350b66504ab017c558a0d4f8ea
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-08-17 09:38:13 +0100

    Avoid build error if aws cli tools are already installed

--- bin/scripts/provision.sh
+++ bin/scripts/provision.sh
@@ -39,6 +39,7 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 apt-get install -y nodejs
 
 # install aws cli (to fetch secrets from parameterstore)
+rm -f awscli-bundle.zip
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
