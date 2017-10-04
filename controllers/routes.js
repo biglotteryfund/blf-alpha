42286226b04f43268603ec335bc50d553e250307
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-10-04 16:26:42 +0100

    Revert "Test linter"
    This reverts commit 6f58f0c52204ce2c14056785607db742bf5a73e5.

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,14 +1,12 @@
-'use strict';
+"use strict";
 const config = require('config');
 const anchors = config.get('anchors');
 
 // pass some parameters onto each controller
 // so we can take route config and init all at once
-let loadController = path => {
+let loadController = (path) => {
     return (pages, sectionPath, sectionId) => require(path)(pages, sectionPath, sectionId);
 };
-
-console.log('hello');
 
 // define top-level controllers for the site
 const controllers = {
@@ -19,11 +17,11 @@ const controllers = {
 
 // configure base paths for site sections
 const sectionPaths = {
-    toplevel: '',
-    funding: '/funding',
-    about: '/about-big', // @TODO rename on launch
-    aboutLegacy: '/about-big', // used on the old site
-    research: '/research'
+    toplevel: "",
+    funding: "/funding",
+    about: "/about-big", // @TODO rename on launch
+    aboutLegacy: "/about-big", // used on the old site
+    research: "/research"
 };
 
 // these top-level sections appear in the main site nav
@@ -31,51 +29,51 @@ const sectionPaths = {
 const routes = {
     sections: {
         toplevel: {
-            name: 'Top-level pages',
-            langTitlePath: 'global.nav.home',
+            name: "Top-level pages",
+            langTitlePath: "global.nav.home",
             path: sectionPaths.toplevel,
             controller: controllers.toplevel,
             pages: {
                 home: {
-                    name: 'Home',
-                    path: '/',
-                    template: 'pages/toplevel/home',
-                    lang: 'toplevel.home',
+                    name: "Home",
+                    path: "/",
+                    template: "pages/toplevel/home",
+                    lang: "toplevel.home",
                     code: 0,
                     static: false,
                     live: true,
                     isPostable: true
                 },
                 contact: {
-                    name: 'Contact',
-                    path: '/contact',
-                    template: 'pages/toplevel/contact',
-                    lang: 'toplevel.contact',
+                    name: "Contact",
+                    path: "/contact",
+                    template: "pages/toplevel/contact",
+                    lang: "toplevel.contact",
                     code: 4,
                     static: true,
                     live: true,
                     aliases: [
-                        sectionPaths.toplevel + '/about-big/contact-us',
-                        sectionPaths.toplevel + '/help-and-support',
-                        sectionPaths.toplevel + '/england/about-big/contact-us',
-                        sectionPaths.toplevel + '/wales/about-big/contact-us',
-                        sectionPaths.toplevel + '/scotland/about-big/contact-us',
-                        sectionPaths.toplevel + '/northernireland/about-big/contact-us'
+                        sectionPaths.toplevel + "/about-big/contact-us",
+                        sectionPaths.toplevel + "/help-and-support",
+                        sectionPaths.toplevel + "/england/about-big/contact-us",
+                        sectionPaths.toplevel + "/wales/about-big/contact-us",
+                        sectionPaths.toplevel + "/scotland/about-big/contact-us",
+                        sectionPaths.toplevel + "/northernireland/about-big/contact-us"
                     ]
                 },
                 data: {
-                    name: 'Data',
-                    path: '/data',
-                    template: 'pages/toplevel/data',
-                    lang: 'toplevel.data',
+                    name: "Data",
+                    path: "/data",
+                    template: "pages/toplevel/data",
+                    lang: "toplevel.data",
                     static: false,
                     live: true
                 },
                 jobs: {
-                    name: 'Jobs',
-                    path: '/jobs',
-                    template: 'pages/toplevel/jobs',
-                    lang: 'toplevel.jobs',
+                    name: "Jobs",
+                    path: "/jobs",
+                    template: "pages/toplevel/jobs",
+                    lang: "toplevel.jobs",
                     code: 7,
                     static: true,
                     live: true,
@@ -86,37 +84,39 @@ const routes = {
                     ]
                 },
                 benefits: {
-                    name: 'Benefits',
-                    path: '/jobs/benefits',
-                    template: 'pages/toplevel/benefits',
-                    lang: 'toplevel.benefits',
+                    name: "Benefits",
+                    path: "/jobs/benefits",
+                    template: "pages/toplevel/benefits",
+                    lang: "toplevel.benefits",
                     static: true,
                     live: true,
-                    aliases: [sectionPaths.toplevel + '/about-big/jobs/benefits']
+                    aliases: [
+                        sectionPaths.toplevel + '/about-big/jobs/benefits',
+                    ]
                 },
                 under10k: {
-                    name: 'Under 10k',
-                    path: '/under10k',
-                    template: 'pages/toplevel/under10k',
-                    lang: 'toplevel.under10k',
+                    name: "Under 10k",
+                    path: "/under10k",
+                    template: "pages/toplevel/under10k",
+                    lang: "toplevel.under10k",
                     code: 29,
                     static: true,
                     live: true
                 },
                 over10k: {
-                    name: 'Over 10k',
-                    path: '/over10k',
-                    template: 'pages/toplevel/over10k',
-                    lang: 'toplevel.over10k',
+                    name: "Over 10k",
+                    path: "/over10k",
+                    template: "pages/toplevel/over10k",
+                    lang: "toplevel.over10k",
                     code: 30,
                     static: true,
                     live: true
                 },
                 eyp: {
-                    name: 'Empowering Young People',
-                    path: '/empowering-young-people',
-                    template: 'pages/toplevel/eyp',
-                    lang: 'toplevel.eyp',
+                    name: "Empowering Young People",
+                    path: "/empowering-young-people",
+                    template: "pages/toplevel/eyp",
+                    lang: "toplevel.eyp",
                     code: 0,
                     static: true,
                     live: true,
@@ -127,51 +127,49 @@ const routes = {
             }
         },
         funding: {
-            name: 'Funding',
-            langTitlePath: 'global.nav.funding',
+            name: "Funding",
+            langTitlePath: "global.nav.funding",
             path: sectionPaths.funding,
             controller: controllers.funding,
             pages: {
                 logos: {
-                    name: 'Logos',
-                    path: '/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos/logodownloads',
-                    template: 'pages/funding/guidance/logos',
-                    lang: 'funding.guidance.logos',
+                    name: "Logos",
+                    path: "/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos/logodownloads",
+                    template: "pages/funding/guidance/logos",
+                    lang: "funding.guidance.logos",
                     code: 1,
                     static: true,
                     live: true,
                     aliases: [
-                        sectionPaths.funding +
-                            '/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos',
-                        sectionPaths.funding + '/funding-guidance/managing-your-funding/logodownloads'
+                        sectionPaths.funding + "/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos",
+                        sectionPaths.funding + "/funding-guidance/managing-your-funding/logodownloads"
                     ]
                 },
                 manageFunding: {
-                    name: 'Managing your funding',
-                    path: '/funding-guidance/managing-your-funding',
-                    template: 'pages/funding/guidance/managing-your-funding',
-                    lang: 'funding.guidance.managing-your-funding',
+                    name: "Managing your funding",
+                    path: "/funding-guidance/managing-your-funding",
+                    template: "pages/funding/guidance/managing-your-funding",
+                    lang: "funding.guidance.managing-your-funding",
                     code: 2,
                     static: true,
                     live: true,
                     aliases: [
                         sectionPaths.funding + '/funding-guidance/managing-your-funding/help-with-publicity',
-                        '/welcome',
-                        '/publicity'
+                        "/welcome",
+                        "/publicity"
                     ]
                 },
                 freeMaterials: {
-                    name: 'Ordering free materials',
-                    path: '/funding-guidance/managing-your-funding/ordering-free-materials',
-                    template: 'pages/funding/guidance/order-free-materials',
-                    lang: 'funding.guidance.order-free-materials',
+                    name: "Ordering free materials",
+                    path: "/funding-guidance/managing-your-funding/ordering-free-materials",
+                    template: "pages/funding/guidance/order-free-materials",
+                    lang: "funding.guidance.order-free-materials",
                     code: 3,
                     live: true,
                     isPostable: true,
                     isWildcard: true,
                     aliases: [
-                        sectionPaths.funding +
-                            '/funding-guidance/managing-your-funding/ordering-free-materials/bilingual-materials-for-use-in-wales',
+                        sectionPaths.funding + '/funding-guidance/managing-your-funding/ordering-free-materials/bilingual-materials-for-use-in-wales',
                         '/wales/funding/funding-guidance/managing-your-funding/ordering-free-materials',
                         '/scotland/funding/funding-guidance/managing-your-funding/ordering-free-materials',
                         '/england/funding/funding-guidance/managing-your-funding/ordering-free-materials',
@@ -179,19 +177,19 @@ const routes = {
                     ]
                 },
                 helpWithPublicity: {
-                    name: 'Help with publicity',
-                    path: '/funding-guidance/managing-your-funding/social-media',
-                    template: 'pages/funding/guidance/help-with-publicity',
-                    lang: 'funding.guidance.help-with-publicity',
+                    name: "Help with publicity",
+                    path: "/funding-guidance/managing-your-funding/social-media",
+                    template: "pages/funding/guidance/help-with-publicity",
+                    lang: "funding.guidance.help-with-publicity",
                     code: 12,
                     static: true,
                     live: true
                 },
                 pressCoverage: {
-                    name: 'Getting press coverage',
-                    path: '/funding-guidance/managing-your-funding/press',
-                    template: 'pages/funding/guidance/getting-press-coverage',
-                    lang: 'funding.guidance.getting-press-coverage',
+                    name: "Getting press coverage",
+                    path: "/funding-guidance/managing-your-funding/press",
+                    template: "pages/funding/guidance/getting-press-coverage",
+                    lang: "funding.guidance.getting-press-coverage",
                     code: 18,
                     static: true,
                     live: true
@@ -199,22 +197,22 @@ const routes = {
             }
         },
         research: {
-            name: 'Research',
-            langTitlePath: 'global.nav.research',
+            name: "Research",
+            langTitlePath: "global.nav.research",
             path: sectionPaths.research
         },
         // @TODO rename this to 'about' when ready to launch /about
-        'about-big': {
-            name: 'About',
-            langTitlePath: 'global.nav.about',
+        "about-big": {
+            name: "About",
+            langTitlePath: "global.nav.about",
             path: sectionPaths.about,
             controller: controllers.about,
             pages: {
                 root: {
-                    name: 'About',
-                    path: '/',
-                    template: 'pages/toplevel/about',
-                    lang: 'toplevel.about',
+                    name: "About",
+                    path: "/",
+                    template: "pages/toplevel/about",
+                    lang: "toplevel.about",
                     code: 0,
                     static: true,
                     live: false,
@@ -223,29 +221,29 @@ const routes = {
                     ]
                 },
                 freedomOfInformation: {
-                    name: 'Freedom of Information',
-                    path: '/customer-service/freedom-of-information',
-                    template: 'pages/about/freedom-of-information',
-                    lang: 'about.foi',
+                    name: "Freedom of Information",
+                    path: "/customer-service/freedom-of-information",
+                    template: "pages/about/freedom-of-information",
+                    lang: "about.foi",
                     code: 85,
                     static: true,
                     live: true,
                     aliases: [
                         // sectionPaths.aboutLegacy + "/customer-service/freedom-of-information",
-                        '/freedom-of-information'
+                        "/freedom-of-information"
                     ]
                 },
                 dataProtection: {
-                    name: 'Data Protection',
-                    path: '/customer-service/data-protection',
-                    template: 'pages/about/data-protection',
-                    lang: 'about.dataProtection',
+                    name: "Data Protection",
+                    path: "/customer-service/data-protection",
+                    template: "pages/about/data-protection",
+                    lang: "about.dataProtection",
                     code: 84,
                     static: true,
                     live: true,
                     aliases: [
                         // sectionPaths.aboutLegacy + "/customer-service/data-protection",
-                        '/data-protection'
+                        "/data-protection"
                     ]
                 }
             }
@@ -264,8 +262,8 @@ const vanityRedirects = [
     {
         // this has to be here and not as an alias
         // otherwise it won't be recognised as a welsh URL
-        name: 'Publicity (Welsh)',
-        path: '/cyhoeddusrwydd',
+        name: "Publicity (Welsh)",
+        path: "/cyhoeddusrwydd",
         destination: '/welsh' + vanityDestinations.publicity,
         aliasOnly: true
     },
@@ -273,45 +271,45 @@ const vanityRedirects = [
         // this stays here (and not as an alias) as express doesn't care about URL case
         // and this link is the same (besides case) as an existing alias
         // (annoyingly, the Title Case version of this link persists on the web... for now.)
-        name: 'Logo page',
-        path: '/funding/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos/LogoDownloads',
+        name: "Logo page",
+        path: "/funding/funding-guidance/managing-your-funding/grant-acknowledgement-and-logos/LogoDownloads",
         destination: routes.sections.funding.path + routes.sections.funding.pages.logos.path,
         aliasOnly: true
     },
     // the following aliases use custom destinations (eg. with URL anchors)
     // so can't live in the regular aliases section (as they all have the same destination)
     {
-        name: 'Contact press team',
-        path: '/news-and-events/contact-press-team',
+        name: "Contact press team",
+        path: "/news-and-events/contact-press-team",
         destination: vanityDestinations.contact + '#' + anchors.contactPress
     },
     {
-        name: 'Contact press team (Welsh)',
-        path: '/welsh/news-and-events/contact-press-team',
+        name: "Contact press team (Welsh)",
+        path: "/welsh/news-and-events/contact-press-team",
         destination: '/welsh' + vanityDestinations.contact + '#' + anchors.contactPress
     },
     {
-        name: 'Complaint page',
+        name: "Complaint page",
         path: '/about-big/customer-service/making-a-complaint',
         destination: vanityDestinations.contact + '#' + anchors.contactComplaints
     },
     {
-        name: 'Complaint page (England)',
+        name: "Complaint page (England)",
         path: '/england/about-big/customer-service/making-a-complaint',
         destination: vanityDestinations.contact + '#' + anchors.contactComplaints
     },
     {
-        name: 'Complaint page (Welsh)',
+        name: "Complaint page (Welsh)",
         path: '/welsh/about-big/customer-service/making-a-complaint',
         destination: '/welsh' + vanityDestinations.contact + '#' + anchors.contactComplaints
     },
     {
-        name: 'Fraud page',
+        name: "Fraud page",
         path: '/about-big/customer-service/fraud',
         destination: vanityDestinations.contact + '#' + anchors.contactFraud
     },
     {
-        name: 'Fraud page (Welsh)',
+        name: "Fraud page (Welsh)",
         path: '/welsh/about-big/customer-service/fraud',
         destination: '/welsh' + vanityDestinations.contact + '#' + anchors.contactFraud
     }
