61cace23ccc574359679469378fd7ab0e3b0f0ee
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-10-13 14:41:02 +0100

    Move CMS URL to env vars

--- modules/content.js
+++ modules/content.js
@@ -1,7 +1,14 @@
 'use strict';
 const rp = require('request-promise');
+const has = require('lodash/has');
+require('dotenv').config();
 
-const API_URL = 'http://craft3.dev/content/';
+if (!has(process.env, 'CMS_URL')) {
+    console.log('Error: CMS_URL environment variable must be defined');
+    process.exit(1);
+}
+
+const API_URL = process.env.CMS_URL + '/content/';
 
 const getFundingProgrammes = locale => {
     return rp({
