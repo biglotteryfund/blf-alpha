e8bf5762dee0198c2f281e957dcd3332db20054d
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-10-16 10:49:14 +0100

    Use custom build of autotrack rather than importing

--- assets/js/bootstraps/analytics.js
+++ assets/js/bootstraps/analytics.js
@@ -2,8 +2,6 @@
 import $ from 'jquery';
 const analytics = require('../modules/analytics');
 
-import 'autotrack/lib/plugins/max-scroll-tracker';
-
 export const init = () => {
     // grab main script element (for querying data attributes)
     const $thisScript = $('#js-script-main');
@@ -11,19 +9,12 @@ export const init = () => {
     // get per-environment GA code
     const uaCode = $thisScript.data('ga-code');
 
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        (i[r] =
-            i[r] ||
-            function() {
-                (i[r].q = i[r].q || []).push(arguments);
-            }),
-            (i[r].l = 1 * new Date());
-        (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m);
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    window.ga =
+        window.ga ||
+        function() {
+            (ga.q = ga.q || []).push(arguments);
+        };
+    ga.l = +new Date();
 
     const CUSTOM_METRICS = {
         maxScrollPercentage: {
