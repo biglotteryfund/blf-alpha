2fdff4bd309485544083b4484d49bb666c74ac51
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-10-24 12:38:54 +0100

    Salt and hash passwords automatically upon creating user models

--- models/user.js
+++ models/user.js
@@ -4,10 +4,17 @@ const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
     let User = sequelize.define('user', {
         username: {
-            type: DataTypes.STRING
+            type: DataTypes.STRING,
+            allowNull: false
         },
         password: {
-            type: DataTypes.STRING
+            type: DataTypes.STRING,
+            allowNull: false,
+            set(val) {
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(val, salt);
+                this.setDataValue('password', hash);
+            }
         },
         level: {
             type: DataTypes.INTEGER
