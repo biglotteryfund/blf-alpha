f880f16b2e3f758e87d56333aed9dd542811a47d
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-10-30 09:24:42 +0000

    Update getSecrets to only warn in dev/CI

--- modules/secrets.js
+++ modules/secrets.js
@@ -1,17 +1,22 @@
 'use strict';
-const { flow, keyBy, mapValues } = require('lodash/fp');
 const fs = require('fs');
+const { flow, isEmpty, keyBy, mapValues } = require('lodash/fp');
 
-let rawParameters;
-try {
-    rawParameters = JSON.parse(fs.readFileSync('/etc/blf/parameters.json'), 'utf8');
-} catch (e) {
-    console.info('parameters.json not found -- are you in DEV mode?');
+function getRawParameters() {
+    let rawParameters;
+    try {
+        rawParameters = JSON.parse(fs.readFileSync('/etc/blf/parameters.json'), 'utf8');
+    } catch (e) {} // eslint-disable-line no-empty
+    return rawParameters;
 }
-const secrets = flow(keyBy('Name'), mapValues('Value'))(rawParameters);
+
+const mapKeyedValues = flow(keyBy('Name'), mapValues('Value'));
+const secrets = mapKeyedValues(getRawParameters());
 
 function getSecret(name) {
-    if (secrets[name]) {
+    if (process.env.CI === true || isEmpty(secrets)) {
+        console.warn(`Secret "${name}" not found: are you in CI or DEVELOPMENT mode?`);
+    } else if (name in secrets) {
         return secrets[name];
     } else {
         throw new Error(`Could not find property ${name} in secrets`);
