2a884061baa1ff1ffa27ec052a91bc58c648f06d
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-11-02 13:11:19 +0000

    Load dotenv in development, allow overriding of secrets locally

--- modules/content.js
+++ modules/content.js
@@ -2,7 +2,8 @@
 const rp = require('request-promise');
 const getSecret = require('./get-secret');
 
-let CMS_URL = getSecret('cms.url') || process.env.cmsUrl;
+let CMS_URL = process.env.cmsUrl || getSecret('cms.url');
+
 if (!CMS_URL) {
     console.log('Error: CMS_URL endpoint must be defined');
     process.exit(1);
