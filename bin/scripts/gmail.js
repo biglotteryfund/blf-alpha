e685d67f28c7f8c51cc5a1352c787e9f084ec49e
Matt Andrews <matt@mattandrews.info>
2017-11-16 16:16:16 +0000

    Add basic Gmail retrieval script

---
+++ bin/scripts/gmail.js
@@ -0,0 +1,147 @@
+"use strict";
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const googleAuth = require('google-auth-library');
+const Gmail = require('node-gmail-api');
+
+// https://console.developers.google.com/apis/credentials?project=just-rhythm-186214
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/gmail-nodejs-quickstart.json
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
+const TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json';
+
+// Load client secrets from a local file.
+fs.readFile(path.join('./bin/scripts', 'gmail_secrets.json'), function processClientSecrets(err, content) {
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+    }
+    // Authorize a client with the loaded credentials, then call the Gmail API.
+    authorize(JSON.parse(content), getMail);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+    const clientSecret = credentials.installed.client_secret;
+    const clientId = credentials.installed.client_id;
+    const redirectUrl = credentials.installed.redirect_uris[0];
+    const auth = new googleAuth();
+    const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function (err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+    const authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function (code) {
+        rl.close();
+        oauth2Client.getToken(code, function (err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code !== 'EEXIST') {
+            throw err;
+        }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+}
+
+function base64toUTF8 (str) {
+    return new Buffer(str, 'base64').toString('utf8');
+}
+
+function getMail(auth) {
+    const gmail = new Gmail(auth.credentials.access_token);
+    const getMessages = gmail.messages('subject:Order from Big Lottery Fund website', { max: 10 });
+    getMessages.on('data', function (d) {
+        if (d.payload.body.data) {
+            let re = /The order details are below\:([\s\S]*)The customer's personal details are below\:([\s\S]*)This email has been automatically generated from the Big Lottery Fund Website/;
+            let body = base64toUTF8(d.payload.body.data);
+            let subject = d.payload.headers.find(_ => _.name === 'Subject').value;
+            let bits = body.match(re);
+            let order = bits[1].trim();
+            let address = bits[2].trim();
+            order = order.replace(/\t/g, '').replace(/- /g, '').replace(/\(.*\)/g, '').split('\n').map(_ => _.trim());
+
+            let orders = order.map(o => {
+                return {
+                    quantity: o.match(/x(\d+)/)[1],
+                    item: o.match(/(\w+-\w+)/)[1]
+                };
+            });
+
+            let a = {};
+            address = address.replace(/\t/g, '').replace(/\r/g, '').split('\n');
+            address.forEach(line => {
+                let bits = line.split(':');
+                if (bits[1]) {
+                    a[bits[0]] = bits[1].trim();
+                }
+            });
+
+            let timestamp = subject.match(/Order from Big Lottery Fund website - (.*)/);
+
+            let data = {
+                items: orders,
+                address: a,
+                time: timestamp[1]
+            };
+
+            console.log(data);
+            console.log('====');
+        } else {
+            console.log('Missing payload data');
+        }
+    });
+}
