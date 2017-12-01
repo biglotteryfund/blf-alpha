1358d0ad3279a2a44ca18a0c626db36d38d46b0b
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-12-01 16:14:07 +0000

    Update afa code to inject rough copy

--- controllers/toplevel/legacyPages.js
+++ controllers/toplevel/legacyPages.js
@@ -7,91 +7,120 @@ const cached = require('../../middleware/cached');
 const Raven = require('raven');
 
 function initAwardsForAll(router) {
-    function proxyWithoutChanges(req, res) {
-        return proxyLegacyPage(req, res, dom => dom);
-    }
+    function getReplacementText(applyUrl, contactPhone) {
+        const sharedText = `
+            <p>We are currently trialling a quicker and more straightforward online application form for National Lottery Awards for All. Please follow the link below if you are ready to begin filling in your application:</p>
+            <p><a class="roundedButton blueButton" style="display: inline-block;" href="${
+                applyUrl
+            }">Apply Online</a></p>
+            <p>You will be able to quickly set up an account, save your application form, and return to work on it as required. <strong>If you would like to apply online, please visit the link above and bookmark it in your browser so that you can access it later, as it is not yet visible to all visitors to this page</strong>.</p>
+            <p>We are keen to hear any feedback – positive or negative - and to help with any questions or problems you may have. Please do get in touch with us to discuss any of this by calling <strong>${
+                contactPhone
+            }</strong></p>
+            <p>Please note that you can still submit an application by email or post if preferred.</p>
+        `;
 
-    function proxyAwardsForAllTemporaryChangeMe(applyUrl, req, res) {
-        return proxyLegacyPage(req, res, dom => {
-            res.set('X-BLF-Legacy-Modified', true);
-
-            const applyTab = dom.window.document.querySelector('#mainContentContainer .panel:last-of-type');
-
-            const additionalText = `
-                <h4>Apply online</h4>
-                <p><br/><a class="roundedButton blueButton" style="display: inline-block;" href="${
-                    applyUrl
-                }">Apply Online</a></p>
-                <h4>Apply by post</h4>
-            `;
-
-            if (applyTab && applyTab.textContent.indexOf('please contact us at') !== -1) {
-                applyTab.innerHTML = additionalText + applyTab.innerHTML;
-            } else {
-                Raven.captureMessage('Failed to modify awards for all page', {
-                    tags: {
-                        feature: 'awards-for-all'
-                    }
-                });
-            }
-
-            return dom;
-        });
-    }
-
-    function proxyRoute(route) {
-        const testFn = ab.test(`blf-afa-rollout-${route.id}`, {
-            cookie: {
-                name: config.get('cookies.abTestAwardsForAll'),
-                maxAge: moment.duration(4, 'weeks').asMilliseconds()
-            }
-        });
-
-        const percentageForTest = config.get('abTests.tests.awardsForAll.percentage');
-        const percentages = {
-            A: (100 - percentageForTest) / 100,
-            B: percentageForTest / 100
+        return {
+            tab: `
+                <h3>Apply online</h3>
+                ${sharedText}
+                <h3>Apply by email or post</h3>
+            `,
+            extra: `
+                <h3>Apply online</h3>
+                ${sharedText}
+            `
         };
+    }
 
-        router.get(route.path, cached.noCache, testFn(null, percentages.A), proxyWithoutChanges);
-        router.get(route.path, cached.noCache, testFn(null, percentages.B), route.modifyFn.bind(null, route.applyUrl));
-        router.post(route.path, postToLegacyForm);
+    function modify(replacementText) {
+        return function(req, res) {
+            return proxyLegacyPage(req, res, dom => {
+                res.set('X-BLF-Legacy-Modified', true);
+
+                const applyTab = dom.window.document.querySelector('#mainContentContainer .panel:last-of-type');
+                if (applyTab && applyTab.textContent.indexOf('please contact us at') !== -1) {
+                    applyTab.innerHTML = replacementText.tab + applyTab.innerHTML;
+                } else {
+                    Raven.captureMessage('Failed to modify awards for all tab content', {
+                        tags: {
+                            feature: 'awards-for-all'
+                        }
+                    });
+                }
+
+                const mainContent = dom.window.document.getElementById('accordionContainer');
+                if (mainContent) {
+                    const fragment = dom.window.document.createDocumentFragment();
+                    const newEl = dom.window.document.createElement('div');
+                    newEl.style = 'margin-top: 20px';
+                    newEl.innerHTML = replacementText.extra;
+                    fragment.appendChild(newEl);
+                    mainContent.appendChild(fragment);
+                } else {
+                    Raven.captureMessage('Failed to modify awards for all main content', {
+                        tags: {
+                            feature: 'awards-for-all'
+                        }
+                    });
+                }
+
+                return dom;
+            });
+        };
     }
 
     const awardsForAllRoutes = [
         {
             id: 'england',
             path: legacyProxiedRoutes.awardsForAllEngland.path,
-            applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=en',
-            modifyFn: proxyAwardsForAllTemporaryChangeMe
+            modifyFn: modify(getReplacementText('https://apply.biglotteryfund.org.uk/?cn=en', '0345 4 10 20 30'))
         },
         {
             id: 'scotland',
             path: legacyProxiedRoutes.awardsForAllScotland.path,
-            applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=sc',
-            modifyFn: proxyAwardsForAllTemporaryChangeMe
+            modifyFn: modify(getReplacementText('https://apply.biglotteryfund.org.uk/?cn=sc', '0300 123 7110'))
         },
         {
             id: 'wales',
             path: legacyProxiedRoutes.awardsForAllWales.path,
-            applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=wales&ln=en',
-            modifyFn: proxyAwardsForAllTemporaryChangeMe
+            modifyFn: modify(getReplacementText('https://apply.biglotteryfund.org.uk/?cn=wales&ln=en', '0300 123 0735'))
         },
         {
             id: 'wales-welsh',
             path: legacyProxiedRoutes.awardsForAllWalesWelsh.path,
-            applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=wales&ln=welsh',
-            modifyFn: proxyAwardsForAllTemporaryChangeMe
+            modifyFn: modify(
+                getReplacementText('https://apply.biglotteryfund.org.uk/?cn=wales&ln=welsh', '0300 123 0735')
+            )
         }
     ];
 
     if (config.get('abTests.enabled')) {
-        awardsForAllRoutes.forEach(proxyRoute);
+        awardsForAllRoutes.forEach(route => {
+            const testFn = ab.test(`blf-afa-rollout-${route.id}`, {
+                cookie: {
+                    name: config.get('cookies.abTestAwardsForAll'),
+                    maxAge: moment.duration(4, 'weeks').asMilliseconds()
+                }
+            });
+
+            const percentageForTest = config.get('abTests.tests.awardsForAll.percentage');
+            const percentages = {
+                A: (100 - percentageForTest) / 100,
+                B: percentageForTest / 100
+            };
+
+            router.get(route.path, cached.noCache, testFn(null, percentages.A), function proxyWithoutChanges(req, res) {
+                return proxyLegacyPage(req, res, dom => dom);
+            });
+            router.get(route.path, cached.noCache, testFn(null, percentages.B), route.modifyFn);
+            router.post(route.path, postToLegacyForm);
+        });
     } else {
         awardsForAllRoutes.forEach(route => {
             router
                 .route(route.path)
-                .get(cached.noCache, route.modifyFn.bind(null, route.applyUrl))
+                .get(cached.noCache, route.modifyFn)
                 .post(postToLegacyForm);
         });
     }
