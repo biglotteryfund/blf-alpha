97418f2637fdabf4f3b6a217d12703e8445096c3
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2017-12-04 13:03:54 +0000

    Remove form fields and add a dropdown [WIP]

--- controllers/funding/free-materials/formFields.js
+++ controllers/funding/free-materials/formFields.js
@@ -5,8 +5,8 @@ module.exports = [
         type: 'text',
         required: true,
         label: {
-            'en': 'Name',
-            'cy': 'Eich enw'
+            en: 'Name',
+            cy: 'Eich enw'
         }
     },
     {
@@ -14,17 +14,8 @@ module.exports = [
         type: 'email',
         required: true,
         label: {
-            'en': 'Email address',
-            'cy': 'Eich cyfeiriad e-bost'
-        }
-    },
-    {
-        name: 'yourNumber',
-        type: 'text',
-        required: true,
-        label: {
-            'en': 'Phone number',
-            'cy': 'Eich rhif ffôn'
+            en: 'Email address',
+            cy: 'Eich cyfeiriad e-bost'
         }
     },
     {
@@ -32,8 +23,8 @@ module.exports = [
         type: 'text',
         required: true,
         label: {
-            'en': 'Address line 1',
-            'cy': 'Eich cyfeiriad llinell 1'
+            en: 'Address line 1',
+            cy: 'Eich cyfeiriad llinell 1'
         }
     },
     {
@@ -41,8 +32,8 @@ module.exports = [
         type: 'text',
         required: false,
         label: {
-            'en': 'Address line 2',
-            'cy': 'Eich cyfeiriad llinell 2'
+            en: 'Address line 2',
+            cy: 'Eich cyfeiriad llinell 2'
         }
     },
     {
@@ -50,8 +41,8 @@ module.exports = [
         type: 'text',
         required: true,
         label: {
-            'en': 'Town/city',
-            'cy': 'Eich tref/dinas'
+            en: 'Town/city',
+            cy: 'Eich tref/dinas'
         }
     },
     {
@@ -59,8 +50,8 @@ module.exports = [
         type: 'text',
         required: false,
         label: {
-            'en': 'County',
-            'cy': 'Eich sir'
+            en: 'County',
+            cy: 'Eich sir'
         }
     },
     {
@@ -68,35 +59,40 @@ module.exports = [
         type: 'text',
         required: true,
         label: {
-            'en': 'Postcode',
-            'cy': 'Eich côd post'
+            en: 'Postcode',
+            cy: 'Eich côd post'
         }
     },
     {
         name: 'yourProjectName',
         type: 'text',
-        required: true,
+        required: false,
         label: {
-            'en': 'Project name',
-            'cy': 'Enw eich prosiect'
-        }
-    },
-    {
-        name: 'yourProjectID',
-        type: 'text',
-        required: true,
-        label: {
-            'en': 'Project ID number',
-            'cy': 'Rhif adnabod eich prosiect'
+            en: 'Project name',
+            cy: 'Enw eich prosiect'
         }
     },
     {
         name: 'yourGrantAmount',
-        type: 'text',
+        type: 'select',
         required: true,
         label: {
-            'en': 'Grant amount',
-            'cy': 'Swm eich grant'
-        }
+            en: 'Grant amount',
+            cy: 'Swm eich grant'
+        },
+        options: [
+            {
+                name: 'Under £10,000',
+                value: 'under10k'
+            },
+            {
+                name: 'Over £10,000',
+                value: 'over10k'
+            },
+            {
+                name: "Don't know",
+                value: 'dunno'
+            }
+        ]
     }
 ];
