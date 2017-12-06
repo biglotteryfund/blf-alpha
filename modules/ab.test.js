d64fb2fa86d66f6a26bfb62c40ee94caeb59d117
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-12-06 13:10:10 +0000

    Improve ab helper tests

--- modules/ab.test.js
+++ modules/ab.test.js
@@ -6,24 +6,27 @@ const { splitPercentages } = require('./ab');
 
 describe('A/B Test Utilities', () => {
     describe('#splitPercentages', () => {
-        it('Should return split percentages from 0 to 1', () => {
-            const example1 = splitPercentages(50);
-            expect(example1.A).to.equal(0.5);
-            expect(example1.B).to.equal(0.5);
+        function assertValues(percentage, aVal, bVal) {
+            const example = splitPercentages(percentage);
+            expect(example.A).to.equal(aVal);
+            expect(example.B).to.equal(bVal);
+            expect(example.A + example.B).to.equal(1);
+        }
 
-            const example2 = splitPercentages(90);
-            expect(example2.A).to.equal(0.1);
-            expect(example2.B).to.equal(0.9);
+        it('should return split percentages from 0 to 1', () => {
+            assertValues(50, 0.5, 0.5);
+            assertValues(90, 0.1, 0.9);
         });
 
-        it('handles passing strings and floats', () => {
-            const example1 = splitPercentages('75');
-            expect(example1.A).to.equal(0.25);
-            expect(example1.B).to.equal(0.75);
+        it('should handle passing strings', () => {
+            assertValues('75', 0.25, 0.75);
+        });
 
-            const example2 = splitPercentages(33.33);
-            expect(example2.A).to.be.closeTo(0.667, 0.001);
-            expect(example2.B).to.be.closeTo(0.333, 0.001);
+        it('should handle passing floats', () => {
+            const floatExample = splitPercentages(33.33);
+            expect(floatExample.A).to.be.closeTo(0.667, 0.001);
+            expect(floatExample.B).to.be.closeTo(0.333, 0.001);
+            expect(floatExample.A + floatExample.B).to.equal(1);
         });
     });
 });
