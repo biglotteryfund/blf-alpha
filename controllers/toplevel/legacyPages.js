c5fc4288940db68acb7e6153b3922b1760fa9438
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-12-08 10:17:45 +0000

    Add welsh translation

--- controllers/toplevel/legacyPages.js
+++ controllers/toplevel/legacyPages.js
@@ -9,11 +9,53 @@ const { proxyLegacyPage, postToLegacyForm } = require('../../modules/proxy');
 const cached = require('../../middleware/cached');
 
 function initAwardsForAll(router) {
+    const awardsForAllRoutes = [
+        {
+            id: 'england',
+            lang: 'en',
+            path: legacyProxiedRoutes.awardsForAllEngland.path,
+            experimentId: 'O9FsbkKfSeOammAP0JrEyA',
+            replacements: {
+                applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=en',
+                contactPhone: '0345 4 10 20 30'
+            }
+        },
+        {
+            id: 'scotland',
+            lang: 'en',
+            path: legacyProxiedRoutes.awardsForAllScotland.path,
+            experimentId: 'EcAwbF34R5mbCaWW-y_rFQ',
+            replacements: {
+                applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=sc',
+                contactPhone: '0300 123 7110'
+            }
+        },
+        {
+            id: 'wales',
+            lang: 'en',
+            path: legacyProxiedRoutes.awardsForAllWales.path,
+            experimentId: 'Ko6MLYegQfaRO1rVU1UB3w',
+            replacements: {
+                applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=wales&ln=en',
+                contactPhone: '0300 123 0735'
+            }
+        },
+        {
+            id: 'wales-welsh',
+            lang: 'cy',
+            path: legacyProxiedRoutes.awardsForAllWalesWelsh.path,
+            experimentId: 'Ko6MLYegQfaRO1rVU1UB3w',
+            replacements: {
+                applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=wales&ln=welsh',
+                contactPhone: '0300 123 0735'
+            }
+        }
+    ];
 
     function getReplacementText(originalText, replacements) {
         return {
             A: `
-                <p>There are current two ways to apply, via PDF application form or via a trial online application form.</p>
+                <p>There are currently two ways to apply, via PDF application form or via a trial online application form.</p>
                 <h3>Applying via email or post</h3>
                 <p>The usual method for applying is via a PDF form that you email or post to us. To apply this way:</p>
                 ${originalText}
@@ -23,7 +65,7 @@ function initAwardsForAll(router) {
                 }">continue your application here</a>.</p>
             `,
             B: `
-                <p>There are current two ways to apply, via PDF application form or via online application form.
+                <p>There are currently two ways to apply, via PDF application form or via online application form.
                 </p>
                 <h3>Applying using the online form</h3>
                 <p>We are trialling a new application process where you can submit an application via a web form, rather than downloading a PDF.</p>
@@ -38,14 +80,42 @@ function initAwardsForAll(router) {
         };
     }
 
-    function modify(variant, replacements) {
+    function getReplacementTextWelsh(originalText, replacements) {
+        return {
+            A: `
+                <p>Ar hyn o bryd mae dau ddull ymgeisio, trwy ffurflen gais PDF neu drwy ffurflen gais ar-lein.</p>
+                <h3>Applying via email or post</h3>
+                <p>The usual method for applying is via a PDF form that you email or post to us. To apply this way:</p>
+                ${originalText}
+                <h3>Ymgeisio gan ddefnyddio’r ffurflen dreial ar-lein</h3>
+                <p>Rydym yn treialu proses ymgeisio newydd gyda nifer bach o ddefnyddwyr. Os ydych yn rhan o’r treial ar-lein <a href="${
+                    replacements.applyUrl
+                }">parhewch â’ch cais yma</a>.</p>
+            `,
+            B: `
+                <p>Ar hyn o bryd mae dau ddull ymgeisio, trwy ffurflen gais PDF neu drwy ffurflen gais ar-lein.</p>
+                <h3>Ymgeisio gan ddefnyddio’r ffurflen ar-lein</h3>
+                <p>Rydym yn treialu proses ymgeisio newydd sy’n galluogi chi i gyflwyno cais trwy ffurflen we’n hytrach na lawrlwytho PDF.</p>
+                <p><a class="roundedButton blueButton" style="display: inline-block;" href="${
+                    replacements.applyUrl
+                }">Cais Ar-lein</a></p>
+                <p>Mae’r ffurflen newydd hon yn dreial sy’n agored i nifer bach o ymgeiswyr yn unig. Byddem yn gwerthfawrogi’n fawr cael adborth gennych ar sut brofiad yr oedd.</p>
+                <h3>Applying via email or post</h3>
+                <p>You are also welcome to apply via email or post using our PDF form. To apply this way:</p>
+                ${originalText}
+            `
+        };
+    }
+
+    function modify(variant, route) {
         return function(req, res) {
             return proxyLegacyPage(req, res, dom => {
                 res.set('X-BLF-Legacy-Modified', true);
                 const TAB_SELECTOR = '#mainContentContainer .panel:last-of-type';
                 const applyTab = dom.window.document.querySelector(TAB_SELECTOR);
                 if (applyTab && applyTab.textContent.indexOf('please contact us at') !== -1) {
-                    const replacementText = getReplacementText(applyTab.innerHTML, replacements)[variant];
+                    const modifyFn = route.lang === 'cy' ? getReplacementTextWelsh : getReplacementText;
+                    const replacementText = modifyFn(applyTab.innerHTML, route.replacements)[variant];
                     applyTab.innerHTML = replacementText;
                 } else {
                     Raven.captureMessage('Failed to modify awards for all tab content', {
@@ -65,45 +135,6 @@ function initAwardsForAll(router) {
         };
     }
 
-    const awardsForAllRoutes = [
-        {
-            id: 'england',
-            path: legacyProxiedRoutes.awardsForAllEngland.path,
-            experimentId: 'O9FsbkKfSeOammAP0JrEyA',
-            replacements: {
-                applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=en',
-                contactPhone: '0345 4 10 20 30'
-            }
-        },
-        {
-            id: 'scotland',
-            path: legacyProxiedRoutes.awardsForAllScotland.path,
-            experimentId: 'EcAwbF34R5mbCaWW-y_rFQ',
-            replacements: {
-                applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=sc',
-                contactPhone: '0300 123 7110'
-            }
-        },
-        {
-            id: 'wales',
-            path: legacyProxiedRoutes.awardsForAllWales.path,
-            experimentId: 'Ko6MLYegQfaRO1rVU1UB3w',
-            replacements: {
-                applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=wales&ln=en',
-                contactPhone: '0300 123 0735'
-            }
-        },
-        {
-            id: 'wales-welsh',
-            path: legacyProxiedRoutes.awardsForAllWalesWelsh.path,
-            experimentId: 'Ko6MLYegQfaRO1rVU1UB3w',
-            replacements: {
-                applyUrl: 'https://apply.biglotteryfund.org.uk/?cn=wales&ln=welsh',
-                contactPhone: '0300 123 0735'
-            }
-        }
-    ];
-
     if (config.get('abTests.enabled')) {
         awardsForAllRoutes.forEach(route => {
             const testFn = ab.test(`blf-afa-rollout-${route.id}`, {
@@ -117,8 +148,8 @@ function initAwardsForAll(router) {
             const percentageForTest = config.get('abTests.tests.awardsForAll.percentage');
             const percentages = splitPercentages(percentageForTest);
 
-            router.get(route.path, cached.noCache, testFn(null, percentages.A), modify('A', route.replacements));
-            router.get(route.path, cached.noCache, testFn(null, percentages.B), modify('B', route.replacements));
+            router.get(route.path, cached.noCache, testFn(null, percentages.A), modify('A', route));
+            router.get(route.path, cached.noCache, testFn(null, percentages.B), modify('B', route));
             router.post(route.path, postToLegacyForm);
         });
     } else {
