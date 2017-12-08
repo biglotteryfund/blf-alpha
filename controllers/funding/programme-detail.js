a647acce20297be344089f8343c393b26f235491
David Rapson <david.rapson@biglotteryfund.org.uk>
2017-12-08 11:49:01 +0000

    Render tabs for programme-detail, support single section too

--- controllers/funding/programme-detail.js
+++ controllers/funding/programme-detail.js
@@ -1,18 +1,6 @@
-const { get } = require('lodash');
+const Raven = require('raven');
 const contentApi = require('../../modules/content-api');
 const { renderNotFound } = require('../http-errors');
-
-function mapContentSections(entry) {
-    const contentSectionsRaw = get(entry, 'contentSections', []);
-    return contentSectionsRaw.map((section, idx) => {
-        return {
-            id: `section-${idx}`,
-            title: section.title,
-            content: section.body,
-            active: idx === 0
-        };
-    });
-}
 
 function init({ router, config }) {
     router.get('/programmes/:slug', function(req, res) {
@@ -22,19 +10,22 @@ function init({ router, config }) {
                 slug: req.params.slug
             })
             .then(entry => {
-                res.render(config.template, {
-                    title: entry.title,
-                    contentSections: mapContentSections(entry)
-                });
+                if (entry.contentSections.length > 0) {
+                    res.render(config.template, {
+                        title: entry.title,
+                        entry: entry
+                    });
+                } else {
+                    throw new Error('NoContent');
+                }
             })
             .catch(err => {
-                console.log(err);
+                Raven.captureException(err);
                 renderNotFound(req, res);
             });
     });
 }
 
 module.exports = {
-    init,
-    mapContentSections
+    init
 };
