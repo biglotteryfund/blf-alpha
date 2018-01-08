06200f6b69a9d786bacc51656ce9d41c4ed072bc
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-01-08 09:37:51 +0000

    Redirect A4A landing pages to the Under £10k page (fixes #500)

--- controllers/routes.js
+++ controllers/routes.js
@@ -103,7 +103,11 @@ const routes = {
                     lang: 'toplevel.under10k',
                     code: 29,
                     static: true,
-                    live: true
+                    live: true,
+                    aliases: [
+                        sectionPaths.funding + '/Awards-For-All',
+                        sectionPaths.toplevel + '/awardsforall'
+                    ]
                 },
                 over10k: {
                     name: 'Over 10k',
