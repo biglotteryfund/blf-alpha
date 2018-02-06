9aba77e31aa808f44281a12fad9d6df1008ea476
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-02-06 14:22:18 +0000

    Extract sectionId middleware, load first

--- controllers/about/index.js
+++ controllers/about/index.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const routeStatic = require('../utils/routeStatic');
+const addSection = require('../../middleware/addSection');
 
 module.exports = (pages, sectionPath, sectionId) => {
+    router.use(addSection(sectionId));
+
     routeStatic.init({
         router: router,
         pages: pages,
--- controllers/research/index.js
+++ controllers/research/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routeStatic = require('../utils/routeStatic');
+const addSection = require('../../middleware/addSection');
 
 const router = express.Router();
 
 module.exports = (pages, sectionPath, sectionId) => {
+    router.use(addSection(sectionId));
+
     routeStatic.init({
         router,
         pages,
