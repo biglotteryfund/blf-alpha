0ba1834938d8bd61c17d416a7785346223e9a7cb
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-02-09 15:43:47 +0000

    Add another redirect

--- controllers/aliases.js
+++ controllers/aliases.js
@@ -60,6 +60,9 @@ const vanityRedirects = sections => {
         vanity('/a4aengland', '/funding/programmes/national-lottery-awards-for-all-england'),
         vanity('/prog_a4a_eng', '/funding/programmes/national-lottery-awards-for-all-england'),
         vanity('/awardsforallscotland', '/funding/programmes/national-lottery-awards-for-all-scotland'),
+        vanity(
+            '/england/global-content/programmes/scotland/awards-for-all-scotland', '/funding/programmes/national-lottery-awards-for-all-scotland'
+        ),
         vanity('/prog_a4a_ni', '/funding/programmes/awards-for-all-northern-ireland'),
         vanity('/a4awales', '/funding/programmes/national-lottery-awards-for-all-wales'),
         vanity('/prog_a4a_wales', '/funding/programmes/national-lottery-awards-for-all-wales'),
