394cd05cd7d99d9a5eca79bfb6eb070e7eafba92
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-02-19 12:07:59 +0000

    Add tests for preview mode data

---
+++ middleware/preview.test.js
@@ -0,0 +1,42 @@
+/* eslint-env mocha */
+const chai = require('chai');
+const expect = chai.expect;
+const httpMocks = require('node-mocks-http');
+
+const TEST_PREVIEW_DOMAIN = 'test.foo.com';
+process.env.PREVIEW_DOMAIN = TEST_PREVIEW_DOMAIN;
+const preview = require('./preview');
+
+describe('previews', () => {
+    describe('setPreviewData', () => {
+        it('should store preview data in a local variable on valid preview endpoints', () => {
+            const req = httpMocks.createRequest({
+                headers: {
+                    Host: TEST_PREVIEW_DOMAIN
+                },
+                query: {
+                    draft: 42
+                }
+            });
+            const res = httpMocks.createResponse();
+            preview(req, res, () => {});
+            expect(res.locals.PREVIEW_MODE.mode).to.equal('draft');
+            expect(res.locals.PREVIEW_MODE.id).to.equal(42);
+        });
+
+        it('should not add preview data to non-preview hosts', () => {
+            const req = httpMocks.createRequest({
+                headers: {
+                    Host: 'some.other.host'
+                },
+                query: {
+                    draft: 42
+                }
+            });
+            const res = httpMocks.createResponse();
+            preview(req, res, () => {});
+            expect(res.locals.PREVIEW_MODE).to.be.undefined;
+        });
+
+    });
+});
