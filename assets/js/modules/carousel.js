6a10bf289b75783e8bf634f78a29b475e0aa55f9
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-02-23 15:47:23 +0000

    Upgrade swiper to version 4

--- assets/js/modules/carousel.js
+++ assets/js/modules/carousel.js
@@ -1,33 +1,36 @@
 'use strict';
 const Swiper = require('swiper');
+const { forEach } = require('lodash');
 const { trackEvent } = require('../helpers/metrics');
 
 function init() {
-    const settings = {
-        selector: '.js-carousel',
-        nextSelector: '.js-carousel-next',
-        prevSelector: '.js-carousel-prev'
-    };
+    const carouselElems = document.querySelectorAll('.js-carousel');
+    if (carouselElems.length > 0) {
+        forEach(carouselElems, carouselElem => {
+            const dataName = carouselElem.getAttribute('data-name');
+            const nextEl = carouselElem.querySelector('.js-carousel-next');
+            const prevEl = carouselElem.querySelector('.js-carousel-prev');
 
-    const carouselElm = document.querySelector(settings.selector);
+            const carouselSwiper = new Swiper(carouselElem, {
+                navigation: {
+                    next: nextEl,
+                    prev: prevEl
+                },
+                speed: 300,
+                autoHeight: true,
+                a11y: true,
+                loop: true,
+                slidesPerView: 1
+            });
 
-    if (carouselElm) {
-        const dataName = carouselElm.getAttribute('data-name');
-        const carouselSwiper = new Swiper(settings.selector, {
-            nextButton: settings.nextSelector,
-            prevButton: settings.prevSelector,
-            speed: 400,
-            autoHeight: true,
-            a11y: true,
-            loop: true,
-            slidesPerView: 1
-        });
+            carouselSwiper.on('slideChangeEnd', function(swiperInstance) {
+                const idx = swiperInstance.realIndex + 1;
+                if (dataName) {
+                    trackEvent(dataName, 'Changed slide', 'Changed to item ' + idx);
+                }
+            });
 
-        carouselSwiper.on('slideChangeEnd', function(swiperInstance) {
-            const idx = swiperInstance.realIndex + 1;
-            if (dataName) {
-                trackEvent(dataName, 'Changed slide', 'Changed to item ' + idx);
-            }
+            carouselSwiper.init();
         });
     }
 }
