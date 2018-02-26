fff39522177b627deda95637c6bae4ffdc9338da
Matt Andrews <matt@mattandrews.info>
2018-02-26 10:59:42 +0000

    Log survey fetch errors

--- controllers/toplevel/index.js
+++ controllers/toplevel/index.js
@@ -6,6 +6,7 @@ const { sortBy } = require('lodash');
 const { body, validationResult } = require('express-validator/check');
 const xss = require('xss');
 const moment = require('moment');
+const Raven = require('raven');
 
 const router = express.Router();
 
@@ -105,7 +106,14 @@ module.exports = (pages, sectionPath, sectionId) => {
                     status: surveyToShow ? 'success' : 'error',
                     survey: surveyToShow
                 });
-            });
+            }).catch(err => {
+                Raven.captureMessage('Error retrieving surveys', {
+                    extra: err,
+                });
+                res.send({
+                    status: 'error'
+                });
+        });
     });
 
     const surveyValidations = [
