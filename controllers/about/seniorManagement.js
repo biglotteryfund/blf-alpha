9a81fb9809e72f25ac78c09713e33e3ef06f0d92
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-03-01 11:15:54 +0000

    Add better error handling

--- controllers/about/seniorManagement.js
+++ controllers/about/seniorManagement.js
@@ -4,18 +4,23 @@ const contentApi = require('../../services/content-api');
 
 function init({ router, routeConfig }) {
     router.get(routeConfig.path, sMaxAge(routeConfig.sMaxAge), (req, res, next) => {
+        const locale = req.i18n.getLocale();
+        const copy = req.i18n.__(routeConfig.lang);
         contentApi
             .getProfiles({
-                locale: req.i18n.getLocale(),
+                locale: locale,
                 section: 'seniorManagementTeam'
             })
             .then(profiles => {
-                const lang = req.i18n.__(routeConfig.lang);
-                res.render(routeConfig.template, {
-                    copy: lang,
-                    title: lang.title,
-                    profiles: profiles || []
-                });
+                if (profiles.length > 0) {
+                    res.render(routeConfig.template, {
+                        copy: copy,
+                        title: copy.title,
+                        profiles: profiles
+                    });
+                } else {
+                    throw new Error('NoProfiles');
+                }
             })
             .catch(err => {
                 Raven.captureException(err);
