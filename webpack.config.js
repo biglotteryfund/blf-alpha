79768b0afd022115436594a484fec24d65b0b602
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-03-02 09:19:30 +0000

    Use webpacks built in performance hints instead of size-limit

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
         path: path.resolve(__dirname, buildSummary.buildDir, 'javascripts'),
         publicPath: `${buildSummary.publicDir}/javascripts/`
     },
+    performance: {
+        hints: buildSummary.isProduction ? 'error' : 'warn'
+    },
     module: {
         rules: [
             {
