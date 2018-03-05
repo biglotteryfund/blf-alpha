34b61fb0109bd346fa0a23c34b3fc872d510e690
David Rapson <david@davidrapson.co.uk>
2018-03-05 09:54:30 +0000

    Revert "Client-side build upgrade"

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@
 /**
  * Initialise Vue
  */
-import Vue from 'vue';
+const Vue = require('vue');
+Vue.options.delimiters = ['<%', '%>'];
 window.Vue = Vue;
 
 /**
@@ -15,24 +16,31 @@ raven.init(Vue);
 /**
  * Load modules
  */
-import carousel from './modules/carousel';
-
 require('./modules/common').init();
 require('./modules/tabs').init();
 require('./modules/surveys').init();
+require('./modules/carousel').init();
 require('./modules/heroImages').init();
 require('./modules/logos').init();
 require('./modules/materials').init();
 require('./modules/forms').init();
-carousel.init();
+
+/**
+ * Load enhancements as a separate bundle
+ * Dark-launch until ready for launch
+ */
+// if (window.AppConfig.environment !== 'production') {
+//     import(/* webpackChunkName: "enhanced" */ './bootstraps/enhanced').then(enhanced => {
+//         enhanced.init();
+//     });
+// }
 
 /**
  * If we are in the live environment then load analytics
  * @see metaHeadJS.njk for where App.blockAnalytics is set
  */
-const analyticsSplit = () => import(/* webpackChunkName: "analytics" */ './bootstraps/analytics');
 if (!window.AppConfig.blockAnalytics) {
-    analyticsSplit().then(analytics => {
+    import(/* webpackChunkName: "analytics" */ './bootstraps/analytics').then(analytics => {
         analytics.init();
     });
 }
