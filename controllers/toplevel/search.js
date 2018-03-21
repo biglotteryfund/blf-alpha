1a46619f2b96e805054f808e6a302281c2a3d691
Matt Andrews <matt@mattandrews.info>
2018-03-21 11:30:19 +0000

    Log search terms in Raven to help debug mystery duplicate(?) queries

--- controllers/toplevel/search.js
+++ controllers/toplevel/search.js
@@ -1,4 +1,5 @@
 const querystring = require('querystring');
+const Raven = require('raven');
 const { noCache } = require('../../middleware/cached');
 const { customEvent } = require('../../modules/analytics');
 const { normaliseQuery } = require('../../modules/urls');
@@ -10,6 +11,16 @@ function init({ router, routeConfig }) {
 
         if (req.query.q) {
             customEvent('Search', 'Term', req.query.q);
+            // debug: send search logs to Sentry to work out where extra searches come from
+            Raven.captureMessage('Search term', {
+                level: 'info',
+                extra: {
+                    query: req.query.q,
+                },
+                tags: {
+                    feature: 'search'
+                }
+            });
             res.redirect(`${queryBase}+${querystring.escape(req.query.q)}`);
         } else {
             res.redirect('/');
