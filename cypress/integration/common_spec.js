581fb837e37ab2050f5b91b8c544c92c9a9d7700
David Rapson <david@davidrapson.co.uk>
2018-03-28 18:41:44 +0100

    Port some more integration tests

--- cypress/integration/common_spec.js
+++ cypress/integration/common_spec.js
@@ -1,10 +1,64 @@
 describe('Common tests', function() {
-    it('should render basic page', function() {
+    it('should render basic page', () => {
         cy.visit('/');
-
         const metaTitle = 'Home | Big Lottery Fund';
         cy.title().should('equal', metaTitle);
-        // cy.get('meta[name="title"]').should('equal', metaTitle);
-        // cy.get('meta[property="og:title"]').should('equal', metaTitle);
+        cy.get('meta[name="title"]').should('have.attr', 'content', metaTitle);
+        cy.get('meta[property="og:title"]').should('have.attr', 'content', metaTitle);
+    });
+
+    it('should mark correct section as selected', () => {
+        const sections = [
+            { urlPath: '/', activeSection: 'toplevel' },
+            { urlPath: '/funding', activeSection: 'funding' },
+            { urlPath: '/research', activeSection: 'research' },
+            { urlPath: '/about', activeSection: 'about' },
+            { urlPath: '/funding/programmes', activeSection: 'funding' }
+        ];
+
+        sections.forEach(section => {
+            cy.visit(section.urlPath);
+            cy.get(`.qa-nav-link--${section.activeSection}`).should('have.class', 'is-selected');
+        });
+    });
+
+    it('should serve welsh content', () => {
+        cy.request('/welsh').then(response => {
+            expect(response.status).to.eq(200);
+            expect(response.headers['content-language']).to.eq('cy');
+            cy.log(response.headers['content-language']);
+        });
+
+        cy.visit('/welsh');
+
+        const metaTitle = 'Hafan | Cronfa Loteri Fawr';
+        cy.title().should('equal', metaTitle);
+        cy.get('meta[name="title"]').should('have.attr', 'content', metaTitle);
+        cy.get('meta[property="og:title"]').should('have.attr', 'content', metaTitle);
+
+        const navLinksText = [];
+        cy
+            .get('.qa-global-nav .qa-nav-link a')
+            .each(el => {
+                navLinksText.push(el.text());
+            })
+            .then(() => {
+                expect(navLinksText).to.have.members(['Hafan', 'Ariannu', 'Ymchwil', 'Amdanom ni']);
+            });
+    });
+
+    it('should include correct language switcher for en locale', () => {
+        cy.visit('/funding/over10k');
+        cy.get('.qa-lang-switcher').should('have.attr', 'href', '/welsh/funding/over10k');
+    });
+
+    it('should include correct language switcher for cy locale', () => {
+        cy.visit('/welsh/funding/over10k');
+        cy.get('.qa-lang-switcher').should('have.attr', 'href', '/funding/over10k');
+    });
+
+    it('should serve a list of programmes', () => {
+        cy.visit('/funding/programmes');
+        cy.get('.qa-programme-card').should('have.length.greaterThan', 6);
     });
 });
--- integration-tests/test-suites/core.test.js
+++ integration-tests/test-suites/core.test.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const chai = require('chai');
-const { JSDOM } = require('jsdom');
-const { map } = require('lodash');
 const config = require('config');
 
 chai.use(require('chai-http'));
@@ -22,73 +20,6 @@ describe('Core sections and features', () => {
 
     after(() => helper.after(server));
 
-    it('should mark correct section as selected', () => {
-        function testSection({ urlPath, activeSection }) {
-            return chai
-                .request(server)
-                .get(urlPath)
-                .then(res => {
-                    expect(res.status).to.equal(200);
-
-                    const { document } = new JSDOM(res.text).window;
-                    const navItem = document.querySelector(`.qa-nav-link--${activeSection}`);
-
-                    // Test the correct navigation item is selected
-                    expect(navItem.classList.contains('is-selected')).to.be.true;
-                });
-        }
-
-        return Promise.all([
-            testSection({ urlPath: '/', activeSection: 'toplevel' }),
-            testSection({ urlPath: '/funding', activeSection: 'funding' }),
-            testSection({ urlPath: '/research', activeSection: 'research' }),
-            testSection({ urlPath: '/about', activeSection: 'about' }),
-            testSection({ urlPath: '/funding/programmes', activeSection: 'funding' })
-        ]);
-    });
-
-    it('should serve welsh content', () => {
-        return chai
-            .request(server)
-            .get('/welsh')
-            .then(res => {
-                expect(res).to.have.status(200);
-                expect(res).to.have.header('Content-Language', 'cy');
-
-                const metaTitle = `Hafan | Cronfa Loteri Fawr`;
-                expect(res.text).to.include(`<title>${metaTitle}</title>`);
-                expect(res.text).to.include(`<meta name="title" content="${metaTitle}">`);
-                expect(res.text).to.include(`<meta property="og:title" content="${metaTitle}">`);
-
-                const { document } = new JSDOM(res.text).window;
-                const navLinks = document.querySelectorAll('.qa-global-nav .qa-nav-link a');
-                const navLinksText = map(navLinks, el => el.text);
-                expect(navLinksText).to.have.members(['Hafan', 'Ariannu', 'Ymchwil', 'Amdanom ni']);
-            });
-    });
-
-    it('should include correct language switcher for en locale', () => {
-        return chai
-            .request(server)
-            .get('/funding/over10k')
-            .then(res => {
-                const { document } = new JSDOM(res.text).window;
-                const langSwitcherHref = document.querySelector('.qa-lang-switcher').href;
-                expect(langSwitcherHref).to.equal('/welsh/funding/over10k');
-            });
-    });
-
-    it('should include correct language switcher for cy locale', () => {
-        return chai
-            .request(server)
-            .get('/welsh/funding/over10k')
-            .then(res => {
-                const { document } = new JSDOM(res.text).window;
-                const langSwitcherHref = document.querySelector('.qa-lang-switcher').href;
-                expect(langSwitcherHref).to.equal('/funding/over10k');
-            });
-    });
-
     it('should allow contrast preferences to be set', () => {
         let redirectUrl = 'http://www.google.com';
         return chai
@@ -103,17 +34,6 @@ describe('Core sections and features', () => {
                 expect(res).to.have.cookie(config.get('cookies.contrast'));
                 expect(res).to.redirectTo(redirectUrl);
                 expect(res).to.have.status(302);
-            });
-    });
-
-    it('should serve a list of programmes', () => {
-        return chai
-            .request(server)
-            .get('/funding/programmes')
-            .then(res => {
-                const { document } = new JSDOM(res.text).window;
-                const programmes = document.querySelectorAll('.qa-programme-card');
-                expect(programmes.length).to.be.at.least(2);
             });
     });
 
