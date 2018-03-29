2cfe04bd7c1b4833de90dde4acad445343e26ce6
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-03-29 09:59:46 +0100

    Replace artificial tabs test with real interaction

--- cypress/integration/interactions_spec.js
+++ cypress/integration/interactions_spec.js
@@ -1,8 +1,35 @@
 describe('Interaction tests', function() {
-    it('should toggle mobile navigation', function() {
-        cy.viewport(320, 480);
+    it('interact with mobile navigation', () => {
+        cy.viewport(375, 667);
         cy.visit('/');
-        cy.get('#js-mobile-nav-toggle').click();
-        cy.get('#qa-offscreen-navigation').should('be.visible');
+
+        cy.get('#js-mobile-nav-toggle').as('navToggle');
+        cy.get('#qa-offscreen-navigation').as('nav');
+
+        cy.get('@navToggle').click();
+        cy.get('@nav').should('be.visible');
+
+        cy.wait(200);
+        cy.get('@navToggle').click();
+        cy.get('@nav').should('not.be.visible');
+    });
+
+    it('interact with tabs', () => {
+        cy.visit('/funding/programmes/national-lottery-awards-for-all-england');
+        cy.get('.js-tabset .js-tab').each($el => {
+            cy.log($el.attr('href'));
+
+            // Click the tab and check it is active
+            cy
+                .wrap($el)
+                .click()
+                .should('have.class', 'tab--active');
+
+            // Check there is only one tab active
+            cy.get('.js-tabset .tab--active').should('have.length', 1);
+
+            // Check tab content is visible
+            cy.get($el.attr('href')).should('be.visible');
+        });
     });
 });
