58c5d70a1e22987a3818bd159bfeddf795df4bc8
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-04-06 16:00:14 +0100

    Migrate tools test

---
+++ cypress/integration/5_tools_spec.js
@@ -0,0 +1,32 @@
+describe('CMS Tools', function() {
+    it('should block access to staff-only tools', () => {
+        cy.checkRedirect({
+            from: '/tools/survey-results',
+            to: '/user/login',
+            status: 302
+        });
+    });
+
+    it('should not allow unauthorised access to staff-only tools', () => {
+        cy
+            .loginUser({
+                username: 'bad@example.com',
+                password: 'notarealpassword'
+            })
+            .then(res => {
+                expect(res.body).to.contain('Your username and password combination is invalid');
+            });
+    });
+
+    it('should allow authorised access to staff-only tools', () => {
+        cy.seeduser().then(currentUser => {
+            cy.loginUser({
+                username: currentUser.username,
+                password: currentUser.password
+            });
+
+            cy.visit('/tools/survey-results');
+            cy.get('h1').should('contain', 'All survey results');
+        });
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,46 @@
 // https://on.cypress.io/custom-commands
 
+Cypress.Commands.add('getCsrf', () => {
+    return cy
+        .request('/user/login')
+        .its('body')
+        .then(body => {
+            const $html = Cypress.$(body);
+            const csrf = $html.find('input[name=_csrf]').val();
+            return csrf;
+        });
+});
+
+Cypress.Commands.add('registerUser', formBody => {
+    return cy.request({
+        method: 'POST',
+        url: '/user/register',
+        failOnStatusCode: false,
+        followRedirects: false,
+        form: true,
+        body: formBody
+    });
+});
+
+Cypress.Commands.add('seeduser', () => {
+    return cy.request('POST', '/tools/seed/user').its('body');
+});
+
+Cypress.Commands.add('loginUser', ({ username, password }) => {
+    return cy.getCsrf().then(csrfToken => {
+        return cy.request({
+            method: 'POST',
+            url: '/user/login',
+            form: true,
+            body: {
+                _csrf: csrfToken,
+                username: username,
+                password: password
+            }
+        });
+    });
+});
+
 Cypress.Commands.add('checkMetaTitles', expected => {
     cy.title().should('equal', expected);
     cy.get('meta[name="title"]').should('have.attr', 'content', expected);
