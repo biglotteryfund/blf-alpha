473a1152eecaecdd78f19e3977a9df6b47ed79f2
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-04-13 10:30:03 +0100

    Localise recent programmes on funding page

--- controllers/funding/funding.js
+++ controllers/funding/funding.js
@@ -1,12 +1,24 @@
-const { find } = require('lodash');
+const { find, get } = require('lodash');
 const { sMaxAge } = require('../../middleware/cached');
 const injectHeroImage = require('../../middleware/inject-hero');
 const contentApi = require('../../services/content-api');
 
 function init({ router, routeConfig }) {
     router.get(routeConfig.path, sMaxAge(routeConfig.sMaxAge), injectHeroImage(routeConfig), (req, res) => {
+        const lang = req.i18n.__(routeConfig.lang);
+
+        /**
+         * "Latest" programmes
+         * Hardcoded for now but we may want to fetch these dynamically.
+         */
+        function getLatestProgrammes(programmes) {
+            const findBySlug = slug => find(programmes, p => p.urlPath === `funding/programmes/${slug}`);
+            const programmeSlugs = get(lang, 'recentProgrammes', []);
+            const latestProgrammes = programmeSlugs.map(findBySlug);
+            return latestProgrammes;
+        }
+
         function renderLandingPage(programmes) {
-            const lang = req.i18n.__(routeConfig.lang);
             res.render(routeConfig.template, {
                 copy: lang,
                 title: lang.title,
@@ -19,17 +31,7 @@ function init({ router, routeConfig }) {
                 locale: req.i18n.getLocale()
             })
             .then(programmes => {
-                /**
-                 * "Latest" programmes
-                 * @todo Hardcoded for now but we may want to fetch these dynamically.
-                 */
-                const findBySlug = slug => find(programmes, p => p.urlPath === `funding/programmes/${slug}`);
-                const latestProgrammes = [
-                    findBySlug('reaching-communities-england'),
-                    findBySlug('national-lottery-awards-for-all-england'),
-                    findBySlug('empowering-young-people')
-                ];
-
+                const latestProgrammes = getLatestProgrammes(programmes);
                 renderLandingPage(latestProgrammes);
             })
             .catch(() => {
