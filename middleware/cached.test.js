a61581b9778e1898ac65a3494717b5efd86067ac
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-06-06 10:26:01 +0100

    Upgrade to Cypress 3.x
    Upgrades to Cypress 3.x. Moves some cache-header tests into Cypress as
    we were creating an instance of an express app within the unit tests
    where it's better to test in context of the real app. This also allows
    us to remove `chai-http`.

--- middleware/cached.test.js
+++ middleware/cached.test.js
@@ -1,128 +1,13 @@
 /* eslint-env mocha */
 'use strict';
 const chai = require('chai');
-chai.use(require('chai-http'));
 const expect = chai.expect;
 
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const http = require('http');
-const httpMocks = require('node-mocks-http');
-
-const { csrfProtection, defaultVary, defaultCacheControl, noCache, sMaxAge, toSeconds } = require('./cached');
-
-function createAppWithDefaults() {
-    const app = express();
-    app.use(defaultVary, defaultCacheControl);
-    return app;
-}
+const { toSeconds } = require('./cached');
 
 describe('cached', () => {
     it('should convert natural time to seconds', () => {
         expect(toSeconds('10s')).to.equal(10);
         expect(toSeconds('30m')).to.equal(1800);
-    });
-
-    it('should add default vary header', () => {
-        const req = httpMocks.createRequest();
-        const res = httpMocks.createResponse();
-
-        defaultVary(req, res, () => {});
-        expect(res.header('Vary')).to.equal('Cookie');
-    });
-
-    it('should add default cache-control header', done => {
-        const app = createAppWithDefaults();
-        app.get('/', function(req, res) {
-            res.send('Hello');
-        });
-        const server = http.createServer(app);
-
-        chai
-            .request(server)
-            .get('/')
-            .end((err, res) => {
-                expect(res.headers['cache-control']).to.equal('max-age=30,s-maxage=300');
-                done();
-            });
-
-        after(function(afterDone) {
-            server.close(afterDone);
-        });
-    });
-
-    it('should add no-cache headers when middleware is used', done => {
-        const app = createAppWithDefaults();
-        app.get('/', noCache, function(req, res) {
-            res.send('Hello');
-        });
-        const server = http.createServer(app);
-
-        chai
-            .request(server)
-            .get('/')
-            .end((err, res) => {
-                expect(res.headers['cache-control']).to.equal('no-store,no-cache,max-age=0');
-                done();
-            });
-
-        after(function(afterDone) {
-            server.close(afterDone);
-        });
-    });
-
-    it('should should add no-cache headers and csrf token at the same time', done => {
-        const app = createAppWithDefaults();
-        app.use(cookieParser());
-        app.use(
-            session({
-                secret: 'keyboard cat',
-                resave: false,
-                saveUninitialized: true,
-                cookie: { secure: true }
-            })
-        );
-
-        let csrf;
-        app.get('/', csrfProtection, function(req, res) {
-            csrf = req.csrfToken();
-            res.send(csrf);
-        });
-
-        const server = http.createServer(app);
-
-        chai
-            .request(server)
-            .get('/')
-            .end((err, res) => {
-                expect(res.text).to.equal(csrf);
-                expect(res.headers['cache-control']).to.equal('no-store,no-cache,max-age=0');
-                done();
-            });
-
-        after(function(afterDone) {
-            server.close(afterDone);
-        });
-    });
-
-    it('should add custom s-max-age', done => {
-        const app = createAppWithDefaults();
-        app.get('/', sMaxAge('30m'), function(req, res) {
-            res.send('Hello');
-        });
-        const server = http.createServer(app);
-
-        chai
-            .request(server)
-            .get('/')
-            .end((err, res) => {
-                expect(res.headers['cache-control']).to.equal('max-age=30,s-maxage=1800');
-                done();
-            });
-
-        after(function(afterDone) {
-            server.close(afterDone);
-        });
     });
 });
