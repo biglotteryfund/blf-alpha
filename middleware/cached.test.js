7b980657ceeff8bb858ff3248788592dd2c7fd27
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-06-06 17:01:55 +0100

    Convert middleware tests with jest-codemods

--- middleware/cached.test.js
+++ middleware/cached.test.js
@@ -1,13 +1,11 @@
-/* eslint-env mocha */
+/* eslint-env jest */
 'use strict';
-const chai = require('chai');
-const expect = chai.expect;
 
 const { toSeconds } = require('./cached');
 
 describe('cached', () => {
     it('should convert natural time to seconds', () => {
-        expect(toSeconds('10s')).to.equal(10);
-        expect(toSeconds('30m')).to.equal(1800);
+        expect(toSeconds('10s')).toBe(10);
+        expect(toSeconds('30m')).toBe(1800);
     });
 });
--- middleware/locals.test.js
+++ middleware/locals.test.js
@@ -1,12 +1,11 @@
+/* eslint-env jest */
 'use strict';
-/* eslint-env mocha */
-const chai = require('chai');
-const expect = chai.expect;
+
 const { getMetaTitle } = require('./locals');
 
 describe('locals middleware', () => {
     it('should return meta title for page', () => {
-        expect(getMetaTitle('Big Lottery Fund', 'Example')).to.equal('Example | Big Lottery Fund');
-        expect(getMetaTitle('Big Lottery Fund')).to.equal('Big Lottery Fund');
+        expect(getMetaTitle('Big Lottery Fund', 'Example')).toBe('Example | Big Lottery Fund');
+        expect(getMetaTitle('Big Lottery Fund')).toBe('Big Lottery Fund');
     });
 });
