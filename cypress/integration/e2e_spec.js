360ea408068fb8e7a9cb9e0d557e5d6a3e09eb39
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-06-11 11:31:15 +0100

    Clean up e2e tests

--- cypress/integration/e2e_spec.js
+++ cypress/integration/e2e_spec.js
@@ -1,9 +1,18 @@
 describe('e2e', function() {
-    it('should navigate from homepage to funding page', () => {
-        // Start on homepage
-        cy.viewport(375, 667);
+    it('should perform  common interactions', () => {
         cy.visit('/');
 
+        // ================================================
+        // Step: Cookie consent
+        // ================================================ //
+
+        cy.get('.cookie-consent button').click();
+
+        // ================================================
+        // Step: Mobile navigation
+        // ================================================ //
+
+        cy.viewport(375, 667);
         cy.get('#js-mobile-nav-toggle').as('navToggle');
         cy.get('#qa-offscreen-navigation').as('nav');
 
@@ -12,27 +21,93 @@ describe('e2e', function() {
 
         cy.get('@navToggle').click();
         cy.get('@nav').should('not.be.visible');
+        cy.viewport(1024, 768);
 
+        // ================================================
+        // Step: Micro-surveys
+        // ================================================ //
+
+        cy.get('.survey').as('survey');
+        cy
+            .get('@survey')
+            .find('button:first-child')
+            .click();
+        cy
+            .get('@survey')
+            .find('p')
+            .should('contain', 'Thank you');
+
+        // ================================================
+        // Step: Inline feedback
+        // ================================================ //
+
+        cy.visit('/funding/past-grants');
+        cy.get('#js-feedback').as('feedbackForm');
+        cy
+            .get('@feedbackForm')
+            .find('summary')
+            .click();
+        cy
+            .get('@feedbackForm')
+            .find('textarea')
+            .type('Test feedback');
+        cy
+            .get('@feedbackForm')
+            .find('form')
+            .submit();
+        cy.get('@feedbackForm').should('contain', 'Thank you for sharing');
+    });
+
+    it('should navigate from homepage to funding page', () => {
+        // ================================================
+        // Step: Homepage
+        // ================================================ //
+
+        cy.visit('/');
         cy.checkMetaTitles('Home | Big Lottery Fund');
-
         cy.checkActiveSection('toplevel');
 
-        // Navigate to over 10k page
+        // ================================================
+        // Step: Test language switcher
+        // ================================================ //
+
+        cy.get('.qa-global-nav .qa-lang-switcher').as('langSwitcher');
+        cy.get('@langSwitcher').click();
+        cy.checkMetaTitles('Hafan | Cronfa Loteri Fawr');
+        cy
+            .get('.qa-global-nav .qa-nav-link a')
+            .first()
+            .should('have.text', 'Hafan');
+        cy.get('@langSwitcher').click();
+
+        // ================================================
+        // Step:  Navigate to over 10k page
+        // ================================================ //
+
         cy.get('#qa-button-over10k').click();
         cy.checkActiveSection('funding');
 
-        // Navigate to funding programmes list for England
+        // ================================================
+        // Step: Navigate to funding programmes list
+        // ================================================ //
+
         cy.get('#qa-button-england').click();
         cy.checkActiveSection('funding');
 
-        // Navigate to funding programme
+        // ================================================
+        // Step: Navigate to funding programme
+        // ================================================ //
+
         cy
             .get('.qa-programme-card')
             .contains('Reaching Communities')
             .click();
+        cy.checkActiveSection('funding');
 
-        cy.viewport(1024, 768);
-        // Click the tab and check it is active
+        // ================================================
+        // Step: Interact with tabs
+        // ================================================ //
+
         cy.get('.js-tabset .js-tab').each($el => {
             cy
                 .wrap($el)
@@ -47,7 +122,7 @@ describe('e2e', function() {
         });
     });
 
-    it('basic application form', () => {
+    it('should submit a basic application form', () => {
         const submitSelector = '.js-application-form input[type="submit"]';
         cy.visit('/apply/your-idea');
 
@@ -122,23 +197,5 @@ describe('e2e', function() {
 
         // Confirm submission
         cy.get('h2').should('contain', 'Thank you for your order');
-    });
-
-    it('should allow feedback submissions', () => {
-        cy.visit('/funding/past-grants');
-        cy.get('#js-feedback').as('feedbackForm');
-        cy
-            .get('@feedbackForm')
-            .find('summary')
-            .click();
-        cy
-            .get('@feedbackForm')
-            .find('textarea')
-            .type('Test feedback');
-        cy
-            .get('@feedbackForm')
-            .find('form')
-            .submit();
-        cy.get('@feedbackForm').should('contain', 'Thank you for sharing');
     });
 });
