085ff07099ea60879a25358b76dfe44f0f2bd753
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-06-11 09:53:19 +0100

    Tidy up cookie consent code

--- assets/js/modules/cookie-consent.js
+++ assets/js/modules/cookie-consent.js
@@ -1,25 +1,23 @@
 import $ from 'jquery';
 import { storageAvailable } from '../helpers/storage';
 const { trackEvent } = require('../helpers/metrics');
-
-const CAN_STORE = storageAvailable('localStorage');
-const KEY_NAME = 'blf.cookie-consent';
+const canStore = storageAvailable('localStorage');
 
 function init() {
     const $cookieWarning = $('#js-cookie-consent');
     const $acceptButton = $('#js-cookie-consent-accept');
 
-    const hasAccepted = window.localStorage.getItem(KEY_NAME) === 'true';
+    const hasAccepted = canStore && window.localStorage.getItem('biglotteryfund:cookie-consent') === 'true';
 
-    if (CAN_STORE && hasAccepted === false) {
+    if (hasAccepted === false) {
         setTimeout(() => {
             $cookieWarning.prependTo('body').addClass('is-shown');
             $acceptButton.on('click', function() {
-                window.localStorage.setItem(KEY_NAME, 'true');
+                window.localStorage.setItem('biglotteryfund:cookie-consent', 'true');
                 trackEvent('Cookie Warning', 'Click', 'Accept');
                 $cookieWarning.removeClass('is-shown');
             });
-        }, 600);
+        }, 250);
     }
 }
 
