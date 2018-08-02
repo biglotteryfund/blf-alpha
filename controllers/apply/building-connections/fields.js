28f3fc0a4a22869936cea1c9763ef2351fa1f496
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-08-02 15:47:01 +0100

    Formatting fix

--- controllers/apply/building-connections/fields.js
+++ controllers/apply/building-connections/fields.js
@@ -33,7 +33,7 @@ const LENGTH_HINTS = {
 // via https://github.com/chriso/validator.js/blob/master/lib/isPostalCode.js#L54
 // we have to double-escape the regex patterns here to output it as a native RegExp
 // but also as a string for the HTML pattern attribute
-const POSTCODE_PATTERN = "(gir\\s?0aa|[a-zA-Z]{1,2}\\d[\\da-zA-Z]?\\s?(\\d[a-zA-Z]{2})?)";
+const POSTCODE_PATTERN = '(gir\\s?0aa|[a-zA-Z]{1,2}\\d[\\da-zA-Z]?\\s?(\\d[a-zA-Z]{2})?)';
 const POSTCODE_REGEX = new RegExp(POSTCODE_PATTERN, 'i');
 
 const currentWork = [
