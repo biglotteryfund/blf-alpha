ef2d1fa8f2a0a1b4393171053f0bb28d736826a1
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-09-03 12:30:03 +0100

    Update tests to use mock nodemailer transport

--- modules/__tests__/mail-helpers.test.js
+++ modules/__tests__/mail-helpers.test.js
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 'use strict';
+const nodemailer = require('nodemailer');
 const { getSendAddress, buildMailOptions } = require('../mail-helpers');
 
 describe('getSendAddress', () => {
@@ -20,70 +21,113 @@ describe('getSendAddress', () => {
     });
 });
 
-// function buildMailOptions({ subject, type = 'text', content, sendTo, sendMode = 'to', customSendFrom = null })
+function mockTransport(mailOptions) {
+    const transport = nodemailer.createTransport({ jsonTransport: true });
+    return transport.sendMail(mailOptions);
+}
 
 describe('buildMailOptions', () => {
-    it('should build mail options for a text email', () => {
-        expect(
-            buildMailOptions({
-                subject: 'Test',
-                sendTo: { address: 'example@example.com' },
-                content: 'This is a test'
-            })
-        ).toEqual({
+    it('should build mail options for a text email', async () => {
+        const mailOptions = buildMailOptions({
+            subject: 'Test',
+            sendTo: { address: 'example@example.com' },
+            content: 'This is a test'
+        });
+
+        const expectedOptions = {
             from: 'Big Lottery Fund <noreply@biglotteryfund.org.uk>',
             subject: 'Test',
             text: 'This is a test',
             to: { address: 'example@example.com' }
-        });
+        };
+
+        expect(mailOptions).toEqual(expectedOptions);
+
+        const info = await mockTransport(mailOptions);
+        const message = JSON.parse(info.message);
+
+        expect(message.text).toBe(expectedOptions.text);
+        expect(message.to).toEqual([{ address: 'example@example.com', name: '' }]);
+        expect(info.envelope.from).toBe('noreply@biglotteryfund.org.uk');
+        expect(info.envelope.to).toEqual(['example@example.com']);
     });
 
-    it('should build mail options for a text email with a name', () => {
-        expect(
-            buildMailOptions({
-                subject: 'Test',
-                sendTo: { name: 'Example Person', address: 'example@example.com' },
-                content: 'This is a test'
-            })
-        ).toEqual({
+    it('should build mail options for a text email with a name', async () => {
+        const mailOptions = buildMailOptions({
+            subject: 'Test',
+            sendTo: { name: 'Example Person', address: 'example@example.com' },
+            content: 'This is a test'
+        });
+
+        const expectedOptions = {
             from: 'Big Lottery Fund <noreply@biglotteryfund.org.uk>',
             subject: 'Test',
             text: 'This is a test',
             to: { name: 'Example Person', address: 'example@example.com' }
-        });
+        };
+
+        expect(mailOptions).toEqual(expectedOptions);
+
+        const info = await mockTransport(mailOptions);
+        const message = JSON.parse(info.message);
+
+        expect(message.text).toBe(expectedOptions.text);
+        expect(message.to).toEqual([{ address: 'example@example.com', name: 'Example Person' }]);
+        expect(info.envelope.from).toBe('noreply@biglotteryfund.org.uk');
+        expect(info.envelope.to).toEqual(['example@example.com']);
     });
 
-    it('should build mail options for a html email', () => {
-        expect(
-            buildMailOptions({
-                subject: 'Test',
-                sendTo: { address: 'example@example.com' },
-                type: 'html',
-                content: '<p>This is a test</p>'
-            })
-        ).toEqual({
+    it('should build mail options for a html email', async () => {
+        const mailOptions = buildMailOptions({
+            subject: 'Test',
+            sendTo: { address: 'example@example.com' },
+            type: 'html',
+            content: '<p>This is a test</p>'
+        });
+
+        const expectedOptions = {
             from: 'Big Lottery Fund <noreply@biglotteryfund.org.uk>',
             subject: 'Test',
             html: '<p>This is a test</p>',
             text: 'This is a test',
             to: { address: 'example@example.com' }
-        });
+        };
+
+        expect(mailOptions).toEqual(expectedOptions);
+
+        const info = await mockTransport(mailOptions);
+        const message = JSON.parse(info.message);
+
+        expect(message.text).toBe(expectedOptions.text);
+        expect(message.to).toEqual([{ address: 'example@example.com', name: '' }]);
+        expect(info.envelope.from).toBe('noreply@biglotteryfund.org.uk');
+        expect(info.envelope.to).toEqual(['example@example.com']);
     });
 
-    it('should build mail options for an internal html email', () => {
-        expect(
-            buildMailOptions({
-                subject: 'Test',
-                sendTo: { address: 'example@biglotteryfund.org.uk' },
-                type: 'html',
-                content: '<p>This is a test</p>'
-            })
-        ).toEqual({
+    it('should build mail options for an internal html email', async () => {
+        const mailOptions = buildMailOptions({
+            subject: 'Test',
+            sendTo: { address: 'example@biglotteryfund.org.uk' },
+            type: 'html',
+            content: '<p>This is a test</p>'
+        });
+
+        const expectedOptions = {
             from: 'Big Lottery Fund <noreply@blf.digital>',
             subject: 'Test',
             html: '<p>This is a test</p>',
             text: 'This is a test',
             to: { address: 'example@biglotteryfund.org.uk' }
-        });
+        };
+
+        expect(mailOptions).toEqual(expectedOptions);
+
+        const info = await mockTransport(mailOptions);
+        const message = JSON.parse(info.message);
+
+        expect(message.text).toBe(expectedOptions.text);
+        expect(message.to).toEqual([{ address: 'example@biglotteryfund.org.uk', name: '' }]);
+        expect(info.envelope.from).toBe('noreply@blf.digital');
+        expect(info.envelope.to).toEqual(['example@biglotteryfund.org.uk']);
     });
 });
