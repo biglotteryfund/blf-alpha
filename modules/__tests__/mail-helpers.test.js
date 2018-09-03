e18a37602fff2aedd91adc073fd280f2a999da57
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-09-03 12:00:17 +0100

    Add tests for buildMailOptions

--- modules/__tests__/mail-helpers.test.js
+++ modules/__tests__/mail-helpers.test.js
@@ -1,8 +1,8 @@
 /* eslint-env jest */
 'use strict';
-const { getSendAddress } = require('../mail-helpers');
+const { getSendAddress, buildMailOptions } = require('../mail-helpers');
 
-describe('mail', () => {
+describe('getSendAddress', () => {
     const expectedDefault = `noreply@biglotteryfund.org.uk`;
     const expectedInternal = `noreply@blf.digital`;
 
@@ -17,5 +17,73 @@ describe('mail', () => {
         expect(getSendAddress('example@biggerlotteryfund.org.uk')).toBe(expectedDefault);
         expect(getSendAddress('example@biggestlotteryfund.org.uk')).toBe(expectedDefault);
         expect(getSendAddress('biglotteryfund.org.uk@example.com')).toBe(expectedDefault);
+    });
+});
+
+// function buildMailOptions({ subject, type = 'text', content, sendTo, sendMode = 'to', customSendFrom = null })
+
+describe('buildMailOptions', () => {
+    it('should build mail options for a text email', () => {
+        expect(
+            buildMailOptions({
+                subject: 'Test',
+                sendTo: { address: 'example@example.com' },
+                content: 'This is a test'
+            })
+        ).toEqual({
+            from: 'Big Lottery Fund <noreply@biglotteryfund.org.uk>',
+            subject: 'Test',
+            text: 'This is a test',
+            to: { address: 'example@example.com' }
+        });
+    });
+
+    it('should build mail options for a text email with a name', () => {
+        expect(
+            buildMailOptions({
+                subject: 'Test',
+                sendTo: { name: 'Example Person', address: 'example@example.com' },
+                content: 'This is a test'
+            })
+        ).toEqual({
+            from: 'Big Lottery Fund <noreply@biglotteryfund.org.uk>',
+            subject: 'Test',
+            text: 'This is a test',
+            to: { name: 'Example Person', address: 'example@example.com' }
+        });
+    });
+
+    it('should build mail options for a html email', () => {
+        expect(
+            buildMailOptions({
+                subject: 'Test',
+                sendTo: { address: 'example@example.com' },
+                type: 'html',
+                content: '<p>This is a test</p>'
+            })
+        ).toEqual({
+            from: 'Big Lottery Fund <noreply@biglotteryfund.org.uk>',
+            subject: 'Test',
+            html: '<p>This is a test</p>',
+            text: 'This is a test',
+            to: { address: 'example@example.com' }
+        });
+    });
+
+    it('should build mail options for an internal html email', () => {
+        expect(
+            buildMailOptions({
+                subject: 'Test',
+                sendTo: { address: 'example@biglotteryfund.org.uk' },
+                type: 'html',
+                content: '<p>This is a test</p>'
+            })
+        ).toEqual({
+            from: 'Big Lottery Fund <noreply@blf.digital>',
+            subject: 'Test',
+            html: '<p>This is a test</p>',
+            text: 'This is a test',
+            to: { address: 'example@biglotteryfund.org.uk' }
+        });
     });
 });
