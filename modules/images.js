97c73cb625853ccb04f98fd2df6dbf45f04cc3e4
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-09-05 12:49:41 +0100

    Remove remaining use of assets, inline image path

--- modules/images.js
+++ modules/images.js
@@ -1,43 +1,22 @@
 'use strict';
-const { has } = require('lodash');
-const assets = require('./assets');
-
-function createHeroImage(opts) {
-    if (!['small', 'medium', 'large'].every(x => has(opts, x))) {
-        throw new Error('Must pass a small, medium, and large image');
-    }
-
-    if (!has(opts, 'default')) {
-        throw new Error('Must define a default image with opts.default');
-    }
-
-    return {
-        small: assets.getImagePath(opts.small),
-        medium: assets.getImagePath(opts.medium),
-        large: assets.getImagePath(opts.large),
-        default: assets.getImagePath(opts.default),
-        caption: opts.caption || ''
-    };
-}
 
 const heroImages = {
-    fallbackHeroImage: createHeroImage({
-        small: 'hero/hero-fallback-small.jpg',
-        medium: 'hero/hero-fallback-medium.jpg',
-        large: 'hero/hero-fallback-large.jpg',
-        default: 'hero/hero-fallback-medium.jpg',
+    fallbackHeroImage: {
+        small: '/assets/images/hero/hero-fallback-small.jpg',
+        medium: '/assets/images/hero/hero-fallback-medium.jpg',
+        large: '/assets/images/hero/hero-fallback-large.jpg',
+        default: '/assets/images/hero/hero-fallback-medium.jpg',
         caption: 'Rathlin Island Development and Community Association'
-    }),
-    fallbackSuperheroImage: createHeroImage({
-        small: 'hero/superhero-fallback-small.jpg',
-        medium: 'hero/superhero-fallback-medium.jpg',
-        large: 'hero/superhero-fallback-large.jpg',
+    },
+    fallbackSuperheroImage: {
+        small: '/assets/images/hero/superhero-fallback-small.jpg',
+        medium: '/assets/images/hero/superhero-fallback-medium.jpg',
+        large: '/assets/images/hero/superhero-fallback-large.jpg',
         default: 'hero/superhero-fallback-medium.jpg',
         caption: 'Stepping Stones Programme, Grant £405,270'
-    })
+    }
 };
 
 module.exports = {
-    heroImages,
-    createHeroImage
+    heroImages
 };
