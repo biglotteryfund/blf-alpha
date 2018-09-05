e2ba4212d1de4f40604b20827984834d93f3fa50
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-09-05 16:50:44 +0100

    Inline fallbackSuperheroImage

--- modules/images.js
+++ modules/images.js
@@ -7,13 +7,6 @@ const heroImages = {
         large: '/assets/images/hero/hero-fallback-large.jpg',
         default: '/assets/images/hero/hero-fallback-medium.jpg',
         caption: 'Rathlin Island Development and Community Association'
-    },
-    fallbackSuperheroImage: {
-        small: '/assets/images/hero/superhero-fallback-small.jpg',
-        medium: '/assets/images/hero/superhero-fallback-medium.jpg',
-        large: '/assets/images/hero/superhero-fallback-large.jpg',
-        default: '/assets/images/hero/superhero-fallback-medium.jpg',
-        caption: 'Stepping Stones Programme, Grant £405,270'
     }
 };
 
