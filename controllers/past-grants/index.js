9093212740516b7487ccbe1c1dccc4a93b9ee311
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-09-20 11:35:59 +0100

    Refine past grants search results

--- controllers/past-grants/index.js
+++ controllers/past-grants/index.js
@@ -3,49 +3,42 @@ const path = require('path');
 const express = require('express');
 const request = require('request-promise-native');
 const querystring = require('querystring');
-const { head, sortBy } = require('lodash');
+const { pick, isEmpty } = require('lodash');
 
 const { PAST_GRANTS_API_URI } = require('../../modules/secrets');
 
 const router = express.Router();
 
-function buildPagination(paginationMeta, currentQuerystring = {}) {
+/**
+ * Combine a ?page param with existing querystrings for grant search
+ * @param {object} currentQuery
+ * @param {number} page
+ */
+function makePageLink(currentQuery, page) {
+    const withPage = Object.assign({}, currentQuery, { page });
+    return '?' + querystring.stringify(withPage);
+}
+
+function buildPagination(paginationMeta, currentQuery = {}) {
     if (paginationMeta && paginationMeta.totalPages > 1) {
         const currentPage = paginationMeta.currentPage;
         const totalPages = paginationMeta.totalPages;
 
-        // combine a ?page param with existing querystrings for grant search
-        const makePageLink = pageNum => {
-            return (
-                '?' +
-                querystring.stringify(
-                    Object.assign({}, currentQuerystring, {
-                        page: pageNum
-                    })
-                )
-            );
+        const prevLink = {
+            url: makePageLink(currentQuery, currentPage - 1),
+            label: 'Previous page'
         };
 
-        const prevLink = makePageLink(currentPage - 1);
-        const nextLink = makePageLink(currentPage + 1);
+        const nextLink = {
+            url: makePageLink(currentQuery, currentPage + 1),
+            label: 'Next page'
+        };
 
         return {
             currentPage: currentPage,
             totalPages: totalPages,
-            prevLink:
-                currentPage > 1
-                    ? {
-                          url: prevLink,
-                          label: 'prev'
-                      }
-                    : null,
-            nextLink:
-                currentPage < totalPages
-                    ? {
-                          url: nextLink,
-                          label: 'next'
-                      }
-                    : null
+            prevLink: currentPage > 1 ? prevLink : null,
+            nextLink: currentPage < totalPages ? nextLink : null
         };
     } else {
         return;
@@ -53,39 +46,26 @@ function buildPagination(paginationMeta, currentQuerystring = {}) {
 }
 
 router.get('/', async (req, res) => {
-    const query = {
-        page: req.query.page || 1
-    };
-
-    if (req.query.q) {
-        query.q = req.query.q;
-    }
-
-    if (req.query.postcode) {
-        query.postcode = req.query.postcode;
-    }
-
-    if (req.query.programme) {
-        query.programme = req.query.programme;
-    }
-
-    if (req.query.orgType) {
-        query.orgType = req.query.orgType;
-    }
+    /**
+     * Pick out an allowed list of query parameters for forward on to the grants API
+     * @type {object}
+     */
+    const facetParams = pick(req.query, ['q', 'postcode', 'programme', 'year', 'orgType']);
+    const queryWithPage = Object.assign({}, facetParams, { page: req.query.page || 1 });
 
     const data = await request({
         url: PAST_GRANTS_API_URI,
         json: true,
-        qs: query
+        qs: queryWithPage
     });
-    
+
     res.render(path.resolve(__dirname, './views/past-grants'), {
         title: 'Past grants search',
-        queryParams: req.query,
+        queryParams: isEmpty(facetParams) ? false : facetParams,
         grants: data.results,
         facets: data.facets,
         meta: data.meta,
-        pagination: buildPagination(data.meta.pagination, req.query)
+        pagination: buildPagination(data.meta.pagination, queryWithPage)
     });
 });
 
