44d5a3e1935133c2ffb38607d26955b24c714d69
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-09-27 12:05:17 +0100

    Clean up templates

--- controllers/digital-funding/strand-1.js
+++ controllers/digital-funding/strand-1.js
@@ -4,22 +4,21 @@ const express = require('express');
 const { concat } = require('lodash');
 const router = express.Router();
 
-const { injectCopy } = require('../../middleware/inject-content');
-
-const strandTitle = 'Using digital to change your business';
-
 router.get('/', (req, res) => {
     res.render(path.resolve(__dirname, './views/strand-1'), {
-        title: strandTitle,
-        breadcrumbs: concat(res.locals.breadcrumbs, [{ label: strandTitle }])
+        title: res.locals.copy.strand1.title,
+        breadcrumbs: concat(res.locals.breadcrumbs, [{ label: res.locals.copy.strand1.title }])
     });
 });
 
-router.get('/eligibility', injectCopy('funding.digitalFunding.strand1.eligibilityDetail'), (req, res) => {
-    const title = res.locals.copy.title;
+router.get('/eligibility', (req, res) => {
+    const title = res.locals.copy.strand1.eligibilityDetail.title;
     res.render(path.resolve(__dirname, './views/strand-1-eligibility'), {
         title: title,
-        breadcrumbs: concat(res.locals.breadcrumbs, [{ label: strandTitle, url: './' }, { label: title }])
+        breadcrumbs: concat(res.locals.breadcrumbs, [
+            { label: res.locals.copy.strand1.title, url: './' },
+            { label: title }
+        ])
     });
 });
 
@@ -27,7 +26,10 @@ router.get('/eligibility/ineligible', (req, res) => {
     const title = 'Sorry, you’re ineligible';
     res.render(path.resolve(__dirname, './views/strand-1-ineligible'), {
         title: title,
-        breadcrumbs: concat(res.locals.breadcrumbs, [{ label: strandTitle, url: './' }, { label: 'Ineligible' }])
+        breadcrumbs: concat(res.locals.breadcrumbs, [
+            { label: res.locals.copy.strand1.title, url: './' },
+            { label: 'Ineligible' }
+        ])
     });
 });
 
