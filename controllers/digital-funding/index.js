c8123c417d79d9ea7a55cdd4d1c4e6392aff2d10
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-09-27 10:05:19 +0100

    Update landing page, add copy to yml file

--- controllers/digital-funding/index.js
+++ controllers/digital-funding/index.js
@@ -2,36 +2,27 @@
 const express = require('express');
 const path = require('path');
 const { concat } = require('lodash');
-const { injectBreadcrumbs } = require('../../middleware/inject-content');
+const { injectBreadcrumbs, injectCopy, injectHeroImage } = require('../../middleware/inject-content');
 
 const router = express.Router();
 
-router.use(injectBreadcrumbs, (req, res, next) => {
-    res.locals.isBilingual = false;
-    res.locals.heroImage = {
-        small: '/assets/images/hero/blank-small.jpg',
-        medium: '/assets/images/hero/blank-medium.jpg',
-        large: '/assets/images/hero/blank-large.jpg',
-        default: '/assets/images/hero/blank-medium.jpg'
-    };
-
-    res.locals.breadcrumbs = concat(res.locals.breadcrumbs, [
-        {
-            label: req.i18n.__('funding.programmes.title'),
-            url: '/funding/programmes'
-        },
-        {
-            label: 'Digital Funding',
-            url: req.baseUrl
-        }
-    ]);
-    next();
-});
+router.use(
+    injectHeroImage('digital-buddies-2'),
+    injectBreadcrumbs,
+    injectCopy('funding.digitalFunding'),
+    (req, res, next) => {
+        res.locals.breadcrumbs = concat(res.locals.breadcrumbs, [
+            {
+                label: res.locals.title,
+                url: req.baseUrl
+            }
+        ]);
+        next();
+    }
+);
 
 router.get('/', (req, res) => {
-    res.render(path.resolve(__dirname, './views/landing'), {
-        title: 'Digital Funding'
-    });
+    res.render(path.resolve(__dirname, './views/landing'));
 });
 
 router.get('/eligibility', (req, res) => {
