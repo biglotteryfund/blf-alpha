abefcaafa2555b511e9e31a612f9ba254bb1d1b9
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-09-28 11:31:59 +0100

    Share ineligible template

--- controllers/digital-funding/strand-1.js
+++ controllers/digital-funding/strand-1.js
@@ -4,6 +4,11 @@ const express = require('express');
 const { concat } = require('lodash');
 const router = express.Router();
 
+router.use((req, res, next) => {
+    res.locals.pageAccent = 'blue';
+    next();
+});
+
 router.get('/', (req, res) => {
     res.render(path.resolve(__dirname, './views/strand-1'), {
         title: res.locals.copy.strand1.title,
@@ -23,9 +28,10 @@ router.get('/eligibility', (req, res) => {
 });
 
 router.get('/eligibility/ineligible', (req, res) => {
-    const title = res.locals.copy.strand1.ineligible.title;
-    res.render(path.resolve(__dirname, './views/strand-1-ineligible'), {
+    const title = res.locals.copy.ineligible.title;
+    res.render(path.resolve(__dirname, './views/ineligible'), {
         title: title,
+        bodyCopy: res.locals.copy.ineligible.strand1,
         breadcrumbs: concat(res.locals.breadcrumbs, [
             { label: res.locals.copy.strand1.shortTitle, url: './' },
             { label: title }
--- controllers/digital-funding/strand-2.js
+++ controllers/digital-funding/strand-2.js
@@ -5,6 +5,11 @@ const { concat } = require('lodash');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    res.locals.pageAccent = 'cyan';
+    next();
+});
+
 router.get('/', (req, res) => {
     res.render(path.resolve(__dirname, './views/strand-2'), {
         title: res.locals.copy.strand2.title,
@@ -24,9 +29,10 @@ router.get('/eligibility', (req, res) => {
 });
 
 router.get('/eligibility/ineligible', (req, res) => {
-    const title = res.locals.copy.strand2.ineligible.title;
-    res.render(path.resolve(__dirname, './views/strand-2-ineligible'), {
+    const title = res.locals.copy.ineligible.title;
+    res.render(path.resolve(__dirname, './views/ineligible'), {
         title: title,
+        bodyCopy: res.locals.copy.ineligible.strand2,
         breadcrumbs: concat(res.locals.breadcrumbs, [
             { label: res.locals.copy.strand2.shortTitle, url: './' },
             { label: title }
