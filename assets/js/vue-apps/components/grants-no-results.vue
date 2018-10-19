92492da4cb77a98ffa1e768ab88884672e35a517
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-10-19 12:03:15 +0100

    Fix typo and allow clear filter button to work

--- assets/js/vue-apps/components/grants-no-results.vue
+++ assets/js/vue-apps/components/grants-no-results.vue
@@ -1,12 +1,17 @@
-<template functional>
-    <div class="grants-no-results s-prose" v-if="props.totalResults === 0">
+<script>
+    export default {
+        props: ['totalResults', 'clearFilters']
+    };
+</script>
+<template>
+    <div class="grants-no-results s-prose" v-if="totalResults === 0">
         <h3>We're sorry, we couldn't find any results matching your criteria</h3>
 
         <ul>
             <li>Try a different query or clear your filters</li>
             <li>Try searching something less specific — sometimes using a more general search term may help</li>
             <li>Double check the spelling of any keywords you are searching for</li>
-            <li>Our database might not have the details for granst awarded under 1 month ago. Please check our <a href="/news-and-events">news section</a> if you are looking for a recently awarded grant</li>
+            <li>Our database might not have the details for grants awarded under 1 month ago. Please check our <a href="/news-and-events">news section</a> if you are looking for a recently awarded grant</li>
         </ul>
 
         <p><button type="button" class="btn btn--medium"
@@ -14,6 +19,6 @@
             Clear all filters
         </button></p>
 
-        <p>If you're looking for something specific and would like some help <a href="/contact">please contact us</a></p>
+        <p>If you're looking for something specific and would like some help <a href="/contact">please contact us</a>.</p>
     </div>
 </template>
