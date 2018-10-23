fee5024b19a7ddcd3bb1c3a611af03f61784a501
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-10-23 11:47:03 +0100

    Improve logging for newsletter service

--- services/newsletter-service.js
+++ services/newsletter-service.js
@@ -12,34 +12,44 @@ const { DOTMAILER_API } = require('../modules/secrets');
  * @param {object} options
  * @param {string} options.addressBookId
  * @param {object} options.subscriptionData
+ * @see https://developer.dotmailer.com/docs
+ * @see https://developer.dotmailer.com/docs/error-response-types
  */
 function subscribe({ addressBookId, subscriptionData }) {
     const ENDPOINT = `${config.get('ebulletinApiEndpoint')}/address-books/${addressBookId}/contacts`;
-    return request({
-        uri: ENDPOINT,
-        method: 'POST',
-        auth: {
-            user: DOTMAILER_API.user,
-            pass: DOTMAILER_API.password,
-            sendImmediately: true
-        },
-        json: true,
-        body: subscriptionData,
-        resolveWithFullResponse: true
-    }).then(response => {
-        if (response.statusCode === 200) {
-            debug(`Subscribed to address book: ${addressBookId}`);
-            metrics.count({
-                name: addressBookId,
-                namespace: 'SITE/NEWSLETTER',
-                dimension: 'SUBSCRIBED',
-                value: 'SUBSCRIBED_COUNT'
+
+    return new Promise((resolve, reject) => {
+        request({
+            uri: ENDPOINT,
+            method: 'POST',
+            auth: {
+                user: DOTMAILER_API.user,
+                pass: DOTMAILER_API.password,
+                sendImmediately: true
+            },
+            json: true,
+            body: subscriptionData,
+            resolveWithFullResponse: true
+        })
+            .then(response => {
+                if (response.statusCode === 200) {
+                    debug(`Subscribed to address book: ${addressBookId}`);
+                    metrics.count({
+                        name: addressBookId,
+                        namespace: 'SITE/NEWSLETTER',
+                        dimension: 'SUBSCRIBED',
+                        value: 'SUBSCRIBED_COUNT'
+                    });
+                    resolve(response);
+                } else {
+                    raven.captureMessage(response.message);
+                    reject(new Error(response.message));
+                }
+            })
+            .catch(error => {
+                raven.captureMessage(error.message);
+                reject(new Error(error.message));
             });
-            return response;
-        } else {
-            raven.captureMessage(response.message);
-            throw new Error(response.message);
-        }
     });
 }
 
