cecfb8e1d6aa71989f1032c4418d05c27e9b65d0
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-10-30 10:49:19 +0000

    Tidyup

--- assets/js/vue-apps/components/grants-filter-summary.vue
+++ assets/js/vue-apps/components/grants-filter-summary.vue
@@ -7,20 +7,18 @@ export default {
 </script>
 
 <template>
-    <div>
-        <ul class="filter-list">
-            <li v-for="item in filterSummary"
-                :key="item.name"
-                @click="$emit('clear-filters', item.name)"
-                class="filter-list__item">
-                {{ item.label }}
-                <IconClose id="prompt-close" description="Dismiss prompt" />
-            </li>
-            <li v-if="filterSummary.length > 0">
-                <button type="button" class="btn-link" @click="$emit('clear-filters')">
-                    {{ clearLabel }}
-                </button>
-            </li>
-        </ul>
-    </div>
+    <ul class="filter-list">
+        <li v-for="item in filterSummary"
+            :key="item.name"
+            @click="$emit('clear-filters', item.name)"
+            class="filter-list__item">
+            {{ item.label }}
+            <IconClose id="prompt-close" description="Dismiss prompt" />
+        </li>
+        <li v-if="filterSummary.length > 0">
+            <button type="button" class="btn-link" @click="$emit('clear-filters')">
+                {{ clearLabel }}
+            </button>
+        </li>
+    </ul>
 </template>
