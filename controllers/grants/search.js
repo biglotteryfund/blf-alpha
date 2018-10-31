17c41d2469dfb030aed202977239894efe1cccac
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-10-31 10:23:53 +0000

    Rename a bunch of URLs to move past grants search to /funding/grants and merge the landing/search page

--- controllers/grants/search.js
+++ controllers/grants/search.js
@@ -1,16 +1,21 @@
 'use strict';
 const path = require('path');
 const express = require('express');
+const config = require('config');
 const router = express.Router();
 const request = require('request-promise-native');
 const querystring = require('querystring');
-const { concat, pick, isEmpty, get, head } = require('lodash');
+const { concat, pick, isEmpty, get, head, sampleSize } = require('lodash');
 const nunjucks = require('nunjucks');
 const Raven = require('raven');
 
 const { PAST_GRANTS_API_URI } = require('../../modules/secrets');
 const { injectHeroImage, injectCopy } = require('../../middleware/inject-content');
 const contentApi = require('../../services/content-api');
+const grantDataDates = config.get('grantData.dateRange');
+
+const grantNavLink =
+    'http://grantnav.threesixtygiving.org/search?json_query=%7B%22query%22%3A+%7B%22bool%22%3A+%7B%22filter%22%3A+%5B%7B%22bool%22%3A+%7B%22should%22%3A+%5B%7B%22term%22%3A+%7B%22fundingOrganization.id_and_name%22%3A+%22%5B%5C%22The+Big+Lottery+Fund%5C%22%2C+%5C%22360G-blf%5C%22%5D%22%7D%7D%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%2C+%22must%22%3A+%7B%7D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%7B%22range%22%3A+%7B%22amountAwarded%22%3A+%7B%7D%7D%7D%2C+%22must%22%3A+%7B%7D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%5D%2C+%22must%22%3A+%7B%22query_string%22%3A+%7B%22default_field%22%3A+%22_all%22%2C+%22query%22%3A+%22%2A%22%7D%7D%7D%7D%2C+%22sort%22%3A+%7B%22_score%22%3A+%7B%22order%22%3A+%22desc%22%7D%7D%2C+%22aggs%22%3A+%7B%22recipientDistrictName%22%3A+%7B%22terms%22%3A+%7B%22size%22%3A+3%2C+%22field%22%3A+%22recipientDistrictName%22%7D%7D%2C+%22currency%22%3A+%7B%22terms%22%3A+%7B%22size%22%3A+3%2C+%22field%22%3A+%22currency%22%7D%7D%2C+%22recipientOrganization%22%3A+%7B%22terms%22%3A+%7B%22size%22%3A+3%2C+%22field%22%3A+%22recipientOrganization.id_and_name%22%7D%7D%2C+%22fundingOrganization%22%3A+%7B%22terms%22%3A+%7B%22size%22%3A+3%2C+%22field%22%3A+%22fundingOrganization.id_and_name%22%7D%7D%2C+%22recipientRegionName%22%3A+%7B%22terms%22%3A+%7B%22size%22%3A+3%2C+%22field%22%3A+%22recipientRegionName%22%7D%7D%7D%2C+%22extra_context%22%3A+%7B%22awardYear_facet_size%22%3A+3%2C+%22amountAwardedFixed_facet_size%22%3A+3%7D%7D';
 
 /**
  * Combine a ?page param with existing querystrings for grant search
@@ -97,6 +102,14 @@ router.get(
         queryWithPage.locale = res.locals.locale;
         let data;
 
+        // Grab some case studies
+        const caseStudiesResponse = await contentApi.getCaseStudies({
+            locale: req.i18n.getLocale()
+        });
+
+        // Shuffle the valid case studies and grab the first few
+        const caseStudies = sampleSize(caseStudiesResponse.filter(c => c.grantId), 3);
+
         try {
             data = await queryGrantsApi(queryWithPage);
         } catch (errorResponse) {
@@ -121,6 +134,9 @@ router.get(
                     grants: data.results,
                     facets: data.facets,
                     meta: data.meta,
+                    grantDataDates: grantDataDates,
+                    caseStudies: caseStudies,
+                    grantNavLink: grantNavLink,
                     pagination: buildPagination(data.meta.pagination, queryWithPage)
                 });
             },
@@ -158,7 +174,40 @@ router.get(
     }
 );
 
-router.get('/grant/:id', injectCopy('funding.pastGrants.search'), async (req, res, next) => {
+router.get('/recipients/:id', injectCopy('funding.pastGrants.search'), async (req, res, next) => {
+    try {
+        let qs = addPaginationParameters({}, req.query.page);
+        qs.recipient = req.params.id;
+        qs.locale = res.locals.locale;
+        const data = await request({
+            url: PAST_GRANTS_API_URI,
+            json: true,
+            qs: qs
+        });
+
+        if (data && data.meta.totalResults > 0) {
+            const firstResult = head(data.results);
+            const organisation = head(firstResult.recipientOrganization);
+            res.render(path.resolve(__dirname, './views/recipient-detail'), {
+                title: organisation.name,
+                organisation: organisation,
+                recipientGrants: data.results,
+                recipientProgrammes: data.facets.grantProgramme,
+                recipientLocalAuthorities: data.facets.localAuthorities,
+                totalAwarded: data.meta.totalAwarded.toLocaleString(),
+                totalResults: data.meta.totalResults.toLocaleString(),
+                breadcrumbs: concat(res.locals.breadcrumbs, { label: organisation.name }),
+                pagination: buildPagination(data.meta.pagination, qs)
+            });
+        } else {
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:id', injectCopy('funding.pastGrants.search'), async (req, res, next) => {
     try {
         const data = await request({
             url: `${PAST_GRANTS_API_URI}/${req.params.id}`,
@@ -186,39 +235,6 @@ router.get('/grant/:id', injectCopy('funding.pastGrants.search'), async (req, re
                 grant: grant,
                 fundingProgramme: fundingProgramme,
                 breadcrumbs: concat(res.locals.breadcrumbs, { label: data.result.title })
-            });
-        } else {
-            next();
-        }
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.get('/recipient/:id', injectCopy('funding.pastGrants.search'), async (req, res, next) => {
-    try {
-        let qs = addPaginationParameters({}, req.query.page);
-        qs.recipient = req.params.id;
-        qs.locale = res.locals.locale;
-        const data = await request({
-            url: PAST_GRANTS_API_URI,
-            json: true,
-            qs: qs
-        });
-
-        if (data && data.meta.totalResults > 0) {
-            const firstResult = head(data.results);
-            const organisation = head(firstResult.recipientOrganization);
-            res.render(path.resolve(__dirname, './views/recipient-detail'), {
-                title: organisation.name,
-                organisation: organisation,
-                recipientGrants: data.results,
-                recipientProgrammes: data.facets.grantProgramme,
-                recipientLocalAuthorities: data.facets.localAuthorities,
-                totalAwarded: data.meta.totalAwarded.toLocaleString(),
-                totalResults: data.meta.totalResults.toLocaleString(),
-                breadcrumbs: concat(res.locals.breadcrumbs, { label: organisation.name }),
-                pagination: buildPagination(data.meta.pagination, qs)
             });
         } else {
             next();
