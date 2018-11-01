588d49d67958a477f1496124a441866634688bf0
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-11-01 16:53:14 +0000

    Remove hero image from search past grants

--- controllers/grants/search.js
+++ controllers/grants/search.js
@@ -10,7 +10,7 @@ const nunjucks = require('nunjucks');
 const Raven = require('raven');
 
 const { PAST_GRANTS_API_URI } = require('../../modules/secrets');
-const { injectHeroImage, injectCopy } = require('../../middleware/inject-content');
+const { injectCopy } = require('../../middleware/inject-content');
 const contentApi = require('../../services/content-api');
 const grantDataDates = config.get('grantData.dateRange');
 
@@ -102,99 +102,94 @@ function buildReturnLink(queryParams, urlBase = './') {
     return returnLink;
 }
 
-router.get(
-    '/',
-    injectHeroImage('active-plus-communities'),
-    injectCopy('funding.pastGrants.search'),
-    async (req, res, next) => {
-        let data;
-        const facetParams = buildAllowedParams(req.query);
-        const paginationLabels = req.i18n.__('global.misc.pagination');
-        let queryWithPage = addPaginationParameters(facetParams, req.query.page);
+router.get('/', injectCopy('funding.pastGrants.search'), async (req, res, next) => {
+    let data;
+    const facetParams = buildAllowedParams(req.query);
+    const paginationLabels = req.i18n.__('global.misc.pagination');
+    let queryWithPage = addPaginationParameters(facetParams, req.query.page);
 
-        // Add a parameter so we know the user came from search
-        // (so we can link them back to their results)
-        const searchQueryString = querystring.stringify(
-            Object.assign({}, queryWithPage, {
-                from: 'search'
-            })
-        );
+    // Add a parameter so we know the user came from search
+    // (so we can link them back to their results)
+    const searchQueryString = querystring.stringify(
+        Object.assign({}, queryWithPage, {
+            from: 'search'
+        })
+    );
 
-        queryWithPage.locale = res.locals.locale;
+    queryWithPage.locale = res.locals.locale;
 
-        try {
-            data = await queryGrantsApi(queryWithPage);
-        } catch (errorResponse) {
-            return res.format({
-                html: () => {
-                    next(errorResponse.error);
-                },
-                'application/json': () => {
-                    res.status(errorResponse.error.error.status || 400).json({
-                        error: errorResponse.error.error
-                    });
-                }
-            });
-        }
-
-        res.format({
-            // Initial / server-only search
-            html: async () => {
-                // Grab some case studies
-                const caseStudiesResponse = await contentApi.getCaseStudies({
-                    locale: req.i18n.getLocale()
-                });
-
-                // Shuffle the valid case studies and grab the first few
-                const caseStudies = sampleSize(caseStudiesResponse.filter(c => c.grantId), 3);
-
-                res.render(path.resolve(__dirname, './views/search'), {
-                    title: res.locals.copy.title,
-                    queryParams: isEmpty(facetParams) ? false : facetParams,
-                    grants: data.results,
-                    facets: data.facets,
-                    meta: data.meta,
-                    grantDataDates: grantDataDates,
-                    caseStudies: caseStudies,
-                    grantNavLink: grantNavLink,
-                    searchQueryString: searchQueryString,
-                    pagination: buildPagination(data.meta.pagination, queryWithPage, paginationLabels)
-                });
+    try {
+        data = await queryGrantsApi(queryWithPage);
+    } catch (errorResponse) {
+        return res.format({
+            html: () => {
+                next(errorResponse.error);
             },
-
-            // AJAX search for client-side app
             'application/json': () => {
-                const isRelatedSearch = req.query.related === 'true';
-
-                // Repopulate existing app globals so Nunjucks can read them
-                // outside of Express's view engine context
-                const context = Object.assign({}, res.locals, req.app.locals, {
-                    grants: data.results,
-                    searchQueryString: searchQueryString,
-                    pagination: buildPagination(data.meta.pagination, queryWithPage, paginationLabels),
-                    options: {
-                        wrapperClass: isRelatedSearch ? 'flex-grid__item' : false,
-                        hidePagination: isRelatedSearch
-                    }
-                });
-                const template = path.resolve(__dirname, './views/ajax-results.njk');
-
-                nunjucks.render(template, context, (renderErr, html) => {
-                    if (renderErr) {
-                        Raven.captureException(renderErr);
-                        res.status(400).json({ error: 'ERR-TEMPLATE-ERROR' });
-                    } else {
-                        res.json({
-                            meta: data.meta,
-                            facets: data.facets,
-                            resultsHtml: html
-                        });
-                    }
+                res.status(errorResponse.error.error.status || 400).json({
+                    error: errorResponse.error.error
                 });
             }
         });
     }
-);
+
+    res.format({
+        // Initial / server-only search
+        html: async () => {
+            // Grab some case studies
+            const caseStudiesResponse = await contentApi.getCaseStudies({
+                locale: req.i18n.getLocale()
+            });
+
+            // Shuffle the valid case studies and grab the first few
+            const caseStudies = sampleSize(caseStudiesResponse.filter(c => c.grantId), 3);
+
+            res.render(path.resolve(__dirname, './views/search'), {
+                title: res.locals.copy.title,
+                queryParams: isEmpty(facetParams) ? false : facetParams,
+                grants: data.results,
+                facets: data.facets,
+                meta: data.meta,
+                grantDataDates: grantDataDates,
+                caseStudies: caseStudies,
+                grantNavLink: grantNavLink,
+                searchQueryString: searchQueryString,
+                pagination: buildPagination(data.meta.pagination, queryWithPage, paginationLabels)
+            });
+        },
+
+        // AJAX search for client-side app
+        'application/json': () => {
+            const isRelatedSearch = req.query.related === 'true';
+
+            // Repopulate existing app globals so Nunjucks can read them
+            // outside of Express's view engine context
+            const context = Object.assign({}, res.locals, req.app.locals, {
+                grants: data.results,
+                searchQueryString: searchQueryString,
+                pagination: buildPagination(data.meta.pagination, queryWithPage, paginationLabels),
+                options: {
+                    wrapperClass: isRelatedSearch ? 'flex-grid__item' : false,
+                    hidePagination: isRelatedSearch
+                }
+            });
+            const template = path.resolve(__dirname, './views/ajax-results.njk');
+
+            nunjucks.render(template, context, (renderErr, html) => {
+                if (renderErr) {
+                    Raven.captureException(renderErr);
+                    res.status(400).json({ error: 'ERR-TEMPLATE-ERROR' });
+                } else {
+                    res.json({
+                        meta: data.meta,
+                        facets: data.facets,
+                        resultsHtml: html
+                    });
+                }
+            });
+        }
+    });
+});
 
 router.get('/recipients/:id', injectCopy('funding.pastGrants.search'), async (req, res, next) => {
     try {
