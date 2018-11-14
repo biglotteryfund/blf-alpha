c1880a06c95ae32524737df818ed69f3b8c08b88
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-11-14 15:52:25 +0000

    Re-order updates controller

--- controllers/updates/index.js
+++ controllers/updates/index.js
@@ -1,81 +1,66 @@
 'use strict';
 const path = require('path');
 const express = require('express');
-const { concat, isArray, pick, get, head } = require('lodash');
+const { concat, compact, isArray, pick, get, head } = require('lodash');
+
+const { buildPagination } = require('../../modules/pagination');
+const { injectBreadcrumbs, injectCopy } = require('../../middleware/inject-content');
+const appData = require('../../modules/appData');
+const contentApi = require('../../services/content-api');
 
 const router = express.Router();
 
-const { injectBreadcrumbs, injectCopy } = require('../../middleware/inject-content');
-const { getUpdates } = require('../../services/content-api');
-const appData = require('../../modules/appData');
-const { buildPagination } = require('../../modules/pagination');
-
-function renderListing({
-    res,
-    title,
-    entries = [],
-    entriesMeta = null,
-    breadcrumbs = [],
-    isNewsLandingPage = false,
-    copy
-}) {
-    const pagination = buildPagination(entriesMeta.pagination);
-    const template = isNewsLandingPage ? 'landing' : 'listing';
-    res.render(path.resolve(__dirname, `./views/${template}`), {
-        title,
-        entries,
-        entriesMeta,
-        pagination,
-        breadcrumbs,
-        isNewsLandingPage,
-        copy
-    });
-}
-
-function translateEntryTypeName(entry, copy) {
-    const typeTranslation = get(copy.types, entry.updateType.slug);
-    if (typeTranslation) {
-        entry.updateType.name = typeTranslation;
-    }
-    return entry;
+function translateEntryFor(copy) {
+    return function(entry) {
+        const typeTranslation = get(copy.types, entry.updateType.slug);
+        if (typeTranslation) {
+            entry.updateType.name = typeTranslation;
+        }
+        return entry;
+    };
 }
 
 if (appData.isNotProduction) {
     router.get('/:type?/:date?/:slug?', injectBreadcrumbs, injectCopy('toplevel.news'), async (req, res, next) => {
-        let urlPath = req.params.type ? `/${req.params.type}` : '';
-        const isSinglePost = req.params.date && req.params.slug;
-
-        if (isSinglePost) {
-            // eg. fetch a specific entry
-            urlPath += `/${req.params.date}/${req.params.slug}`;
-        }
-
-        // Listings (but not single posts) can be filtered by the following valid query parameters:
-        let query = !isSinglePost ? pick(req.query, ['page', 'tag', 'author', 'category']) : {};
-
         try {
-            const response = await getUpdates({
+            const urlParts = compact([req.params.type, req.params.date, req.params.slug]);
+
+            const response = await contentApi.getUpdates({
                 locale: req.i18n.getLocale(),
-                urlPath: urlPath,
-                query: query
+                urlPath: `/${urlParts.join('/')}`,
+                // Listings (but not single posts) can be filtered by the following valid query parameters:
+                query: urlParts.length === 3 ? {} : pick(req.query, ['page', 'tag', 'author', 'category'])
             });
 
-            if (response.result) {
-                // Do we have a listing page?
-                if (isArray(response.result)) {
-                    // Map and translate the entry type names into plurals
-                    response.result = response.result.map(entry => translateEntryTypeName(entry, res.locals.copy));
+            if (!response.result) {
+                next();
+            }
 
-                    // Is this a specific listing page (eg. blog, press releases) or everything?
+            const entryWithTranslation = translateEntryFor(res.locals.copy);
+
+            if (isArray(response.result)) {
+                // Map and translate the entry type names into plurals
+                const entries = response.result.map(entryWithTranslation);
+
+                const entriesMeta = response.meta;
+                const pagination = buildPagination(entriesMeta.pagination);
+
+                if (urlParts.length === 0) {
+                    /**
+                     * Render landing page
+                     */
+                    const pageTitle = 'All news'; // @TODO: i18n
+                    res.render(path.resolve(__dirname, `./views/landing`), {
+                        title: pageTitle,
+                        entries: entries,
+                        entriesMeta: entriesMeta,
+                        pagination: pagination,
+                        breadcrumbs: concat(res.locals.breadcrumbs, {
+                            label: pageTitle
+                        })
+                    });
+                } else {
                     const filterType = get(response, 'meta.pageType');
-                    const isNewsLandingPage = !req.params.type && filterType === 'single';
-
-                    // @TODO i18n
-                    let defaultPageTitle = 'All news';
-                    if (!isNewsLandingPage) {
-                        const firstItem = head(response.result);
-                        defaultPageTitle = get(firstItem.updateType.name, 'plural', firstItem.updateType.name);
-                    }
 
                     const filterLabel = {
                         author: get(response, 'meta.activeAuthor'),
@@ -84,53 +69,59 @@ if (appData.isNotProduction) {
                     }[filterType];
 
                     // @TODO i18n
-                    const pageTitle = filterLabel
-                        ? {
-                              author: `Author: ${filterLabel.title}`,
-                              tag: `Tag: ${filterLabel.title}`,
-                              category: `Category: ${filterLabel.title}`
-                          }[filterType]
-                        : defaultPageTitle;
+                    const titleMap = {
+                        author: function() {
+                            return `Author: ${filterLabel.title}`;
+                        },
+                        tag: function() {
+                            return `Tag: ${filterLabel.title}`;
+                        },
+                        category: function() {
+                            return `Category: ${filterLabel.title}`;
+                        },
+                        default: function() {
+                            const firstItem = head(entries);
+                            return get(firstItem.updateType.name, 'plural', firstItem.updateType.name);
+                        }
+                    };
 
-                    const breadcrumbs = concat(res.locals.breadcrumbs, {
-                        label: filterLabel ? filterLabel.title : pageTitle
-                    });
+                    const pageTitle = (titleMap[filterType] || titleMap['default'])();
 
-                    return renderListing({
-                        res,
+                    /**
+                     * Render listing page
+                     */
+                    res.render(path.resolve(__dirname, `./views/listing`), {
                         title: pageTitle,
-                        entries: response.result,
-                        entriesMeta: response.meta,
-                        breadcrumbs: breadcrumbs,
-                        isNewsLandingPage: isNewsLandingPage,
-                        copy: res.locals.copy
+                        entries: entries,
+                        entriesMeta: entriesMeta,
+                        pagination: pagination,
+                        breadcrumbs: concat(res.locals.breadcrumbs, {
+                            label: filterLabel ? filterLabel.title : pageTitle
+                        })
                     });
-                } else {
-                    // Single entry page
-                    if (req.baseUrl + req.path !== response.result.linkUrl) {
-                        // Prevent URL tampering with dates, since we don't validate them on the API end
-                        res.redirect(response.result.linkUrl);
-                    } else {
-                        const entry = translateEntryTypeName(response.result, res.locals.copy);
-                        return res.render(path.resolve(__dirname, './views/post'), {
-                            title: entry.title,
-                            entry: entry,
-                            copy: res.locals.copy,
-                            breadcrumbs: concat(
-                                res.locals.breadcrumbs,
-                                {
-                                    label: get(entry.updateType.name, 'singular', entry.updateType.name),
-                                    url: res.locals.localify(`${req.baseUrl}/${req.params.type}`)
-                                },
-                                {
-                                    label: entry.title
-                                }
-                            )
-                        });
-                    }
                 }
             } else {
-                next();
+                // Prevent URL tampering with dates, since we don't validate them on the API end
+                if (req.baseUrl + req.path !== response.result.linkUrl) {
+                    res.redirect(response.result.linkUrl);
+                } else {
+                    const entry = entryWithTranslation(response.result);
+                    const title = entry.title;
+
+                    const breadcrumbs = concat(
+                        res.locals.breadcrumbs,
+                        {
+                            label: get(entry.updateType.name, 'plural', entry.updateType.name),
+                            url: res.locals.localify(`${req.baseUrl}/${req.params.type}`)
+                        },
+                        { label: title }
+                    );
+
+                    /**
+                     * Render single entry page
+                     */
+                    return res.render(path.resolve(__dirname, './views/post'), { title, entry, breadcrumbs });
+                }
             }
         } catch (e) {
             next(e);
