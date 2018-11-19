30dee0e1eecbb477d6aaea311b283116d3f22dc5
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-11-19 10:04:04 +0000

    Close the Youth Capacity form

--- controllers/apply/index.js
+++ controllers/apply/index.js
@@ -1,11 +1,11 @@
 'use strict';
 const express = require('express');
+const path = require('path');
 
 const { initFormRouter } = require('./form-router');
 
 const digitalFundForm = require('./digital-fund/form-model');
 const reachingCommunitiesForm = require('./reaching-communities/form-model');
-const youthCapacityForm = require('./youth-capacity/form-model');
 
 const router = express.Router();
 
@@ -13,6 +13,14 @@ router.get('/', (req, res) => res.redirect('/'));
 router.use('/digital-fund-strand-1', initFormRouter(digitalFundForm.strand1));
 router.use('/digital-fund-strand-2', initFormRouter(digitalFundForm.strand2));
 router.use('/your-idea', initFormRouter(reachingCommunitiesForm));
-router.use('/youth-capacity', initFormRouter(youthCapacityForm));
+
+router.get('/youth-capacity', (req, res) => {
+    res.render(path.resolve(__dirname, './youth-capacity/startpage-closed'), {
+        title: 'Youth Capacity Fund'
+    });
+});
+router.all('/youth-capacity/*', (req, res) => {
+    res.redirect(`${req.baseUrl}/youth-capacity`);
+});
 
 module.exports = router;
