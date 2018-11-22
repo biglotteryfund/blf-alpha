d41beba35dacf20e8bda66de9f0fc9f65d4225a2
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2018-11-22 14:20:21 +0000

    Fix bug where a cleared query wasn't updating the search

--- assets/js/vue-apps/past-grants.js
+++ assets/js/vue-apps/past-grants.js
@@ -204,6 +204,8 @@ function init(STORAGE_KEY) {
                     combinedFilters.q = this.activeQuery;
                     this.handleActiveFilter({ label: this.activeQuery || undefined, name: 'q' });
                 } else {
+                    // Clear the filter if necessary
+                    this.filters.q = undefined;
                     this.filterSummary = this.filterSummary.filter(i => i.name !== 'q');
                 }
 
