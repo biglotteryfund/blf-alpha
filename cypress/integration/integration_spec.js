6e5c5ad2114357cfde650eade81b2e02159ac96c
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-01-21 16:43:09 +0000

    Update to search against both new and old domain

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -23,14 +23,16 @@ describe('common', function() {
     it('should redirect search queries to a google site search', () => {
         cy.checkRedirect({
             from: '/search?q=This is my search query',
-            to: 'https://www.google.co.uk/search?q=site%3Awww.biglotteryfund.org.uk+This%20is%20my%20search%20query',
+            to:
+                'https://www.google.co.uk/search?q=This%20is%20my%20search%20query+site%3Awww.biglotteryfund.org.uk+OR+site%3Awww.tnlcommunityfund.org.uk&btnK=Google+Search&oq=national+site%3Awww.biglotteryfund.org.uk+OR+site%3Awww.tnlcommunityfund.org.uk',
             isRelative: false,
             status: 302
         });
 
         cy.checkRedirect({
             from: '/search?lang=en-GB&amp;q=something&amp;type=All&amp;order=r',
-            to: 'https://www.google.co.uk/search?q=site%3Awww.biglotteryfund.org.uk+something',
+            to:
+                'https://www.google.co.uk/search?q=something+site%3Awww.biglotteryfund.org.uk+OR+site%3Awww.tnlcommunityfund.org.uk&btnK=Google+Search&oq=national+site%3Awww.biglotteryfund.org.uk+OR+site%3Awww.tnlcommunityfund.org.uk',
             isRelative: false,
             status: 302
         });
