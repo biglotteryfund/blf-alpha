f732ade350deee5a9707d4a9c6f6fb3e0eefc257
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2019-01-23 10:58:11 +0000

    Redirect archived press releases to the National Archives

--- modules/archived.js
+++ modules/archived.js
@@ -2,9 +2,9 @@
 
 const LAST_GOOD_CRAWL_DATE = '20171011152352';
 
-function buildArchiveUrl(urlPath) {
+function buildArchiveUrl(urlPath, crawlDate = LAST_GOOD_CRAWL_DATE) {
     const fullUrl = `https://www.biglotteryfund.org.uk${urlPath}`;
-    return `http://webarchive.nationalarchives.gov.uk/${LAST_GOOD_CRAWL_DATE}/${fullUrl}`;
+    return `http://webarchive.nationalarchives.gov.uk/${crawlDate}/${fullUrl}`;
 }
 
 module.exports = {
