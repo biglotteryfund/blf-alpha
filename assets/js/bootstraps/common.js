35d508684cf240ed3cd20ab30052615861890572
Matt Andrews <matt.andrews@biglotteryfund.org.uk>
2019-01-24 13:20:31 +0000

    Remove old logo

--- assets/js/bootstraps/common.js
+++ assets/js/bootstraps/common.js
@@ -5,14 +5,12 @@
 import global from '../modules/global';
 import tabs from '../modules/tabs';
 import heroImages from '../modules/heroImages';
-import logos from '../modules/logos';
 import forms from '../modules/forms';
 
 function init() {
     global.init();
     tabs.init();
     heroImages.init();
-    logos.init();
     forms.init();
 }
 
