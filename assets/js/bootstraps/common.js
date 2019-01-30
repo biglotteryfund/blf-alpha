b5e5837a20d563ef25971e44c422e37573c67cce
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-01-30 16:49:05 +0000

    Remove some unused code

--- assets/js/bootstraps/common.js
+++ assets/js/bootstraps/common.js
@@ -2,15 +2,14 @@
  * Common modules that are run across the site.
  * (Non-Vue components)
  */
-import global from '../modules/global';
+import fitvids from 'fitvids';
 import tabs from '../modules/tabs';
-import heroImages from '../modules/heroImages';
 import forms from '../modules/forms';
 
 function init() {
-    global.init();
+    fitvids('.video-container');
+
     tabs.init();
-    heroImages.init();
     forms.init();
 }
 
