4ea26638f229c5e1ace113c7102aa1f842c79102
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-02-25 16:29:53 +0000

    Begin adding tests for form validators

--- controllers/apply-next/simple/__tests__/form-model.test.js
+++ controllers/apply-next/simple/__tests__/form-model.test.js
@@ -1,20 +1,75 @@
 /* eslint-env jest */
 'use strict';
+const httpMocks = require('node-mocks-http');
+const { validationResult } = require('express-validator/check');
 
-const { FIELDS, CONDITIONS, ORGANISATION_TYPES } = require('../form-model');
+const { VALIDATORS } = require('../form-model');
 
-describe('form conditions', () => {
-    it('should test condition for charity number', () => {
-        expect(
-            CONDITIONS.needsCharityNumber({
-                [FIELDS.organisationType.name]: ORGANISATION_TYPES.unincorporatedRegisteredCharity.value
-            })
-        ).toBeTruthy();
+describe('form validators', () => {
+    function requestWithBody(body) {
+        const req = httpMocks.createRequest({
+            method: 'POST',
+            body: body
+        });
 
-        expect(
-            CONDITIONS.needsCharityNumber({
-                [FIELDS.organisationType.name]: 'invalid'
-            })
-        ).toBeFalsy();
+        req.i18n = {
+            getLocale() {
+                return 'en';
+            }
+        };
+
+        return req;
+    }
+
+    describe('required', () => {
+        const validatorFn = VALIDATORS.required({ en: 'Value must be provided', cy: '' })({
+            name: 'example'
+        });
+
+        test('required field missing', done => {
+            const reqA = requestWithBody({});
+            validatorFn(reqA, httpMocks.createResponse(), () => {
+                const errors = validationResult(reqA).mapped();
+                expect(errors.example.msg).toBe('Value must be provided');
+                done();
+            });
+        });
+
+        test('required field present', done => {
+            const req = requestWithBody({
+                example: 'something'
+            });
+            validatorFn(req, httpMocks.createResponse(), () => {
+                const errors = validationResult(req).array();
+                expect(errors.length).toBe(0);
+                done();
+            });
+        });
+    });
+
+    describe('email', () => {
+        const validatorFn = VALIDATORS.email({
+            name: 'email'
+        });
+
+        test('invalid email', done => {
+            const reqA = requestWithBody({});
+            validatorFn(reqA, httpMocks.createResponse(), () => {
+                const errors = validationResult(reqA).mapped();
+                expect(errors.email.msg).toBe('Please provide a valid email address');
+                done();
+            });
+        });
+
+        test('email present', done => {
+            const req = requestWithBody({
+                email: 'example@example.com'
+            });
+            validatorFn(req, httpMocks.createResponse(), () => {
+                const errors = validationResult(req).array();
+                expect(errors.length).toBe(0);
+                done();
+            });
+        });
     });
 });
