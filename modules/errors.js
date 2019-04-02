55cf2ce59ebbcf8d8a923e428f53ebcf2d0ed0ee
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-04-02 10:45:14 +0100

    Extract messagesForError function from normaliseErrors

--- modules/errors.js
+++ modules/errors.js
@@ -3,6 +3,23 @@ const { concat, has, head, flatMap, includes } = require('lodash');
 const { filter, get, getOr, uniqBy } = require('lodash/fp');
 
 /**
+ * Find suitable errors
+ * 1. Find messages which either have a key **and** type or **only** a type
+ *    Allows us to scope errors messages to specific keys in groups of fields (e.g. addresses, dates of birth)
+ * 2. If no matching messages are found then look for a type of 'base'
+ *    Allows us to show a generic message for any unmatched error type e.g. "Please enter your name"
+ */
+function messagesForError(detail, messages) {
+    const filterKeyAndType = filter(message => message.key === detail.context.key && message.type === detail.type);
+    const filterTypeOnly = filter(message => !has(message, 'key') && message.type === detail.type);
+    const filterBase = filter(message => !has(message, 'key') && message.type === 'base');
+
+    const matches = concat(filterKeyAndType(messages), filterTypeOnly(messages));
+
+    return matches.length ? matches : filterBase(messages);
+}
+
+/**
  * Normalise errors for use in views
  * - Maps raw joi error objects to a simplified format and
  * - In order to avoid showing multiple validation errors per field we find the first error per field name
@@ -17,32 +34,17 @@ const { filter, get, getOr, uniqBy } = require('lodash/fp');
 function normaliseErrors({ validationError, errorMessages, locale, fieldNames = [] }) {
     const errors = getOr([], 'details')(validationError);
 
-    const filteredErrors =
+    const errorDetails =
         fieldNames.length > 0 ? errors.filter(detail => includes(fieldNames, head(detail.path))) : errors;
 
-    const uniqueFilteredErrors = uniqBy(detail => head(detail.path))(filteredErrors);
+    const uniqeErrorsDetails = uniqBy(detail => head(detail.path))(errorDetails);
 
-    /**
-     * Find suitable errors
-     * 1. Find messages which either have a key **and** type or **only** a type
-     *    Allows us to scope errors messages to specific keys in groups of fields (e.g. addresses, dates of birth)
-     * 2. If no matching messages are found then look for a type of 'base'
-     *    Allows us to show a generic message for any unmatched error type e.g. "Please enter your name"
-     */
-    const suitableErrors = flatMap(uniqueFilteredErrors, detail => {
+    const suitableErrors = flatMap(uniqeErrorsDetails, detail => {
         const name = head(detail.path);
+        const fieldMessages = getOr([], name)(errorMessages);
+        const matchingMessages = messagesForError(detail, fieldMessages);
 
-        const fieldMessages = errorMessages[name];
-
-        const filterKeyAndType = filter(message => message.key === detail.context.key && message.type === detail.type);
-        const filterTypeOnly = filter(message => !has(message, 'key') && message.type === detail.type);
-        const filterBase = filter(message => !has(message, 'key') && message.type === 'base');
-
-        const matches = concat(filterKeyAndType(fieldMessages), filterTypeOnly(fieldMessages));
-
-        const matchesWithFallback = matches.length ? matches : filterBase(fieldMessages);
-
-        return matchesWithFallback.map(match => {
+        return matchingMessages.map(match => {
             return {
                 param: name,
                 msg: get(`message.${locale}`)(match)
