e1f3a2995d8e39ead49ecbf81a10691a17fea636
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-04-16 11:19:46 +0100

    Refactor password forms

--- controllers/user/dashboard.js
+++ controllers/user/dashboard.js
@@ -4,10 +4,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    let alertMessage;
+    switch (req.query.s) {
+        case 'passwordUpdated':
+            alertMessage = 'Your password was successfully updated!';
+            break;
+        case 'activationSent':
+            alertMessage = `We have sent an email to ${
+                req.user.userData.username
+            } with a link to activate your account.`;
+            break;
+    }
+
     res.render(path.resolve(__dirname, './views/dashboard'), {
         user: req.user,
         errors: res.locals.errors || [],
-        activationSent: req.query.s === 'activationSent'
+        alertMessage: alertMessage
     });
 });
 
--- controllers/user/password.js
+++ controllers/user/password.js
@@ -45,6 +45,47 @@ async function processResetRequest(req, res, user) {
     });
 }
 
+function verifyToken(token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, JWT_SIGNING_TOKEN, async (err, decoded) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (decoded.data.reason === 'resetpassword') {
+                    resolve(decoded.data);
+                } else {
+                    reject(new Error('Invalid token reason'));
+                }
+            }
+        });
+    });
+}
+
+function validatePasswordChangeRequest(username, password, locale) {
+    /**
+     * Validate new password,
+     * Include username in password to allow us to check against conditional rules
+     * e.g. password must not match username
+     */
+    const validationResult = schema.accountSchema.validate(
+        { username: username, password: password },
+        { abortEarly: false, stripUnknown: true }
+    );
+
+    const errors = normaliseErrors({
+        validationError: validationResult.error,
+        errorMessages: schema.errorMessages,
+        locale: locale,
+        fieldNames: ['password']
+    });
+
+    return { validationResult, errors };
+}
+
+function redirectToLogin(req, res) {
+    res.redirect(localify(req.i18n.getLocale())('/user/login'));
+}
+
 function renderForgotForm(req, res, data = null, errors = []) {
     res.render(path.resolve(__dirname, './views/forgotten-password'), {
         formValues: data,
@@ -52,6 +93,20 @@ function renderForgotForm(req, res, data = null, errors = []) {
     });
 }
 
+function renderResetForm(req, res) {
+    res.render(path.resolve(__dirname, './views/reset-password'), {
+        errors: res.locals.errors || [],
+        user: req.user
+    });
+}
+
+function renderResetFormExpired(req, res) {
+    res.render(path.resolve(__dirname, './views/reset-password-expired'));
+}
+
+/**
+ * Route: forgotten password form
+ */
 router
     .route('/forgot')
     .all(requireUnauthed)
@@ -92,108 +147,117 @@ router
         }
     });
 
-function verifyToken(token) {
-    return new Promise((resolve, reject) => {
-        jwt.verify(token, JWT_SIGNING_TOKEN, async (err, decoded) => {
-            if (err) {
-                reject(err);
-            } else {
-                if (decoded.data.reason === 'resetpassword') {
-                    resolve(decoded.data);
-                } else {
-                    reject(new Error('Invalid token reason'));
-                }
-            }
-        });
-    });
-}
-
-function redirectToLogin(req, res) {
-    res.redirect(localify(req.i18n.getLocale())('/user/login'));
-}
-
-function renderResetForm(req, res) {
-    res.render(path.resolve(__dirname, './views/reset-password'), {
-        errors: res.locals.errors || []
-    });
-}
-
-function renderResetFormExpired(req, res) {
-    res.render(path.resolve(__dirname, './views/reset-password-expired'));
-}
-
+/**
+ * Route: reset password form
+ */
 router
     .route('/reset')
-    .all(requireUnauthed)
     .get(async (req, res) => {
+        // If we have a logged-in user, simply let them change their password
+        if (req.user) {
+            return renderResetForm(req, res);
+        }
+
+        // Otherwise handle an unauthed user and verify their query token is valid
         let token = req.query.token ? req.query.token : res.locals.token;
         if (!token) {
-            redirectToLogin(req, res);
+            return redirectToLogin(req, res);
         } else {
             try {
                 await verifyToken(token);
                 res.locals.token = token;
-                renderResetForm(req, res);
+                return renderResetForm(req, res);
             } catch (error) {
-                renderResetFormExpired(req, res);
+                return renderResetFormExpired(req, res);
             }
         }
     })
     .post(async (req, res) => {
-        const { token } = req.body;
+        // If we have a logged-in user, attempt to change their password
+        if (req.user) {
+            // Confirm the typed password matches the currently-stored one
+            const passwordMatches = await userService.isValidPassword(
+                req.user.userData.password,
+                req.body['password-old']
+            );
 
-        if (!token) {
-            redirectToLogin(req, res);
-        } else {
-            try {
-                const decodedData = await verifyToken(token);
-
-                /**
-                 * Validate new password,
-                 * Include username in password to allow us to check against conditional rules
-                 * e.g. password must not match username
-                 */
-                const validationResult = schema.accountSchema.validate(
-                    { username: decodedData.userId, password: req.body.password },
-                    { abortEarly: false, stripUnknown: true }
+            if (passwordMatches) {
+                // Update the stored password to new one
+                const { validationResult, errors } = validatePasswordChangeRequest(
+                    req.user.userData.id,
+                    req.body.password,
+                    req.i18n.getLocale()
                 );
-
-                const errors = normaliseErrors({
-                    validationError: validationResult.error,
-                    errorMessages: schema.errorMessages,
-                    locale: req.i18n.getLocale(),
-                    fieldNames: ['password']
-                });
 
                 if (errors.length > 0) {
                     res.locals.errors = errors;
-                    res.locals.token = token;
                     res.locals.formValues = validationResult.value;
                     return renderResetForm(req, res);
                 } else {
                     try {
-                        const user = await userService.findWithActivePasswordReset({
-                            id: validationResult.value.username
+                        await userService.updateNewPassword({
+                            id: validationResult.value.username,
+                            newPassword: validationResult.value.password
                         });
-
-                        if (user) {
-                            await userService.updateNewPassword({
-                                id: validationResult.value.username,
-                                newPassword: validationResult.value.password
-                            });
-                        } else {
-                            throw new Error('User not found');
-                        }
-
-                        res.redirect('/user/login?s=passwordUpdated');
+                        res.redirect('/user?s=passwordUpdated');
                     } catch (error) {
-                        res.locals.token = token;
                         res.locals.alertMessage = 'There was an problem updating your password - please try again';
                         renderResetForm(req, res);
                     }
                 }
-            } catch (error) {
-                renderResetFormExpired(req, res);
+            } else {
+                // Show an error for invalid old password
+                res.locals.alertMessage = 'Your old password was not correct - please try again';
+                renderResetForm(req, res);
+            }
+        } else {
+            // Handle an unauthed user who submitted the form with a JWT
+            const { token } = req.body;
+
+            if (!token) {
+                redirectToLogin(req, res);
+            } else {
+                try {
+                    const decodedData = await verifyToken(token);
+
+                    // Is this user's token valid to modify this password?
+                    const { validationResult, errors } = validatePasswordChangeRequest(
+                        decodedData.userId,
+                        req.body.password,
+                        req.i18n.getLocale()
+                    );
+
+                    if (errors.length > 0) {
+                        res.locals.errors = errors;
+                        res.locals.token = token;
+                        res.locals.formValues = validationResult.value;
+                        return renderResetForm(req, res);
+                    } else {
+                        try {
+                            // Confirm the user was in password reset mode
+                            const user = await userService.findWithActivePasswordReset({
+                                id: validationResult.value.username
+                            });
+
+                            if (user) {
+                                await userService.updateNewPassword({
+                                    id: validationResult.value.username,
+                                    newPassword: validationResult.value.password
+                                });
+                            } else {
+                                res.redirect('/user/login');
+                            }
+
+                            res.redirect('/user/login?s=passwordUpdated');
+                        } catch (error) {
+                            res.locals.token = token;
+                            res.locals.alertMessage = 'There was an problem updating your password - please try again';
+                            renderResetForm(req, res);
+                        }
+                    }
+                } catch (error) {
+                    renderResetFormExpired(req, res);
+                }
             }
         }
     });
