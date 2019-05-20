d8f16c686d9d667368d60a593877f414077fe9a3
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-05-20 14:29:07 +0100

    Update processor with CI check

--- controllers/apply-next/simple/processor.js
+++ controllers/apply-next/simple/processor.js
@@ -1,10 +1,35 @@
 'use strict';
 const path = require('path');
 const clone = require('lodash/clone');
+const debug = require('debug')('tnlcf:awards-for-all');
 
 const salesforceService = require('../../../services/salesforce');
 const { generateHtmlEmail, sendEmail } = require('../../../services/mail');
 const { fromDateParts } = require('../../../modules/dates');
+
+async function buildCustomerMailConfig(form, application) {
+    const sendTo = {
+        name: `${application.mainContactFirstName} ${
+            application.mainContactLastName
+        }`,
+        address: application.mainContactEmail
+    };
+
+    const html = await generateHtmlEmail({
+        template: path.resolve(__dirname, './customer-email.njk'),
+        templateData: {
+            form: form,
+            showDataProtectionStatement: true
+        }
+    });
+
+    return {
+        sendTo: sendTo,
+        subject: `Thank you for getting in touch with The National Lottery Community Fund!`,
+        type: 'html',
+        content: html
+    };
+}
 
 function salesforceApplication(application) {
     function dateFormat(dt) {
@@ -23,29 +48,17 @@ function salesforceApplication(application) {
     return enriched;
 }
 
-async function buildCustomerMailConfig(form, application) {
-    const sendTo = {
-        name: `${application['mainContactFirstName']} ${
-            application['mainContactLastName']
-        }`,
-        address: application['mainContactEmail']
-    };
-
-    const html = await generateHtmlEmail({
-        template: path.resolve(__dirname, './customer-email.njk'),
-        templateData: {
-            form: form,
-            isArray: xs => Array.isArray(xs),
-            showDataProtectionStatement: true
-        }
-    });
-
-    return {
-        sendTo: sendTo,
-        subject: `Thank you for getting in touch with The National Lottery Community Fund!`,
-        type: 'html',
-        content: html
-    };
+async function submitToSalesforce(submission) {
+    /**
+     * Skip sending mail in test environments
+     */
+    if (!!process.env.CI === true) {
+        debug(`skipped salesforce submission for ${submission.meta.form}`);
+        return Promise.resolve(submission);
+    } else {
+        const salesforce = await salesforceService.authorise();
+        return salesforce.submitFormData(submission);
+    }
 }
 
 /**
@@ -57,17 +70,14 @@ async function buildCustomerMailConfig(form, application) {
  * @param {any} mailTransport
  */
 async function processor({ form, application, meta }, mailTransport = null) {
-    const salesforce = await salesforceService.authorise();
-    const customerMailConfig = await buildCustomerMailConfig(form, application);
-
     return Promise.all([
-        salesforce.submitFormData({
+        submitToSalesforce({
             meta: meta,
             application: salesforceApplication(application)
         }),
         sendEmail({
-            name: 'simple_prototype_customer',
-            mailConfig: customerMailConfig,
+            name: `${form.id}-customer-email`,
+            mailConfig: await buildCustomerMailConfig(form, application),
             mailTransport: mailTransport
         })
     ]);
