b721d2ae74aa60151a023a145ad97ce6eb7608f0
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-06-06 16:45:36 +0100

    Prevent tests from uploading an actual file

--- controllers/apply/form-router-next/lib/s3.js
+++ controllers/apply/form-router-next/lib/s3.js
@@ -3,6 +3,7 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const config = require('config');
 const { S3_KMS_KEY_ID } = require('../../../../common/secrets');
+const debug = require('debug')('tnlcf:s3');
 
 const s3 = new AWS.S3({ signatureVersion: 'v4', region: 'eu-west-2' });
 const bucket = config.get('aws.s3.formUploadBucket');
@@ -33,19 +34,25 @@ const uploadFile = ({ formId, applicationId, fileMetadata }) => {
                 ServerSideEncryption: 'aws:kms',
                 SSEKMSKeyId: S3_KMS_KEY_ID
             };
-            s3.putObject(params, (uploadErr, data) => {
-                if (uploadErr) {
-                    return reject({
-                        error: uploadErr,
-                        fieldName: fileMetadata.fieldName
-                    });
-                } else {
-                    return resolve({
-                        data: data,
-                        key: uploadKey
-                    });
-                }
-            });
+
+            if (!!process.env.TEST_SERVER === true) {
+                debug(`skipped uploading file ${uploadKey}`);
+                return resolve();
+            } else {
+                s3.putObject(params, (uploadErr, data) => {
+                    if (uploadErr) {
+                        return reject({
+                            error: uploadErr,
+                            fieldName: fileMetadata.fieldName
+                        });
+                    } else {
+                        return resolve({
+                            data: data,
+                            key: uploadKey
+                        });
+                    }
+                });
+            }
         });
     });
 };
