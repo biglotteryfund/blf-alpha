b2f37d9d2cec6e9b23912cba9051654291517bbf
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-06-17 10:09:51 +0100

    Fix another test

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -329,7 +329,7 @@ describe('user', () => {
     });
 });
 
-describe('awards for all', function() {
+describe.only('awards for all', function() {
     it('should submit full awards for all application', () => {
         const randomCountry = faker.random.arrayElement([
             'England',
@@ -664,7 +664,7 @@ describe('awards for all', function() {
             cy.getByLabelText('Day').type('5');
             cy.getByLabelText('Month').type('11');
             cy.getByLabelText('Year').type('1926');
-            cy.getByText('Current address')
+            cy.getByText('Home address')
                 .parent()
                 .within(() => {
                     fillAddress();
@@ -691,7 +691,7 @@ describe('awards for all', function() {
             cy.getByLabelText('Day').type('5');
             cy.getByLabelText('Month').type('11');
             cy.getByLabelText('Year').type('1926');
-            cy.getByText('Current address')
+            cy.getByText('Home address')
                 .parent()
                 .within(() => {
                     fillAddress();
