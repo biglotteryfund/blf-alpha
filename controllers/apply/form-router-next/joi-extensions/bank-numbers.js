af76bcab56db1a5b7b55bba137817be4fb061157
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-06-21 11:46:15 +0100

    Add bank details fields to properly coerce account/sort codes

---
+++ controllers/apply/form-router-next/joi-extensions/bank-numbers.js
@@ -0,0 +1,57 @@
+'use strict';
+const { isString } = require('lodash');
+
+const getNumberLength = n => {};
+
+module.exports = function bankNumbers(joi) {
+    return {
+        name: 'bankNumbers',
+        base: joi.string(),
+        /* eslint-disable-next-line no-unused-vars */
+        coerce(value, state, options) {
+            if (isString(value)) {
+                // Strip out any non-numeric characters (eg. -)
+                return value.replace(/\D/g, '');
+            }
+            return value;
+        },
+        rules: [
+            {
+                name: 'sortCode',
+                params: {
+                    requiredLength: joi.number().required()
+                },
+                validate(params, value, state, options) {
+                    const codeLength = value.toString().length;
+                    if (codeLength !== params.requiredLength) {
+                        return this.createError(
+                            'sortCode.wrongSize',
+                            { v: value },
+                            state,
+                            options
+                        );
+                    }
+                    return value;
+                }
+            },
+            {
+                name: 'accountNumber',
+                params: {
+                    requiredLength: joi.number().required()
+                },
+                validate(params, value, state, options) {
+                    const codeLength = value.toString().length;
+                    if (codeLength !== params.requiredLength) {
+                        return this.createError(
+                            'accountNumber.wrongSize',
+                            { v: value },
+                            state,
+                            options
+                        );
+                    }
+                    return value;
+                }
+            }
+        ]
+    };
+};
