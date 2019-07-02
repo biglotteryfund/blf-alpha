1faf92e1ec4e3513096d04809fe42dbd3d8adfb4
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-07-02 10:23:16 +0100

    Add missing file

---
+++ assets/js/vue-apps/form-components/address-line.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    props: ['name', 'label', 'isRequired', 'value', 'size'],
+    computed: {
+        required() {
+            return this.isRequired === 'true';
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="ff-address__field">
+        <label class="ff-label"
+               :for="'field-' + name">
+            <span v-html="label"></span>
+            <!-- @TODO i18n -->
+            <span v-if="!required" class="ff-label-note">(Optional)</span>
+        </label>
+
+        <input
+            class="ff-text"
+            type="text"
+            :id="'field-' + name"
+            :name="name"
+            :value="value"
+            :required="required"
+            :aria-required="required"
+            :size="size || 40"
+        />
+    </div>
+</template>
