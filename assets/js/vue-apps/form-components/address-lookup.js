984fb39114a801c9070d5e32a1ab0ec9a3974dd7
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-07-03 09:56:58 +0100

    Remove empty code

--- assets/js/vue-apps/form-components/address-lookup.js
+++ assets/js/vue-apps/form-components/address-lookup.js
@@ -14,45 +14,6 @@ function init() {
                 'address-lookup': AddressLookup,
                 'conditional-radios': ConditionalRadios
             },
-            data() {
-                return {
-                    // showFallbackFields: false,
-                    // showSummaryLabel: true
-                };
-            },
-            methods: {
-                // handleAddress(data) {
-                //     this.showFallbackFields = false;
-                //     this.showSummaryLabel = false;
-                //     this.$refs.addressLine1.value = data.line1;
-                //     this.$refs.addressLine2.value = data.line2 || '';
-                //     this.$refs.townCity.value = data.townCity;
-                //     this.$refs.county.value = data.county;
-                //     this.$refs.postcode.value = data.postcode;
-                // },
-                // clearAddress() {
-                //     this.$refs.addressLine1.value = null;
-                //     this.$refs.addressLine2.value = null;
-                //     this.$refs.townCity.value = null;
-                //     this.$refs.county.value = null;
-                //     this.$refs.postcode.value = null;
-                //     this.showFallbackFields = false;
-                //     this.showSummaryLabel = true;
-                // },
-                // handleFallback() {
-                //     this.showFallbackFields = true;
-                //     this.showSummaryLabel = false;
-                // }
-            },
-            watch: {
-                // showFallbackFields() {
-                //     // Inform the address component when the fallback is toggled
-                //     // so we can toggle the postcode's required status
-                //     this.$refs.addressLookup.trackFallbackState(
-                //         this.showFallbackFields
-                //     );
-                // }
-            },
             mounted() {
                 // Clear out <noscript> fallbacks otherwise they submit as well as the
                 // Vue-enhanced fields (eg. double inputs)
