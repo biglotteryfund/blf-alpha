5f59eb5865f5a345e52b4c8f650f09a254d0d32d
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-07-19 15:47:32 +0100

    Add failing schema test with expected validations

---
+++ controllers/user/lib/account-schemas.test.js
@@ -0,0 +1,58 @@
+/* eslint-env jest */
+'use strict';
+const sample = require('lodash/sample');
+const commonPasswords = require('./common-passwords');
+const { newAccounts } = require('./account-schemas');
+const validateSchema = require('./validate-schema');
+
+describe('account schemas', () => {
+    test('new accounts', () => {
+        const username = 'example@example.com';
+        const commonPassword = sample(commonPasswords);
+        const weakPassword = 'password321';
+
+        expect(
+            validateSchema(newAccounts('en'), {
+                username: username,
+                password: username,
+                passwordConfirmation: username
+            }).messages
+        ).toEqual([
+            {
+                param: 'password',
+                type: 'any.invalid',
+                msg: expect.stringContaining(
+                    'Password is the same as your username'
+                )
+            }
+        ]);
+
+        expect(
+            validateSchema(newAccounts('en'), {
+                username: username,
+                password: commonPassword,
+                passwordConfirmation: commonPassword
+            }).messages
+        ).toEqual([
+            {
+                param: 'password',
+                type: 'password.common',
+                msg: expect.stringContaining('Password is too weak')
+            }
+        ]);
+
+        expect(
+            validateSchema(newAccounts('en'), {
+                username: username,
+                password: weakPassword,
+                passwordConfirmation: weakPassword
+            }).messages
+        ).toEqual([
+            {
+                param: 'password',
+                type: 'password.strength',
+                msg: expect.stringContaining('Password is too weak')
+            }
+        ]);
+    });
+});
