e0d2ed2d2dc88cd36d16c5426c452527fed2b9f9
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-07-19 09:14:47 +0100

    Enable console logs in test environment

--- common/logger.js
+++ common/logger.js
@@ -3,18 +3,10 @@ const { createLogger, format, transports } = require('winston');
 const WinstonCloudWatch = require('winston-cloudwatch');
 const features = require('config').get('features');
 
-const { isDev, isTestServer, environment, buildNumber } = require('./appData');
-
-function enableCloudWatchLogs() {
-    if (isTestServer) {
-        return false;
-    } else {
-        return features.enableCloudWatchLogs;
-    }
-}
+const { isDev, environment, buildNumber } = require('./appData');
 
 function transport() {
-    if (enableCloudWatchLogs()) {
+    if (features.enableCloudWatchLogs) {
         return new WinstonCloudWatch({
             awsRegion: 'eu-west-2',
             logGroupName: `/tnlcf/${environment}/app`,
@@ -24,7 +16,6 @@ function transport() {
         });
     } else {
         return new transports.Console({
-            silent: isTestServer,
             format: format.combine(format.colorize(), format.simple())
         });
     }
