72f795a80186c4b1d53446725b39f12d05e4c656
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-07-19 11:51:41 +0100

    Improve console logging

--- common/logger.js
+++ common/logger.js
@@ -1,28 +1,38 @@
 'use strict';
+const config = require('config');
 const { createLogger, format, transports } = require('winston');
 const WinstonCloudWatch = require('winston-cloudwatch');
-const features = require('config').get('features');
 
-const { isDev, environment, buildNumber } = require('./appData');
+const { environment, buildNumber } = require('./appData');
 
-function transport() {
-    if (features.enableCloudWatchLogs) {
-        return new WinstonCloudWatch({
-            awsRegion: 'eu-west-2',
-            logGroupName: `/tnlcf/${environment}/app`,
-            logStreamName: `build-${buildNumber}`,
-            jsonMessage: true,
-            retentionInDays: 30
-        });
+function getTransports() {
+    if (config.get('features.enableCloudWatchLogs')) {
+        return [
+            new WinstonCloudWatch({
+                awsRegion: config.get('aws.region'),
+                logGroupName: `/tnlcf/${environment}/app`,
+                logStreamName: `build-${buildNumber}`,
+                jsonMessage: true,
+                retentionInDays: 30
+            })
+        ];
     } else {
-        return new transports.Console({
-            format: format.combine(format.colorize(), format.simple())
-        });
+        return [
+            new transports.Console({
+                format: format.combine(
+                    format.colorize(),
+                    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                    format.printf(function(info) {
+                        return `${info.timestamp} [${info.level}] ${info.message}`;
+                    })
+                )
+            })
+        ];
     }
 }
 
 module.exports = createLogger({
-    level: isDev ? 'debug' : 'info',
+    level: process.env.LOG_LEVEL || config.get('logLevel'),
     format: format.combine(format.errors({ stack: true }), format.json()),
-    transports: [transport()]
+    transports: getTransports()
 });
