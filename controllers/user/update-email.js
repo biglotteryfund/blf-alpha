e0770ff8d24fbf9d97c365f5413d665620b42090
David Rapson <david.rapson@biglotteryfund.org.uk>
2019-07-23 14:23:29 +0100

    Lift handler method up

--- controllers/user/update-email.js
+++ controllers/user/update-email.js
@@ -25,62 +25,62 @@ function render(req, res, data = null, errors = []) {
     });
 }
 
+async function handleSubmission(req, res, next) {
+    const validationResult = validateSchema(
+        emailOnly(req.i18n.getLocale()),
+        req.body
+    );
+
+    if (validationResult.isValid) {
+        try {
+            const { username } = validationResult.value;
+            const existingUser = await Users.findByUsername(username);
+            const passwordMatches = await Users.checkValidPassword(
+                req.user.userData.password,
+                req.body.password
+            );
+
+            /**
+             * Allow updating if the password confirmation matches
+             * and there is not an existing user with the requested email address
+             */
+            const canUpdate =
+                passwordMatches === true &&
+                username !== get('username')(existingUser);
+
+            if (canUpdate) {
+                const userId = req.user.userData.id;
+                await Users.updateNewEmail({
+                    id: userId,
+                    newEmail: username
+                });
+                const updatedUser = await Users.findById(userId);
+                await sendActivationEmail(req, updatedUser);
+                logger.info('Update email change successful');
+                redirectForLocale(req, res, '/user?s=emailUpdated');
+            } else {
+                logger.warn(`Invalid credentials when updating email address`);
+
+                /**
+                 * Return a generic error to avoid exposing that a user
+                 * exists with the requested email address
+                 */
+                render(req, res, validationResult.value, [
+                    { msg: res.locals.copy.genericError }
+                ]);
+            }
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        render(req, res, validationResult.value, validationResult.messages);
+    }
+}
+
 router
     .route('/')
     .all(requireUserAuth, csrfProtection, injectCopy('user.updateEmail'))
     .get(render)
-    .post(async function(req, res, next) {
-        const validationResult = validateSchema(
-            emailOnly(req.i18n.getLocale()),
-            req.body
-        );
-
-        if (validationResult.isValid) {
-            try {
-                const { username } = validationResult.value;
-                const existingUser = await Users.findByUsername(username);
-                const passwordMatches = await Users.checkValidPassword(
-                    req.user.userData.password,
-                    req.body.password
-                );
-
-                /**
-                 * Allow updating if the password confirmation matches
-                 * and there is not an existing user with the requested email address
-                 */
-                const canUpdate =
-                    passwordMatches === true &&
-                    username !== get('username')(existingUser);
-
-                if (canUpdate) {
-                    const userId = req.user.userData.id;
-                    await Users.updateNewEmail({
-                        id: userId,
-                        newEmail: username
-                    });
-                    const updatedUser = await Users.findById(userId);
-                    await sendActivationEmail(req, updatedUser);
-                    logger.info('Update email change successful');
-                    redirectForLocale(req, res, '/user?s=emailUpdated');
-                } else {
-                    logger.warn(
-                        `Invalid credentials when updating email address`
-                    );
-
-                    /**
-                     * Return a generic error to avoid exposing that a user
-                     * exists with the requested email address
-                     */
-                    render(req, res, validationResult.value, [
-                        { msg: res.locals.copy.genericError }
-                    ]);
-                }
-            } catch (error) {
-                next(error);
-            }
-        } else {
-            render(req, res, validationResult.value, validationResult.messages);
-        }
-    });
+    .post(handleSubmission);
 
 module.exports = router;
