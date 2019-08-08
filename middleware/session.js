e2107d073746c6874aba7234d56bc887e9512d6b
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2019-08-08 16:46:23 +0100

    Extend session duration to two hours

--- middleware/session.js
+++ middleware/session.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
     /**
      * Configure session
      */
-    const IDLE_TIMEOUT_MINUTES = 60;
+    const IDLE_TIMEOUT_MINUTES = 120;
     const sessionConfig = {
         store: store,
         name: config.get('cookies.session'),
