476cacd2451f9e5758a052954e960b7f1e6cb6a6
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-08-12 08:16:59 +0100

    Match format of other extensions

--- controllers/apply/form-router-next/joi-extensions/phone-number.js
+++ controllers/apply/form-router-next/joi-extensions/phone-number.js
@@ -6,31 +6,40 @@ const phoneUtil = PhoneNumberUtil.getInstance();
 /**
  * Allows you to do `Joi.string().phoneNumber()`
  *
- * @param {Object} joi Joi instance provided by Joi
+ * @param {Object} joi Joi instance
  * @return {Object} Joi plugin object
  */
-module.exports = joi => ({
-    base: joi.string(),
-    name: 'string',
-    language: {
-        phonenumber: 'did not seem to be a phone number'
-    },
-    rules: [
-        {
-            name: 'phoneNumber',
-            validate(params, value, state, options) {
-                try {
-                    const parsedValue = phoneUtil.parse(value, 'GB');
-                    if (phoneUtil.isValidNumber(parsedValue)) {
-                        if (options.convert) {
-                            return phoneUtil.format(
-                                parsedValue,
-                                PhoneNumberFormat.NATIONAL
-                            );
+module.exports = function phoneNumber(joi) {
+    return {
+        base: joi.string(),
+        name: 'string',
+        language: {
+            phonenumber: 'did not seem to be a phone number'
+        },
+        rules: [
+            {
+                name: 'phoneNumber',
+                validate(params, value, state, options) {
+                    try {
+                        const parsedValue = phoneUtil.parse(value, 'GB');
+                        if (phoneUtil.isValidNumber(parsedValue)) {
+                            if (options.convert) {
+                                return phoneUtil.format(
+                                    parsedValue,
+                                    PhoneNumberFormat.NATIONAL
+                                );
+                            } else {
+                                return value;
+                            }
                         } else {
-                            return value;
+                            return this.createError(
+                                'string.phonenumber',
+                                { value },
+                                state,
+                                options
+                            );
                         }
-                    } else {
+                    } catch (err) {
                         return this.createError(
                             'string.phonenumber',
                             { value },
@@ -38,15 +47,8 @@ module.exports = joi => ({
                             options
                         );
                     }
-                } catch (err) {
-                    return this.createError(
-                        'string.phonenumber',
-                        { value },
-                        state,
-                        options
-                    );
                 }
             }
-        }
-    ]
-});
+        ]
+    };
+};
