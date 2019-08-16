76791d0c8edf124652d7924f7341549e0c8fbed8
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-08-16 15:04:16 +0100

    Project idea question translation

--- controllers/apply/awards-for-all/fields.js
+++ controllers/apply/awards-for-all/fields.js
@@ -536,8 +536,8 @@ module.exports = function fieldsFor({ locale, data = {} }) {
         return {
             name: 'yourIdeaProject',
             label: localise({
-                en: 'What would you like to do?',
-                cy: ''
+                en: `What would you like to do?`,
+                cy: `Beth yr hoffech ei wneud?`
             }),
             explanation: localise({
                 en: `<p><strong>
@@ -558,7 +558,28 @@ module.exports = function fieldsFor({ locale, data = {} }) {
                     You can write up to ${maxWords} words for this section,
                     but don't worry if you use less.
                 </strong></p>`,
-                cy: ''
+
+                cy: `<p><strong>
+                    Dyma rhai syniadau o’r hyn i ddweud wrthym am eich prosiect:
+                </strong></p>
+                <ul>
+                    <li>Beth y hoffech ei wneud?</li>
+                    <li>Pa wahaniaeth bydd eich prosiect yn ei wneud?</li>
+                    <li>Pwy fydd yn cael budd ohono?</li>
+                    <li>Am ba mor hir rydych yn disgwyl ei gynnal?
+                        Gall hyn fod yn amcangyfrif.</li> 
+                    <li>Sut y byddwch yn sicrhau fod pobl yn gwybod amdano?</li>
+                    <li>Sut rydych yn bwriadu dysgu ohono a defnyddio’r dysg hwn
+                        i siapio prosiectau yn y dyfodol?</li>
+                    <li>A yw’n rhywbeth newydd, neu ydych yn parhau â rhywbeth
+                        sydd wedi gweithio’n dda yn flaenorol? Rydym eisiau
+                        ariannu’r ddau fath o brosiectau.</li>
+
+                </ul>
+                <p><strong>
+                    Gallwch ysgrifennu hyd at ${maxWords} gair i’r adran hon,
+                    ond mae modd ysgrifennu llai.
+                </strong></p>`
             }),
             type: 'textarea',
             settings: {
@@ -578,21 +599,21 @@ module.exports = function fieldsFor({ locale, data = {} }) {
                     type: 'base',
                     message: localise({
                         en: 'Tell us about your project',
-                        cy: ''
+                        cy: 'Dywedwch wrthym am eich prosiect'
                     })
                 },
                 {
                     type: 'string.minWords',
                     message: localise({
                         en: `Answer must be at least ${minWords} words`,
-                        cy: ''
+                        cy: `Rhaid i’r ateb fod yn o leiaf ${minWords} gair`
                     })
                 },
                 {
                     type: 'string.maxWords',
                     message: localise({
                         en: `Answer must be no more than ${maxWords} words`,
-                        cy: ''
+                        cy: `Rhaid i’r ateb fod yn llai na ${maxWords} gair`
                     })
                 }
             ]
