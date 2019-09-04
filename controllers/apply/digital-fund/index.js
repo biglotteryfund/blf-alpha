abb78632ea9f379602910b560810d7e390da764b
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-09-04 12:02:19 +0100

    Remove digital fund form code

--- controllers/apply/digital-fund/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const { initFormRouter } = require('../form-router');
-const formModel = require('./form-model');
-
-module.exports = {
-    strand1: initFormRouter(formModel.strand1),
-    strand2: initFormRouter(formModel.strand2)
-};
