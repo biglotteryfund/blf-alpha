52ba3dee1e8d8fbc1ddab5ec6ad04441bc97f868
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-09-05 11:56:03 +0100

    Revert "Re-test wait in place of sleep"
    This reverts commit 523674dd7798c19efa803fcc74df7dbd4a540b53.

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -14,7 +14,7 @@ if [[ $DEPLOYMENT_GROUP_NAME =~ $TEST_FLEET ]] ||
 then
     apt-get install -y clamav clamav-daemon
     service clamav-freshclam restart
-    wait
+    sleep 3m
     service clamav-daemon start
     service clamav-daemon status
 fi
