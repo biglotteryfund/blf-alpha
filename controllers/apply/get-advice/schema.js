26e98ebba398f00c7ae6dba4a50c54075f805c94
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-09-06 10:23:38 +0100

    Project duration years schema

--- controllers/apply/get-advice/schema.js
+++ controllers/apply/get-advice/schema.js
@@ -1,6 +1,24 @@
 'use strict';
 const Joi = require('../lib/joi-extensions');
 
+function singleArrayItem(item) {
+    return Joi.array()
+        .items(
+            Joi.string()
+                .only(item)
+                .required()
+        )
+        .required();
+}
+
+function numberRange(min, max) {
+    return Joi.number()
+        .integer()
+        .required()
+        .min(min)
+        .max(max);
+}
+
 const schema = Joi.object({
     projectCountry: Joi.array()
         .items(
@@ -11,7 +29,8 @@ const schema = Joi.object({
                 'wales'
             ])
         )
-        .single(),
+        .single()
+        .required(),
     projectLocation: Joi.when('projectCountry', {
         is: Joi.array().min(2),
         then: Joi.any().strip(),
@@ -22,9 +41,16 @@ const schema = Joi.object({
         .optional(),
     projectCosts: Joi.friendlyNumber()
         .integer()
-        .required()
         .min(10000)
-        .required()
+        .required(),
+    projectDurationYears: Joi.when('projectCountry', {
+        is: Joi.array().min(2),
+        then: Joi.any().strip()
+    }).when('projectCountry', {
+        is: singleArrayItem('scotland'),
+        then: numberRange(3, 5),
+        otherwise: numberRange(1, 5)
+    })
 });
 
 module.exports = schema;
--- controllers/apply/get-advice/schema.test.js
+++ controllers/apply/get-advice/schema.test.js
@@ -2,6 +2,8 @@
 // @ts-nocheck
 'use strict';
 const omit = require('lodash/omit');
+const random = require('lodash/random');
+const faker = require('faker');
 
 const schema = require('./schema');
 
@@ -20,7 +22,8 @@ function mockResponse(overrides) {
         projectCountry: 'england',
         projectLocation: 'placeholder-location',
         projectLocationDescription: 'optional description',
-        projectCosts: '250,000'
+        projectCosts: '250,000',
+        projectDurationYears: 3
     };
 
     return Object.assign(defaults, overrides);
@@ -34,7 +37,8 @@ test('minimal valid form', () => {
         projectCountry: ['england'],
         projectLocation: 'placeholder-location',
         projectLocationDescription: 'optional description',
-        projectCosts: 250000
+        projectCosts: 250000,
+        projectDurationYears: 3
     });
 });
 
@@ -42,13 +46,15 @@ test('minimal invalid form', () => {
     const result = validate({
         projectCountry: 'invalid-country',
         projectLocation: null,
-        projectCosts: 5000
+        projectCosts: 5000,
+        projectDurationYears: 10
     });
 
     expect(mapMessages(result)).toEqual([
         '"projectCountry" must be one of [england, scotland, northern-ireland, wales]',
         '"projectLocation" must be a string',
-        '"projectCosts" must be larger than or equal to 10000'
+        '"projectCosts" must be larger than or equal to 10000',
+        '"projectDurationYears" must be less than or equal to 5'
     ]);
 });
 
@@ -67,4 +73,52 @@ test('optional project location description', () => {
     const expected = omit(mockResponse(), 'projectLocationDescription');
     const result = validate(expected);
     expect(result.error).toBeNull();
+});
+
+test.each([
+    ['england', 1, 5],
+    ['northern-ireland', 1, 5],
+    ['scotland', 3, 5],
+    ['wales', 1, 5]
+])('project duration in %p is %p–%p years', function(country, min, max) {
+    const result = validate(
+        mockResponse({
+            projectCountry: country,
+            projectDurationYears: random(min, max)
+        })
+    );
+    expect(result.error).toBeNull();
+
+    const resultMin = validate({
+        projectCountry: country,
+        projectDurationYears: min - 1
+    });
+
+    expect(mapMessages(resultMin)).toEqual(
+        expect.arrayContaining([
+            `"projectDurationYears" must be larger than or equal to ${min}`
+        ])
+    );
+
+    const resultMax = validate({
+        projectCountry: country,
+        projectDurationYears: max + 1
+    });
+
+    expect(mapMessages(resultMax)).toEqual(
+        expect.arrayContaining([
+            `"projectDurationYears" must be less than or equal to ${max}`
+        ])
+    );
+});
+
+test('strip project duration when applying for more than one country', () => {
+    const result = validate(
+        mockResponse({
+            projectCountry: ['england', 'scotland'],
+            projectDurationYears: 'this-should-be-stripped'
+        })
+    );
+
+    expect(result.value).not.toHaveProperty('projectDurationYears');
 });
