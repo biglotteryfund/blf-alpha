13fd4fffac527be3e835a618072f0518ede012f7
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-09-18 15:58:08 +0100

    Move validate method into form code

--- controllers/apply/get-advice/docs/schema.test.js
+++ controllers/apply/get-advice/docs/schema.test.js
@@ -4,14 +4,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const schema = require('../schema');
+const formBuilder = require('../form');
+
+const form = formBuilder();
 
 const docContents = fs.readFileSync(
     path.resolve(__dirname, './schema.md'),
     'utf8'
 );
 
-test.each(Object.keys(schema.describe().children))(
+test.each(Object.keys(form.schema.describe().children))(
     '%s has documentation entry',
     fieldName => {
         const fieldNameRegexp = new RegExp(`### ${fieldName}`);
