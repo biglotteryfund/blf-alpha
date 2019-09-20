79b0db8004d338058a2a863441c98c22a3b68732
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-09-20 11:29:42 +0100

    Email and phone field type

--- controllers/apply/lib/field-types.js
+++ controllers/apply/lib/field-types.js
@@ -1,8 +1,12 @@
 'use strict';
+const get = require('lodash/fp/get');
+const { oneLine } = require('common-tags');
 const Joi = require('./joi-extensions');
 
 class TextField {
-    constructor(props) {
+    constructor(props, locale = 'en') {
+        this.locale = locale;
+
         if (props.name) {
             this.name = props.name;
         } else {
@@ -37,11 +41,87 @@ class TextField {
         this.attributes = props.attributes || {};
         this.settings = props.settings || {};
     }
+    localise(msg) {
+        return get(this.locale)(msg);
+    }
+}
+
+class EmailField extends TextField {
+    constructor(props, locale) {
+        super(props, locale);
+        this.type = 'email';
+
+        if (props.schema) {
+            this.schema = props.schema;
+        } else {
+            this.schema = this.isRequired
+                ? Joi.string()
+                      .email()
+                      .required()
+                : Joi.string()
+                      .email()
+                      .optional();
+        }
+
+        this.messages = [
+            {
+                type: 'base',
+                message: this.localise({
+                    en: 'Enter an email address',
+                    cy: 'Rhowch gyfeiriad e-bost'
+                })
+            },
+            {
+                type: 'string.email',
+                message: this.localise({
+                    en: oneLine`Email address must be in the correct format,
+                        like name@example.com`,
+                    cy: oneLine`Rhaid i’r cyfeiriad e-bost for yn y ffurf cywir,
+                        e.e enw@example.com`
+                })
+            }
+        ];
+    }
+}
+
+class PhoneField extends TextField {
+    constructor(props, locale) {
+        super(props, locale);
+
+        if (props.schema) {
+            this.schema = props.schema;
+        } else {
+            this.schema = this.isRequired
+                ? Joi.string().phoneNumber()
+                : Joi.string()
+                      .phoneNumber()
+                      .allow('')
+                      .optional();
+        }
+
+        this.messages = [
+            {
+                type: 'base',
+                message: this.localise({
+                    en: 'Enter a UK telephone number',
+                    cy: 'Rhowch rif ffôn Prydeinig'
+                })
+            },
+            {
+                type: 'string.phonenumber',
+                message: this.localise({
+                    en: 'Enter a real UK telephone number',
+                    cy: 'Rhowch rif ffôn Prydeinig go iawn'
+                })
+            }
+        ];
+    }
 }
 
 class RadioField extends TextField {
-    constructor(props) {
-        super(props);
+    constructor(props, locale) {
+        super(props, locale);
+
         this.type = 'radio';
 
         const options = props.options || [];
@@ -67,5 +147,7 @@ class RadioField extends TextField {
 
 module.exports = {
     TextField,
+    EmailField,
+    PhoneField,
     RadioField
 };
