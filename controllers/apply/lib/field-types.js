6d43a32b978b59117a9bd92a44f6fe220b0a1b59
David Rapson <david@davidrapson.co.uk>
2019-09-24 14:24:14 +0100

    Name field type

--- controllers/apply/lib/field-types.js
+++ controllers/apply/lib/field-types.js
@@ -536,6 +536,77 @@ class AddressField extends TextField {
     }
 }
 
+class NameField extends TextField {
+    constructor(props, locale) {
+        super(props, locale);
+
+        this.type = 'full-name';
+
+        if (props.schema) {
+            this.schema = props.schema;
+        } else {
+            this.schema = Joi.object({
+                firstName: Joi.string()
+                    .max(40)
+                    .required(),
+                lastName: Joi.string()
+                    .max(80)
+                    .required()
+            });
+        }
+
+        this.messages = [
+            {
+                type: 'base',
+                message: this.localise({
+                    en: 'Enter first and last name',
+                    cy: 'Rhowch enw cyntaf a chyfenw'
+                })
+            },
+            {
+                type: 'any.empty',
+                key: 'firstName',
+                message: this.localise({
+                    en: 'Enter first name',
+                    cy: 'Rhowch enw cyntaf'
+                })
+            },
+            {
+                type: 'string.max',
+                key: 'firstName',
+                message: this.localise({
+                    en: `First name must be 40 characters or less`,
+                    cy: `Rhaid i’r enw cyntaf fod yn llai na 40 nod`
+                })
+            },
+            {
+                type: 'string.max',
+                key: 'lastName',
+                message: this.localise({
+                    en: `Last name must be 80 characters or less`,
+                    cy: `Rhaid i’r cyfenw fod yn llai na 80 nod`
+                })
+            },
+            {
+                type: 'any.empty',
+                key: 'lastName',
+                message: this.localise({
+                    en: 'Enter last name',
+                    cy: 'Rhowch gyfenw'
+                })
+            }
+        ].concat(props.messages || []);
+    }
+
+    get displayValue() {
+        if (this.value) {
+            return `${this.value.firstName} ${this.value.lastName}`;
+        } else {
+            return '';
+        }
+    }
+}
+
 module.exports = {
     TextField,
     EmailField,
@@ -545,5 +616,6 @@ module.exports = {
     RadioField,
     CheckboxField,
     SelectField,
-    AddressField
+    AddressField,
+    NameField
 };
--- controllers/apply/lib/field-types.test.js
+++ controllers/apply/lib/field-types.test.js
@@ -11,7 +11,8 @@ const {
     RadioField,
     CheckboxField,
     SelectField,
-    AddressField
+    AddressField,
+    NameField
 } = require('./field-types');
 
 test('TextField', function() {
@@ -231,4 +232,15 @@ test('AddressField', function() {
     expect(field.displayValue).toBe(
         '1234 example street,\nBirmingham,\nWest Midlands,\nB15 1TR'
     );
+});
+
+test('NameField', function() {
+    const field = new NameField({
+        name: 'example',
+        label: 'Name field'
+    });
+
+    expect(field.type).toBe('full-name');
+    field.withValue({ firstName: 'Björk', lastName: 'Guðmundsdóttir' });
+    expect(field.displayValue).toBe('Björk Guðmundsdóttir');
 });
