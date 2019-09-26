f1d05773ee6295f733982cbd528c6f9f5dc4bcd8
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-09-26 09:35:27 +0100

    Basic confirmation builder

--- controllers/apply/get-advice/index.js
+++ controllers/apply/get-advice/index.js
@@ -3,10 +3,12 @@ const path = require('path');
 const { initFormRouter } = require('../form-router-next');
 
 const formBuilder = require('./form');
+const confirmationBuilder = require('./confirmation');
 
 module.exports = initFormRouter({
     formId: 'get-advice',
     formBuilder: formBuilder,
     startTemplate: path.resolve(__dirname, './views/startpage.njk'),
+    confirmationBuilder: confirmationBuilder,
     enableSalesforceConnector: false
 });
