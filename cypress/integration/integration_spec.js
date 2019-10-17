ab8f7f998a2db05a4ee6fb760ca98d2da871e3ea
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-10-17 16:44:44 +0100

    Randomise cypress test for salesforce testing

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -1064,11 +1064,13 @@ it('should submit full awards for all application', () => {
     });
 });
 
-it('should complete get advice form', () => {
+it.only('should complete get advice form', () => {
     const mock = {
         projectName: faker.lorem.words(5),
-        projectCountries: ['England'],
-        projectLocation: 'Derbyshire',
+        projectCountries: sampleSize(
+            ['England', 'Scotland', 'Northern Ireland'],
+            random(1, 2)
+        ),
         projectLocationDescription: faker.lorem.words(5),
         projectCosts: random(10000, 5000000),
         projectDurationYears: sample(['3 years', '4 years', '5 years']),
@@ -1127,9 +1129,24 @@ it('should complete get advice form', () => {
 
         submitStep();
 
-        cy.findByLabelText('Where will your project take place?').select(
-            mock.projectLocation
-        );
+        function randomProjectLocation() {
+            if (mock.projectCountries.includes('Northern Ireland')) {
+                return 'Derry and Strabane';
+            } else if (mock.projectCountries.includes('Scotland')) {
+                return 'Highland';
+            } else if (mock.projectCountries.includes('Wales')) {
+                return 'Caerphilly';
+            } else {
+                return 'Bath and North East Somerset';
+            }
+        }
+
+        if (mock.projectCountries.length === 1) {
+            cy.findByLabelText('Where will your project take place?').select(
+                randomProjectLocation()
+            );
+        }
+
         cy.findByLabelText('Project location', { exact: false }).type(
             mock.projectLocationDescription
         );
@@ -1141,8 +1158,10 @@ it('should complete get advice form', () => {
         );
         submitStep();
 
-        cy.findByLabelText(mock.projectDurationYears).click();
-        submitStep();
+        if (mock.projectCountries.length === 1) {
+            cy.findByLabelText(mock.projectDurationYears).click();
+            submitStep();
+        }
 
         cy.findByLabelText('What would you like to do?')
             .invoke('val', mock.yourIdeaProject)
@@ -1211,6 +1230,12 @@ it('should complete get advice form', () => {
         cy.findAllByText('Nearly done', { exact: false })
             .first()
             .should('exist');
+
+        cy.findAllByText('Submit application')
+            .first()
+            .click();
+
+        cy.get('h1').should('contain', 'Thanks for telling us about your idea');
     });
 });
 
