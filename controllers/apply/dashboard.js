c1ddb9d6558131b1acbb35da8272b23bea36b78e
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-10-21 17:03:18 +0100

    Fixup overzealous middleware

--- controllers/apply/dashboard.js
+++ controllers/apply/dashboard.js
@@ -13,29 +13,24 @@ const { enrichPending, enrichSubmitted } = require('./lib/enrich-application');
 
 const router = express.Router();
 
-router.use(
-    noStore,
-    requireActiveUser,
-    injectCopy('applyNext.dashboardNew'),
-    function(req, res, next) {
-        res.locals.userNavigationLinks = [
-            {
-                url: req.baseUrl,
-                label: 'Latest application'
-            },
-            {
-                url: `${req.baseUrl}/all`,
-                label: 'All applications'
-            },
-            {
-                url: localify(req.i18n.getLocale())('/user'),
-                label: 'Account'
-            }
-        ];
+function injectNavigationLinks(req, res, next) {
+    res.locals.userNavigationLinks = [
+        {
+            url: req.baseUrl,
+            label: 'Latest application'
+        },
+        {
+            url: `${req.baseUrl}/all`,
+            label: 'All applications'
+        },
+        {
+            url: localify(req.i18n.getLocale())('/user'),
+            label: 'Account'
+        }
+    ];
 
-        next();
-    }
-);
+    next();
+}
 
 /**
  * Determine the latest application to show and
@@ -60,62 +55,79 @@ async function getLatestApplication(userId, locale) {
     }
 }
 
-router.get('/', async function(req, res, next) {
-    const { copy } = res.locals;
+router.get(
+    '/',
+    noStore,
+    requireActiveUser,
+    injectCopy('applyNext.dashboardNew'),
+    injectNavigationLinks,
+    async function(req, res, next) {
+        const { copy } = res.locals;
 
-    try {
-        /**
-         * Check for existing pending applications
-         * Used to determine if "start a new application" action card
-         * is in a primary or secondary style.
-         * Secondary if we have a pending application for the product
-         */
-        const [pendingSimple, pendingStandard] = await Promise.all([
-            PendingApplication.findUserApplicationsByForm({
-                userId: req.user.id,
-                formId: 'awards-for-all'
-            }),
-            PendingApplication.findUserApplicationsByForm({
-                userId: req.user.id,
-                formId: 'standard-enquiry'
-            })
-        ]);
+        try {
+            /**
+             * Check for existing pending applications
+             * Used to determine if "start a new application" action card
+             * is in a primary or secondary style.
+             * Secondary if we have a pending application for the product
+             */
+            const [pendingSimple, pendingStandard] = await Promise.all([
+                PendingApplication.findUserApplicationsByForm({
+                    userId: req.user.id,
+                    formId: 'awards-for-all'
+                }),
+                PendingApplication.findUserApplicationsByForm({
+                    userId: req.user.id,
+                    formId: 'standard-enquiry'
+                })
+            ]);
 
-        res.render(path.resolve(__dirname, './views/dashboard'), {
-            title: copy.latest.title,
-            latestApplication: await getLatestApplication(
-                req.user.id,
-                req.i18n.getLocale()
-            ),
-            hasPendingSimpleApplication: !isEmpty(pendingSimple),
-            hasPendingStandardApplication: !isEmpty(pendingStandard)
-        });
-    } catch (err) {
-        next(err);
+            res.render(path.resolve(__dirname, './views/dashboard'), {
+                title: copy.latest.title,
+                latestApplication: await getLatestApplication(
+                    req.user.id,
+                    req.i18n.getLocale()
+                ),
+                hasPendingSimpleApplication: !isEmpty(pendingSimple),
+                hasPendingStandardApplication: !isEmpty(pendingStandard)
+            });
+        } catch (err) {
+            next(err);
+        }
     }
-});
+);
 
-router.get('/all', async function(req, res, next) {
-    const { copy } = res.locals;
+router.get(
+    '/all',
+    noStore,
+    requireActiveUser,
+    injectCopy('applyNext.dashboardNew'),
+    injectNavigationLinks,
+    async function(req, res, next) {
+        const { copy } = res.locals;
 
-    try {
-        const [pendingApplications, submittedApplications] = await Promise.all([
-            PendingApplication.findAllByUserId(req.user.id),
-            SubmittedApplication.findAllByUserId(req.user.id)
-        ]);
+        try {
+            const [
+                pendingApplications,
+                submittedApplications
+            ] = await Promise.all([
+                PendingApplication.findAllByUserId(req.user.id),
+                SubmittedApplication.findAllByUserId(req.user.id)
+            ]);
 
-        res.render(path.resolve(__dirname, './views/dashboard-all'), {
-            title: copy.all.title,
-            pendingApplications: pendingApplications.map(application =>
-                enrichPending(application, req.i18n.getLocale())
-            ),
-            submittedApplications: submittedApplications.map(application =>
-                enrichSubmitted(application, req.i18n.getLocale())
-            )
-        });
-    } catch (err) {
-        next(err);
+            res.render(path.resolve(__dirname, './views/dashboard-all'), {
+                title: copy.all.title,
+                pendingApplications: pendingApplications.map(application =>
+                    enrichPending(application, req.i18n.getLocale())
+                ),
+                submittedApplications: submittedApplications.map(application =>
+                    enrichSubmitted(application, req.i18n.getLocale())
+                )
+            });
+        } catch (err) {
+            next(err);
+        }
     }
-});
+);
 
 module.exports = router;
