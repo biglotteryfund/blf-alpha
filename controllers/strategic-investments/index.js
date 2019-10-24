58b90a0b9d794f4cf8ebb45d20daec5d470c3257
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-10-24 16:57:37 +0100

    Inline injectStrategicProgramme

--- controllers/strategic-investments/index.js
+++ controllers/strategic-investments/index.js
@@ -1,8 +1,7 @@
 'use strict';
 const path = require('path');
 const express = require('express');
-const get = require('lodash/get');
-const set = require('lodash/set');
+const compact = require('lodash/compact');
 
 const {
     injectBreadcrumbs,
@@ -40,69 +39,50 @@ router.get('/', injectListingContent, async function(req, res, next) {
     }
 });
 
-async function injectStrategicProgramme(req, res, next) {
+router.get('/:slug/:child_slug?', async function(req, res, next) {
     try {
-        // Assumes a parameter of :slug and :childPageSlug? in the request
-        const { slug, childPageSlug } = req.params;
-        if (slug) {
-            const querySlug = childPageSlug ? `${slug}/${childPageSlug}` : slug;
+        const entry = await contentApi.getStrategicProgrammes({
+            slug: compact([req.params.slug, req.params.child_slug]).join('/'),
+            locale: req.i18n.getLocale(),
+            requestParams: req.query
+        });
 
-            const entry = await contentApi.getStrategicProgrammes({
-                slug: querySlug,
-                locale: req.i18n.getLocale(),
-                requestParams: req.query
+        setCommonLocals({ res, entry });
+
+        /**
+         * Render a plain content page if specified,
+         * otherwise render a standard Strategic page
+         */
+        if (entry.entryType === 'contentPage') {
+            const breadcrumbs = entry.parent
+                ? res.locals.breadcrumbs.concat([
+                      {
+                          label: entry.parent.title,
+                          url: entry.parent.linkUrl
+                      },
+                      { label: res.locals.title }
+                  ])
+                : res.locals.breadcrumbs.concat([{ label: res.locals.title }]);
+
+            res.render(
+                path.resolve(__dirname, '../common/views/flexible-content'),
+                {
+                    breadcrumbs: breadcrumbs,
+                    flexibleContent: entry.content
+                }
+            );
+        } else if (entry) {
+            res.render(path.resolve(__dirname, './views/strategic-programme'), {
+                strategicProgramme: entry,
+                breadcrumbs: res.locals.breadcrumbs.concat({
+                    label: res.locals.title
+                })
             });
-
-            res.locals.strategicProgramme = entry;
-            setCommonLocals({ res, entry });
-        }
-        next();
-    } catch (error) {
-        if (error.statusCode >= 500) {
-            next(error);
         } else {
             next();
         }
-    }
-}
-
-router.get('/:slug/:childPageSlug?', injectStrategicProgramme, function(
-    req,
-    res,
-    next
-) {
-    const { strategicProgramme } = res.locals;
-    // Render a plain content page if specified
-    if (get(strategicProgramme, 'entryType') === 'contentPage') {
-        setCommonLocals({ res, entry: strategicProgramme });
-        set(res.locals, 'content.flexibleContent', strategicProgramme.content);
-
-        const breadcrumbs = strategicProgramme.parent
-            ? res.locals.breadcrumbs.concat([
-                  {
-                      label: strategicProgramme.parent.title,
-                      url: strategicProgramme.parent.linkUrl
-                  },
-                  { label: res.locals.title }
-              ])
-            : res.locals.breadcrumbs.concat([{ label: res.locals.title }]);
-
-        res.render(
-            path.resolve(__dirname, '../common/views/flexible-content'),
-            {
-                breadcrumbs: breadcrumbs,
-                flexibleContent: strategicProgramme.content
-            }
-        );
-    } else if (strategicProgramme) {
-        // Render a standard Strategic page
-        res.render(path.resolve(__dirname, './views/strategic-programme'), {
-            breadcrumbs: res.locals.breadcrumbs.concat({
-                label: res.locals.title
-            })
-        });
-    } else {
-        next();
+    } catch (error) {
+        next(error);
     }
 });
 
