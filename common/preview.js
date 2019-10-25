244d936666a34b7361561ab8979c3b6b2991a9d6
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-10-25 13:59:32 +0100

    Extract checkPreviewMode method

--- common/preview.js
+++ common/preview.js
@@ -1,18 +1,18 @@
 'use strict';
 const { requireStaffAuth } = require('./authed');
+const checkPreviewMode = require('./check-preview-mode');
 
 /**
  * Preview mode
  * Staff-auth powered preview mode
  */
 module.exports = (req, res, next) => {
-    const isLivePreview =
-        req.query['x-craft-live-preview'] && req.query['token'];
-    const isShareLink = req.query['x-craft-preview'] && req.query['token'];
-    if (isLivePreview || isShareLink) {
+    const previewMode = checkPreviewMode(req.query);
+    if (previewMode.isPreview) {
         res.cacheControl = { noStore: true };
         res.locals.PREVIEW_MODE = true;
-        if (isLivePreview) {
+
+        if (previewMode.isLivePreview) {
             // Allow embedding the site via iframe for CMS live preview
             res.removeHeader('X-Frame-Options');
             // Skip staff check for live preview (these URLs are secured via tokens)
