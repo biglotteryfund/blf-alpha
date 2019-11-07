ef6f9ce901ba1cadd090b953ae43faeaf13f6324
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-11-07 12:55:14 +0000

    Write failing joi schema test

---
+++ controllers/materials/lib/material-fields.test.js
@@ -0,0 +1,41 @@
+/* eslint-env jest */
+'use strict';
+
+const { validateSchema } = require('./material-fields');
+
+function mapMessages(validationResult) {
+    return validationResult.error.details.map(detail => detail.message);
+}
+
+test('should validate material fields schema', () => {
+    const invalidResult = validateSchema({
+        yourEmail: 'notanemail@invalid'
+    });
+
+    expect(mapMessages(invalidResult)).toEqual([
+        '"yourName" is required',
+        '"yourEmail" must be a valid email',
+        '"yourAddress1" is required',
+        '"yourTown" is required',
+        '"yourCountry" is required',
+        '"yourPostcode" is required'
+    ]);
+
+    const validResult = validateSchema({
+        yourName: 'Björk Guðmundsdóttir',
+        yourEmail: 'example@example.com',
+        yourAddress1: '3 Embassy Drive',
+        yourAddress2: '',
+        yourTown: 'Birmingham',
+        yourCounty: 'West Midlands',
+        yourCountry: 'England',
+        yourPostcode: 'B15 1TR',
+        yourProjectName: 'Example project name',
+        yourGrantAmount: 'under10k',
+        yourGrantAmountOther: '',
+        yourReason: 'other',
+        yourReasonOther: 'Other reason'
+    });
+
+    expect(validResult.error).toBeNull();
+});
