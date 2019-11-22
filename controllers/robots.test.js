5ac0f7b03e091c4f71c065cc438659eba3d876f5
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-11-22 09:58:01 +0000

    Streamline tests

--- controllers/robots.test.js
+++ controllers/robots.test.js
@@ -4,36 +4,24 @@
 const httpMocks = require('node-mocks-http');
 const robotsHandler = require('./robots');
 
-test('should allow indexing on live domain', () => {
-    const req = httpMocks.createRequest({
+function createRequestFor(host) {
+    return httpMocks.createRequest({
         method: 'GET',
         url: '/robots.txt',
-        headers: {
-            'Host': 'www.tnlcommunityfund.org.uk',
-            'X-Forwarded-Proto': 'https'
-        }
+        headers: { 'Host': host, 'X-Forwarded-Proto': 'https' }
     });
+}
 
+test('should allow indexing on live domain', () => {
+    const req = createRequestFor('www.tnlcommunityfund.org.uk');
     const res = httpMocks.createResponse();
-
     robotsHandler(req, res);
-
     expect(res._getData()).toMatchSnapshot();
 });
 
 test('should block indexing on other domains', () => {
-    const req = httpMocks.createRequest({
-        method: 'GET',
-        url: '/robots.txt',
-        headers: {
-            'Host': 'apply.tnlcommunityfund.org.uk',
-            'X-Forwarded-Proto': 'https'
-        }
-    });
-
+    const req = createRequestFor('apply.tnlcommunityfund.org.uk');
     const res = httpMocks.createResponse();
-
     robotsHandler(req, res);
-
     expect(res._getData()).toMatchSnapshot();
 });
