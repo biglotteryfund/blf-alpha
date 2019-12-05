38c0bcef95ee00984335617390cbc40d0f91e0db
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-12-05 09:34:39 +0000

    Remove grants from robots.txt

--- controllers/robots.js
+++ controllers/robots.js
@@ -5,18 +5,10 @@ const { getAbsoluteUrl } = require('../common/urls');
 module.exports = function(req, res) {
     const shouldIndex = req.get('host') === 'www.tnlcommunityfund.org.uk';
 
-    /**
-     * Prevent crawling of the following URLs for traffic management purposes
-     * @see https://support.google.com/webmasters/answer/6062608
-     */
-    const disallowList = ['/funding/grants/', '/welsh/funding/grants/']
-        .map(line => `disallow: ${line}`)
-        .join('\n');
-
     res.setHeader('Content-Type', 'text/plain');
     res.send(stripIndents`
         user-agent: *
         sitemap: ${getAbsoluteUrl(req, '/sitemap.xml')}
-        ${shouldIndex ? disallowList : 'disallow: /'}
+        ${shouldIndex ? '' : 'disallow: /'}
     `);
 };
