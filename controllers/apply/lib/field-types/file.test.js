740c47a4603f066da77b19228f0d8feb07777c5a
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-12-13 13:50:49 +0000

    Add FileField type with tests

---
+++ controllers/apply/lib/field-types/file.test.js
@@ -0,0 +1,29 @@
+/* eslint-env jest */
+'use strict';
+const FileField = require('./file');
+
+test('FileField', function() {
+    const field = new FileField({
+        name: 'example',
+        label: 'File field'
+    });
+
+    expect(field.type).toBe('file');
+
+    field.withValue({
+        filename: 'example.pdf',
+        size: 13264,
+        type: 'application/pdf'
+    });
+    expect(field.validate().error).toBeNull();
+    expect(field.displayValue).toBe('example.pdf (PDF, 13 KB)');
+
+    field.withValue({
+        filename: 'example.js',
+        size: 13264,
+        type: 'application/javascript'
+    });
+    expect(field.validate().error.message).toEqual(
+        expect.stringContaining('"type" must be one of')
+    );
+});
