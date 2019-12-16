406eec7b3cab95915696d9b6177795cbbc78f136
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-12-16 09:56:46 +0000

    Add step model

---
+++ controllers/apply/lib/step-model.js
@@ -0,0 +1,81 @@
+'use strict';
+const has = require('lodash/has');
+const reject = require('lodash/reject');
+
+class Step {
+    constructor(props) {
+        this._isClass = true;
+
+        if (props.title) {
+            this.title = props.title;
+        } else {
+            throw new Error('Must provide title');
+        }
+
+        this._fieldsets = reject(
+            props.fieldsets || [],
+            fieldset => fieldset.fields.length === 0
+        );
+
+        /**
+         * If there is only one fieldset,
+         * set the legend to be the same as the step
+         */
+        const shouldSetDefaultLegend =
+            this._fieldsets.length === 1 &&
+            has(this._fieldsets[0], 'legend') === false;
+
+        if (shouldSetDefaultLegend) {
+            this.fieldsets[0].legend = this.title;
+        }
+
+        /**
+         * Flag optional steps if there are no fields
+         * i.e. to account for cases where whole step is conditional
+         */
+        this.isRequired = this.getCurrentFields().length > 0;
+
+        this.noValidate = props.noValidate !== false;
+        this.isMultipart = props.isMultipart === true;
+
+        if (props.preFlightCheck) {
+            this.preFlightCheck = props.preFlightCheck;
+        }
+
+        this._slug = null;
+    }
+
+    get fieldsets() {
+        return this._fieldsets;
+    }
+
+    set fieldsets(fieldsets) {
+        this._fieldsets = fieldsets;
+        return this;
+    }
+
+    get slug() {
+        return this._slug;
+    }
+
+    set slug(slug) {
+        this._slug = slug;
+        return this;
+    }
+
+    getCurrentFields() {
+        return this.fieldsets.flatMap(fieldset => fieldset.fields);
+    }
+
+    filterErrors(messages) {
+        return messages.filter(item =>
+            this.getCurrentFields()
+                .map(field => field.name)
+                .includes(item.param)
+        );
+    }
+}
+
+module.exports = {
+    Step
+};
