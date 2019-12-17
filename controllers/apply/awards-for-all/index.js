d3b68d4ac9d25d3e48b09388285a241e4e6d587a
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2019-12-17 16:31:22 +0000

    Switch start template check to isDev

--- controllers/apply/awards-for-all/index.js
+++ controllers/apply/awards-for-all/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const path = require('path');
 
-const { isNotProduction } = require('../../../common/appData');
+const { isDev } = require('../../../common/appData');
 
 const { initFormRouter } = require('../form-router');
 
@@ -11,13 +11,15 @@ const confirmationBuilder = require('./confirmation');
 const { EXPIRY_EMAIL_REMINDERS } = require('./constants');
 const { transform } = require('./transforms');
 
+function getStartTemplate() {
+    return isDev ? path.resolve(__dirname, './views/startpage.njk') : null;
+}
+
 module.exports = initFormRouter({
     formId: 'awards-for-all',
     eligibilityBuilder: eligibilityBuilder,
     formBuilder: formBuilder,
-    startTemplate: isNotProduction
-        ? path.resolve(__dirname, './views/startpage.njk')
-        : null,
+    startTemplate: getStartTemplate(),
     confirmationBuilder: confirmationBuilder,
     transformFunction: transform,
     expiryEmailPeriods: EXPIRY_EMAIL_REMINDERS
