935705c59b03e657c745196994b7ce116471e9ad
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-01-10 12:11:42 +0000

    Extract whole before_deploy step into a script

---
+++ deploy/travis-prepare-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ev
+#################################################
+# Prepare Deploy
+#################################################
+# Prepare Travis deploy artefact
+
+# Prune devDependencies from artefact
+npm prune --production > /dev/null 2>&1;
+
+# Store deploy metadata
+# Later loaded via the app to reference deploy metadata
+destdir=$TRAVIS_BUILD_DIR/config/deploy.json
+touch "$destdir"
+if [ -f "$destdir" ]
+then
+    echo "{ \"buildNumber\": \"$TRAVIS_BUILD_NUMBER\", \"deployId\": \"DEPLOY_ID\", \"commitId\": \"$TRAVIS_COMMIT\" }" > "$destdir"
+fi
+
+# Bundle deploy artefact, excluding any unneeded files
+zip -qr latest * -x .\* -x "README.md" -x assets/\*;
+
+# Store artefact locally for later use in Travis deploy step
+mkdir -p dpl_cd_upload
+mv latest.zip dpl_cd_upload/build-$TRAVIS_BUILD_NUMBER.zip
