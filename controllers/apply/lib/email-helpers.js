433187dd458e5b77d2ab133ff1656c1e784fc5a3
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-01-31 13:39:39 +0000

    Add welsh subject line

--- controllers/apply/lib/email-helpers.js
+++ controllers/apply/lib/email-helpers.js
@@ -1,7 +1,10 @@
 'use strict';
 
-// Determine the subject line text for each email type
-// Currently AFA and Standard share subject lines / expiry reminder timings
+/**
+ * Determine the subject line text for each email type
+ * @TODO: Can we vary this on form type and assume the
+ *        same cadence for email types, this seems messy?
+ */
 function getSubjectLineForEmail(emailType) {
     switch (emailType) {
         case 'AFA_ONE_MONTH':
@@ -9,30 +12,30 @@ function getSubjectLineForEmail(emailType) {
                 en: 'You have one month to finish your application',
                 cy: 'Mae gennych fis i orffen eich cais'
             };
-        case 'STANDARD_ONE_MONTH':
-            return {
-                en: 'You have one month to finish your funding proposal',
-                cy: '@TODO i18n'
-            };
         case 'AFA_ONE_WEEK':
             return {
                 en: 'You have one week to finish your application',
                 cy: 'Mae gennych wythnos i orffen eich cais'
-            };
-        case 'STANDARD_ONE_WEEK':
-            return {
-                en: 'You have one week to finish your funding proposal',
-                cy: '@TODO i18n'
             };
         case 'AFA_ONE_DAY':
             return {
                 en: 'You have one day to finish your application',
                 cy: 'Mae gennych ddiwrnod i orffen eich cais'
             };
+        case 'STANDARD_ONE_MONTH':
+            return {
+                en: 'You have one month to finish your funding proposal',
+                cy: 'Mae gennych fis ar ôl i orffen eich cynnig'
+            };
+        case 'STANDARD_ONE_WEEK':
+            return {
+                en: 'You have one week to finish your funding proposal',
+                cy: 'Mae gennych wythnos ar ôl i orffen eich cynnig'
+            };
         case 'STANDARD_ONE_DAY':
             return {
                 en: 'You have one day to finish your funding proposal',
-                cy: '@TODO i18n'
+                cy: 'Mae gennych ddiwrnod ar ôl i orffen eich cynnig'
             };
     }
 }
