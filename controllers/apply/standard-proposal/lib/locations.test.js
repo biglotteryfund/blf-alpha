f8493a0dd18760ea8d5fc070440e5b9c3da9da46
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-02-05 14:53:49 +0000

    Add new standard form location helpers
    Use a standard specific set of rules rather than sharing locations
    across awards for all and standard to allow for variation.

---
+++ controllers/apply/standard-proposal/lib/locations.test.js
@@ -0,0 +1,21 @@
+/* eslint-env jest */
+'use strict';
+const { englandRegions, englandLocationOptions } = require('./locations');
+
+test('england region groups should match location groups', function() {
+    const regionValues = englandRegions().map(region => region.value);
+    const locationGroups = englandLocationOptions().map(group => group.id);
+    expect(regionValues).toEqual(locationGroups);
+});
+
+test('filter england location options by region', function() {
+    const result = englandLocationOptions([
+        'midlands',
+        'yorkshire-and-the-humber'
+    ]);
+    expect(result.length).toBe(2);
+    expect(result.map(group => group.label)).toEqual([
+        'East and West Midlands',
+        'Yorkshire and the Humber'
+    ]);
+});
