biglotteryfund/blf-alpha

commit a1fd67d4152bc94f13d4f2c6a8483bbf6052c04f

Make location description mandatory (#2740)

controllers/apply/standard-proposal/form.js
@@ -220,6 +220,9 @@ module.exports = function({ locale = 'en', data = {} } = {}) {
             cy: 'Dechrau ar eich cynnig'
         }),
         allFields,
+        featuredErrorsAllowList: [
+            { fieldName: 'projectLocationDescription', includeBase: true }
+        ],
         summary: summary(),
         schemaVersion: 'v0.2',
         forSalesforce() {

controllers/apply/standard-proposal/form.test.js
@@ -53,8 +53,6 @@ test('valid form', () => {
     expect(result.error).toBeNull();
 
     expect(result.value).toMatchSnapshot({
-        projectCountries: expect.any(Array),
-        projectLocation: expect.any(String),
         yourIdeaProject: expect.any(String),
         yourIdeaCommunity: expect.any(String),
         yourIdeaActivities: expect.any(String)
@@ -147,7 +145,6 @@ test('language preference required in wales', function() {
 });
 
 test.each([
-    'projectLocationDescription',
     'organisationTradingName',
     'contactPhone',
     'contactCommunicationNeeds'
@@ -160,17 +157,14 @@ test.each([
     expect(result.error).toBeNull();
 });
 
-test('projectLocationDescription required if multiple countries selected', function() {
+test('featured messages based on allow list', () => {
     const form = formBuilder({
         data: mockResponse({
-            projectCountries: ['england', 'wales'],
             projectLocationDescription: null
         })
     });
 
-    expect(mapMessages(form.validation)).toEqual(
-        expect.arrayContaining([
-            expect.stringContaining('Tell us all of the locations')
-        ])
-    );
+    expect(form.validation.featuredMessages.map(item => item.msg)).toEqual([
+        expect.stringContaining('Tell us all of the locations')
+    ]);
 });

controllers/apply/awards-for-all/form.js
@@ -1335,11 +1335,11 @@ module.exports = function({
         }),
         allFields: fields,
         featuredErrorsAllowList: [
-            'projectDateRange',
-            'seniorContactRole',
-            'mainContactName',
-            'mainContactEmail',
-            'mainContactPhone'
+            { fieldName: 'projectDateRange', includeBase: false },
+            { fieldName: 'seniorContactRole', includeBase: false },
+            { fieldName: 'mainContactName', includeBase: false },
+            { fieldName: 'mainContactEmail', includeBase: false },
+            { fieldName: 'mainContactPhone', includeBase: false }
         ],
         summary: summary(),
         schemaVersion: 'v1.1',

controllers/apply/awards-for-all/form.test.js
@@ -293,7 +293,7 @@ test.each(
     expect(allSectionsComplete).toBeTruthy();
 });
 
-test('list featured errors based on allow list', () => {
+test('featured messages based on allow list', () => {
     const form = formBuilder({
         data: {
             projectDateRange: {
@@ -306,7 +306,7 @@ test('list featured errors based on allow list', () => {
 
     expect(form.validation.featuredMessages.map(item => item.msg)).toEqual([
         expect.stringMatching(/Date you start the project must be after/),
-        'Senior contact role is not valid'
+        expect.stringContaining('Senior contact role is not valid')
     ]);
 });
 

controllers/apply/lib/form-model.js
@@ -212,6 +212,30 @@ class FormModel {
         );
     }
 
+    /**
+     * Determine featured messages.
+     *
+     * Based on allow list on the form model.
+     * e.g. [{ fieldName: 'example', includeBaseMessage: true }]
+     *
+     * Accepts `includeBase` to determine if `base`
+     * message types should be included in the list.
+     */
+    _getFeaturedMessages(messages) {
+        return messages.filter(message => {
+            return this.featuredErrorsAllowList.some(item => {
+                if (item.includeBase === true) {
+                    return item.fieldName === message.param;
+                } else {
+                    return (
+                        item.fieldName === message.param &&
+                        message.type !== 'base'
+                    );
+                }
+            });
+        });
+    }
+
     validate(data) {
         const { value, error } = this.schema.validate(data, {
             abortEarly: false,
@@ -224,23 +248,12 @@ class FormModel {
             formFields: this.allFields
         });
 
-        /**
-         * Consider messages featured if field names are
-         * included in featuredErrorsAllowList and the
-         * message type is not the 'base` message.
-         */
-        const featuredMessages = messages.filter(message => {
-            return this.featuredErrorsAllowList.some(name => {
-                return name === message.param && message.type !== 'base';
-            });
-        });
-
         return {
             value: value,
             error: error,
             isValid: error === null && messages.length === 0,
             messages: messages,
-            featuredMessages: featuredMessages
+            featuredMessages: this._getFeaturedMessages(messages)
         };
     }
 

controllers/apply/standard-proposal/__snapshots__/form.test.js.snap
@@ -22,6 +22,7 @@ Array [
   "Enter a project name",
   "Select a country",
   "Select a location",
+  "Tell us all of the locations that you'll be running your project in",
   "The amount you ask for must be more than £10,000. If you need less than £10,000, apply today through <a href=\\"/funding/under10k\\"> National Lottery Awards for All </a>",
   "Select a project duration",
   "Tell us what you would like to do",
@@ -54,9 +55,11 @@ Object {
   "organisationTradingName": "Example trading name",
   "organisationType": "not-for-profit-company",
   "projectCosts": 250000,
-  "projectCountries": Any<Array>,
+  "projectCountries": Array [
+    "england",
+  ],
   "projectDurationYears": 3,
-  "projectLocation": Any<String>,
+  "projectLocation": "derbyshire",
   "projectLocationDescription": "description",
   "projectName": "My project",
   "yourIdeaActivities": Any<String>,

controllers/apply/standard-proposal/fields.js
@@ -224,7 +224,6 @@ module.exports = function fieldsFor({ locale, data = {} }) {
 
     function fieldProjectLocationDescription() {
         const maxLength = 255;
-        const baseSchema = Joi.string().max(maxLength);
 
         return new Field({
             locale: locale,
@@ -238,12 +237,6 @@ module.exports = function fieldsFor({ locale, data = {} }) {
                     that you'll be running your project in, e.g.
                     'Yorkshire' or 'Glasgow, Cardiff and Belfast'`,
                 cy: ``
-            }),
-            isRequired: projectCountries.length > 1,
-            schema: Joi.when('projectCountries', {
-                is: Joi.array().min(2),
-                then: baseSchema.required(),
-                otherwise: baseSchema.allow('').optional()
             }),
             maxLength: maxLength,
             messages: [

commit 2ee05ef476e97ad739628bb0828a26083a3914ae

Remove projectLocationDescription from featured errors
We introduced this in
https://github.com/biglotteryfund/blf-alpha/pull/2740 to account for us
changing the location description field from optional to required. This
error appears for all new pending applications as soon as you start the
form which is heavy-handed. Enough time has passed since the original
change that we can remove this from the feature list.

controllers/apply/standard-proposal/form.js
@@ -213,9 +213,6 @@ module.exports = function({ locale = 'en', data = {} } = {}) {
             cy: 'Dechrau ar eich cynnig'
         }),
         allFields,
-        featuredErrorsAllowList: [
-            { fieldName: 'projectLocationDescription', includeBase: true }
-        ],
         summary: summary(),
         schemaVersion: 'v0.2',
         forSalesforce() {

controllers/apply/standard-proposal/form.test.js
@@ -123,16 +123,3 @@ test.each([
     const result = form.validate(expected);
     expect(result.error).toBeNull();
 });
-
-test('featured messages based on allow list', () => {
-    const form = formBuilder({
-        data: mockResponse({
-            projectLocationDescription: null
-        })
-    });
-
-    const messages = form.validation.featuredMessages.map(item => item.msg);
-    expect(messages).toEqual([
-        expect.stringContaining('Tell us all of the locations')
-    ]);
-});