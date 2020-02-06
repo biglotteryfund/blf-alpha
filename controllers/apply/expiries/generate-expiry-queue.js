f6e77403d2c092852a3420e79f41286ffa50e591
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-02-06 11:46:03 +0000

    Retain relative time for expiry emails
    We were previously setting all expiry emails to have a send time of 9am.
    This meant that emails would only ever be sent then limiting how
    frequently we can process expiry emails. Setting the send time to retain
    the original time lets us process the queue in smaller more regular
    batches.

--- controllers/apply/expiries/generate-expiry-queue.js
+++ controllers/apply/expiries/generate-expiry-queue.js
@@ -7,14 +7,10 @@ module.exports = function generateExpiryQueue(application, expiryEmailPeriods) {
             applicationId: application.id,
             userId: application.userId,
             emailType: emailConfig.emailType,
-            dateToSend: moment(application.expiresAt)
-                .subtract(
-                    emailConfig.sendBeforeExpiry.amount,
-                    emailConfig.sendBeforeExpiry.unit
-                )
-                .hour(9)
-                .minute(0)
-                .second(0)
+            dateToSend: moment(application.expiresAt).subtract(
+                emailConfig.sendBeforeExpiry.amount,
+                emailConfig.sendBeforeExpiry.unit
+            )
         };
     });
 };
