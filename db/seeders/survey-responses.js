357264d7bf5b1c65650be47fd6c1fe2e9a811bc6
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-02-06 16:07:25 +0000

    Cleanup seeders (#2929)
    - Migrates survey seeder to a sequelize seeder file
    - Removes need for enableSeeders feature flag

---
+++ db/seeders/survey-responses.js
@@ -0,0 +1,38 @@
+'use strict';
+const faker = require('faker');
+const moment = require('moment');
+const random = require('lodash/random');
+const times = require('lodash/times');
+
+module.exports = {
+    up: async function(queryInterface) {
+        const dateRange = times(90, function(i) {
+            return moment()
+                .subtract(i, 'days')
+                .toDate();
+        });
+
+        const surveyResponses = dateRange.flatMap(function(date) {
+            return times(random(5, 15), function() {
+                const choice = Math.random() > 0.05 ? 'yes' : 'no';
+                return {
+                    choice: choice,
+                    path: faker.random.arrayElement([
+                        '/',
+                        '/funding',
+                        '/about'
+                    ]),
+                    message: choice === 'no' ? faker.lorem.paragraphs(1) : null,
+                    createdAt: date,
+                    updatedAt: date
+                };
+            });
+        });
+
+        return queryInterface.bulkInsert('survey', surveyResponses, {});
+    },
+
+    down: function(queryInterface) {
+        return queryInterface.bulkDelete('survey', null, {});
+    }
+};
