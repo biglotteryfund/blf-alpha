f0c728aca78b2d7d9b184206199abd95fb3de0b1
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-02-06 15:02:12 +0000

    Update seeder to generate full range of days

--- db/seeders/survey-responses.js
+++ db/seeders/survey-responses.js
@@ -6,19 +6,28 @@ const times = require('lodash/times');
 
 module.exports = {
     up: async function(queryInterface) {
-        const surveyResponses = times(100, function() {
-            const choice = Math.random() > 0.05 ? 'yes' : 'no';
-            const createdAt = moment()
-                .subtract(random(0, 90), 'days')
+        const dateRange = times(30, function(i) {
+            return moment()
+                .subtract(i, 'days')
                 .toDate();
+        });
 
-            return {
-                choice: choice,
-                path: faker.random.arrayElement(['/', '/funding', '/about']),
-                message: choice === 'no' ? faker.lorem.paragraphs(1) : null,
-                createdAt: createdAt,
-                updatedAt: createdAt
-            };
+        const surveyResponses = dateRange.flatMap(function(date) {
+            const choice = Math.random() > 0.05 ? 'yes' : 'no';
+
+            return times(random(1, 20), function() {
+                return {
+                    choice: choice,
+                    path: faker.random.arrayElement([
+                        '/',
+                        '/funding',
+                        '/about'
+                    ]),
+                    message: choice === 'no' ? faker.lorem.paragraphs(1) : null,
+                    createdAt: date,
+                    updatedAt: date
+                };
+            });
         });
 
         return queryInterface.bulkInsert('survey', surveyResponses, {});
