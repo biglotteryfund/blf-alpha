cc055ae53b87b610e96c1a00b4684281ff755c0d
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-02-24 11:09:37 +0000

    Fix a11y issues with global account icons
    - Use consistent icon macro
    - Add title to all account icons
    - Use aria-hidden rather than skipLabel to control screen reader
      visibility

--- assets/js/vue-apps/components/icon-bin.vue
+++ assets/js/vue-apps/components/icon-bin.vue
@@ -6,8 +6,11 @@
         viewBox="0 0 16 16"
         width="14"
         height="14"
+        :aria-hidden="true"
+        :aria-labelledby="'aria-bin-title-' + props.id"
         :aria-describedby="'aria-bin-desc-' + props.id"
     >
+        <title :id="'aria-bin-title-' + props.id">Bin icon</title>
         <desc :id="'aria-bin-desc-' + props.id">{{ props.description }}</desc>
         <path
             d="M3 16h10l1-11h-12zM10 2v-2h-4v2h-5v3l1-1h12l1 1v-3h-5zM9 2h-2v-1h2v1z"
