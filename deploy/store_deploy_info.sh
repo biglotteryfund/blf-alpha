64aabfe9183b103a799ae8372134ccdac06180cb
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-03-02 09:28:53 +0000

    Merge deploy artefact scripts (#2967)
    Steps for producing the deploy artefact were split across a script and
    inline in the travis.yml. This change merges this process into a single
    script, and adds explanatory comments.

--- deploy/store_deploy_info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-
-destdir=$TRAVIS_BUILD_DIR/config/deploy.json
-touch "$destdir"
-if [ -f "$destdir" ]
-then
-    echo "{ \"buildNumber\": \"$TRAVIS_BUILD_NUMBER\", \"deployId\": \"DEPLOY_ID\", \"commitId\": \"$TRAVIS_COMMIT\" }" > "$destdir"
-fi
