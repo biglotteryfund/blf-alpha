1e84304bbd89f6b51f29441c818baf88297079c4
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-03-09 15:45:01 +0000

    Replace injectCopy with direct usage
    Use copy directly on data page rather than via indirection through
    injectCopy

--- controllers/data/index.js
+++ controllers/data/index.js
@@ -4,31 +4,25 @@ const express = require('express');
 const get = require('lodash/get');
 
 const contentApi = require('../../common/content-api');
-const { injectCopy, injectHeroImage } = require('../../common/inject-content');
+const { injectHeroImage } = require('../../common/inject-content');
 
 const router = express.Router();
 
-router.get(
-    '/',
-    injectHeroImage('fsn-new'),
-    injectCopy('toplevel.data'),
-    async (req, res, next) => {
-        try {
-            const dataStats = await contentApi.getDataStats(
-                req.i18n.getLocale(),
-                req.query
-            );
+router.get('/', injectHeroImage('fsn-new'), async function(req, res, next) {
+    try {
+        const dataStats = await contentApi.getDataStats(
+            req.i18n.getLocale(),
+            req.query
+        );
 
-            res.locals.openGraph = get(dataStats, 'openGraph', false);
-
-            res.render(path.resolve(__dirname, './views/data'), {
-                title: dataStats.title,
-                dataStats
-            });
-        } catch (error) {
-            next(error);
-        }
+        res.render(path.resolve(__dirname, './views/data'), {
+            title: dataStats.title,
+            openGraph: get(dataStats, 'openGraph', false),
+            dataStats: dataStats
+        });
+    } catch (error) {
+        next(error);
     }
-);
+});
 
 module.exports = router;
