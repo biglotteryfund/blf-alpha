e31d8c37baa6e9fe1979f96f0f156afe9c36892f
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2020-03-11 11:57:27 +0000

    Allow custom directory for app secrets (eg. to override Unix-like paths that don't exist on Windows)

--- common/parameter-store.js
+++ common/parameter-store.js
@@ -3,12 +3,18 @@ const fs = require('fs');
 const flow = require('lodash/fp/flow');
 const keyBy = require('lodash/fp/keyBy');
 const mapValues = require('lodash/fp/mapValues');
+const has = require('lodash/has');
+
+require('dotenv').config();
 
 function getRawParameters() {
     let rawParameters;
+    const secretDir = has(process.env, 'SECRET_DIR')
+        ? process.env.SECRET_DIR
+        : '/etc/blf';
     try {
         rawParameters = JSON.parse(
-            fs.readFileSync('/etc/blf/parameters.json', 'utf8')
+            fs.readFileSync(`${secretDir}/parameters.json`, 'utf8')
         );
     } catch (e) {} // eslint-disable-line no-empty
     return rawParameters;
