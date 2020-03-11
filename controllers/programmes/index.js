dd0054fff5b6d5d9265008554d0359dfadcd7f16
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-03-11 12:19:31 +0000

    Remove injectCopy from programmes list

--- controllers/programmes/index.js
+++ controllers/programmes/index.js
@@ -6,7 +6,6 @@ const get = require('lodash/get');
 const groupBy = require('lodash/groupBy');
 
 const {
-    injectCopy,
     injectHeroImage,
     setCommonLocals
 } = require('../../common/inject-content');
@@ -32,86 +31,86 @@ router.use(function(req, res, next) {
 /**
  * Programmes list
  */
-router.get(
-    '/',
-    injectHeroImage('rosemount-1-letterbox-new'),
-    injectCopy('funding.programmes'),
-    async (req, res, next) => {
-        try {
-            const response = await contentApi.getFundingProgrammes({
-                locale: req.i18n.getLocale()
+router.get('/', injectHeroImage('rosemount-1-letterbox-new'), async function(
+    req,
+    res,
+    next
+) {
+    try {
+        const response = await contentApi.getFundingProgrammes({
+            locale: req.i18n.getLocale()
+        });
+
+        const allFundingProgrammes = response.result || [];
+
+        const locationParam = getValidLocation(
+            allFundingProgrammes,
+            req.query.location
+        );
+
+        const programmesFilteredByAmount = allFundingProgrammes
+            .filter(programmeFilters.filterByMinAmount(req.query.min))
+            .filter(programmeFilters.filterByMaxAmount(req.query.max));
+
+        const programmes = programmesFilteredByAmount.filter(
+            programmeFilters.filterByLocation(locationParam)
+        );
+
+        const ukWideProgrammes = programmesFilteredByAmount.filter(
+            programmeFilters.filterByLocation('ukWide')
+        );
+
+        const groupedProgrammes =
+            locationParam && locationParam !== 'ukWide'
+                ? groupBy(programmes.concat(ukWideProgrammes), 'area.label')
+                : null;
+
+        if (parseInt(req.query.min, 10) === 10000) {
+            res.locals.breadcrumbs.push({
+                label: req.i18n.__('funding.programmes.over10k'),
+                url: '/over10k'
             });
-
-            const allFundingProgrammes = response.result || [];
-
-            const locationParam = getValidLocation(
-                allFundingProgrammes,
-                req.query.location
-            );
-
-            const programmesFilteredByAmount = allFundingProgrammes
-                .filter(programmeFilters.filterByMinAmount(req.query.min))
-                .filter(programmeFilters.filterByMaxAmount(req.query.max));
-
-            const programmes = programmesFilteredByAmount.filter(
-                programmeFilters.filterByLocation(locationParam)
-            );
-
-            const ukWideProgrammes = programmesFilteredByAmount.filter(
-                programmeFilters.filterByLocation('ukWide')
-            );
-
-            const groupedProgrammes =
-                locationParam && locationParam !== 'ukWide'
-                    ? groupBy(programmes.concat(ukWideProgrammes), 'area.label')
-                    : null;
-
-            if (parseInt(req.query.min, 10) === 10000) {
-                res.locals.breadcrumbs.push({
-                    label: res.locals.copy.over10k,
-                    url: '/over10k'
-                });
-            }
-
-            if (parseInt(req.query.max, 10) === 10000) {
-                res.locals.breadcrumbs.push({
-                    label: res.locals.copy.under10k,
-                    url: '/under10k'
-                });
-            }
-
-            if (locationParam) {
-                const globalCopy = req.i18n.__('global');
-                res.locals.breadcrumbs.push({
-                    label: {
-                        england: globalCopy.regions.england,
-                        wales: globalCopy.regions.wales,
-                        scotland: globalCopy.regions.scotland,
-                        northernIreland: globalCopy.regions.northernIreland,
-                        ukWide: globalCopy.regions.ukWide
-                    }[locationParam]
-                });
-            }
-
-            // If we have more than two breadcrumbs assign a url to the second (the current page)
-            if (res.locals.breadcrumbs.length > 2) {
-                res.locals.breadcrumbs[1].url = req.baseUrl;
-            }
-
-            const activeBreadcrumbsSummary = res.locals.breadcrumbs
-                .map(breadcrumb => breadcrumb.label)
-                .join(', ');
-
-            res.render(path.resolve(__dirname, './views/programmes-list'), {
-                programmes,
-                groupedProgrammes,
-                activeBreadcrumbsSummary
-            });
-        } catch (error) {
-            next(error);
         }
+
+        if (parseInt(req.query.max, 10) === 10000) {
+            res.locals.breadcrumbs.push({
+                label: req.i18n.__('funding.programmes.under10k'),
+                url: '/under10k'
+            });
+        }
+
+        if (locationParam) {
+            const globalCopy = req.i18n.__('global');
+            res.locals.breadcrumbs.push({
+                label: {
+                    england: globalCopy.regions.england,
+                    wales: globalCopy.regions.wales,
+                    scotland: globalCopy.regions.scotland,
+                    northernIreland: globalCopy.regions.northernIreland,
+                    ukWide: globalCopy.regions.ukWide
+                }[locationParam]
+            });
+        }
+
+        // If we have more than two breadcrumbs assign a url to the second (the current page)
+        if (res.locals.breadcrumbs.length > 2) {
+            res.locals.breadcrumbs[1].url = req.baseUrl;
+        }
+
+        const activeBreadcrumbsSummary = res.locals.breadcrumbs
+            .map(breadcrumb => breadcrumb.label)
+            .join(', ');
+
+        res.render(path.resolve(__dirname, './views/programmes-list'), {
+            title: req.i18n.__('funding.programmes.title'),
+            programmes,
+            groupedProgrammes,
+            activeBreadcrumbsSummary
+        });
+    } catch (error) {
+        next(error);
     }
-);
+});
 
 /**
  * All programmes
@@ -120,8 +119,7 @@ router.get(
     '/all',
     sMaxAge(86400 /* 1 day in seconds */),
     injectHeroImage('cbsa-2-letterbox-new'),
-    injectCopy('funding.allProgrammes'),
-    async (req, res, next) => {
+    async function(req, res, next) {
         try {
             const response = await contentApi.getFundingProgrammes({
                 locale: req.i18n.getLocale(),
@@ -181,6 +179,7 @@ router.get(
             }
 
             res.render(path.resolve(__dirname, './views/all-programmes'), {
+                title: req.i18n.__('funding.allProgrammes.title'),
                 locationLinks: locationLinks,
                 locationParam: locationParam,
                 groupedProgrammes: groupedProgrammes,
