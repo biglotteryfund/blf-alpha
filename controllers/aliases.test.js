f618ff3f42dc0bfcdd8729e0f67d2d504c0b74f7
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-03-19 10:37:57 +0000

    Comment aliases code to make behaviour clearer

--- controllers/aliases.test.js
+++ controllers/aliases.test.js
@@ -2,8 +2,12 @@
 'use strict';
 const aliases = require('./aliases');
 
-describe('aliases', () => {
-    it('should export mappedAliases', () => {
-        expect(aliases).toMatchSnapshot();
-    });
+/**
+ * The list of redirects is largely static but includes
+ * some dynamic generation of welsh language and multi-region
+ * versions of each URL. A snapshot test is a quick way of us
+ * confirming that each alias has the expected variants.
+ */
+test('should export mappedAliases', () => {
+    expect(aliases).toMatchSnapshot();
 });
