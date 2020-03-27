d096234907845703640554cafdd86a3dd8dee54e
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2020-03-27 15:12:30 +0000

    Prettier fixes

--- controllers/apply/index.js
+++ controllers/apply/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const appData = require('../../common/appData');
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     if (req.user) {
         res.locals.user = req.user;
     }
@@ -16,12 +16,12 @@ router.use(function(req, res, next) {
 
 router.use('/', require('./dashboard'));
 
-router.get('/your-idea*', function(req, res) {
+router.get('/your-idea*', function (req, res) {
     return res.redirect(res.locals.sectionUrl);
 });
 
 // Redirect renamed Awards For All paths to more generic under10k route
-router.get('/awards-for-all*', function(req, res) {
+router.get('/awards-for-all*', function (req, res) {
     const newPath = req.originalUrl.replace('/awards-for-all/', '/under-10k/');
     res.redirect(newPath);
 });
