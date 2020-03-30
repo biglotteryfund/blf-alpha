8789c165b4f8be9717084d627b0052e65f834efd
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-03-30 16:54:17 +0100

    [WIP] Migrate salesforce queries to use got
    This allows us to full migrate away from request in favour of got. The
    main difference here is needing to construct the form-data object
    directly rather than relying on requests wrapping of it. Aside from
    this, the overall result is a little simpler.

--- controllers/apply/form-router/lib/salesforce.js
+++ controllers/apply/form-router/lib/salesforce.js
@@ -1,78 +1,69 @@
 'use strict';
-const request = require('request-promise-native');
+const got = require('got');
+const FormData = require('form-data');
 const { SALESFORCE_AUTH } = require('../../../../common/secrets');
 
 class Salesforce {
     constructor(apiUrl, accessToken) {
-        this.apiUrl = apiUrl;
         this.apiVersion = 'v45.0';
-        this.headers = { Authorization: `Bearer ${accessToken}` };
-    }
-    get(urlPath) {
-        return request.get({
-            uri: `${this.apiUrl}${urlPath}`,
-            headers: this.headers,
-            json: true,
-        });
-    }
-    postJson(urlPath, body) {
-        return request.post({
-            url: `${this.apiUrl}${urlPath}`,
-            headers: this.headers,
-            json: true,
-            body: body,
-        });
-    }
-    submitFormData({ application, meta }) {
-        return this.postJson('/services/apexrest/FormData/', {
-            meta: meta,
-            application: application,
-        });
-    }
-    async contentVersion({ recordId, attachmentName, versionData }) {
-        return request.post({
-            url: `${this.apiUrl}/services/data/${this.apiVersion}/sobjects/ContentVersion`,
-            headers: this.headers,
-            formData: {
-                entity_content: {
-                    value: JSON.stringify({
-                        FirstPublishLocationId: recordId,
-                        ReasonForChange: `Application attachment ${attachmentName}`,
-                        PathOnClient: attachmentName,
-                    }),
-                    options: {
-                        contentType: 'application/json',
-                    },
-                },
-                VersionData: versionData,
+
+        this.query = got.extend({
+            prefixUrl: apiUrl,
+            headers: {
+                'authorization': `Bearer ${accessToken}`,
+                'user-agent': 'tnlcf-salesforce',
             },
         });
+    }
+
+    submitFormData({ application, meta }) {
+        return this.query
+            .post('services/apexrest/FormData/', {
+                body: { meta, application },
+            })
+            .json();
+    }
+
+    contentVersion({ recordId, attachmentName, versionData }) {
+        const form = new FormData();
+        form.append(
+            'entity_content',
+            JSON.stringify({
+                FirstPublishLocationId: recordId,
+                ReasonForChange: `Application attachment ${attachmentName}`,
+                PathOnClient: attachmentName,
+            }),
+            { contentType: 'application/json' }
+        );
+        form.append('VersionData', versionData.value, versionData.options);
+
+        return this.query.post(
+            `services/data/${this.apiVersion}/sobjects/ContentVersion`,
+            { body: form }
+        );
     }
 }
 
 async function authorise() {
     const AUTH_URL = `https://${SALESFORCE_AUTH.apiUrl}/services/oauth2/token`;
-    const resultJson = await request.post({
-        url: AUTH_URL,
-        json: true,
-        form: {
-            grant_type: 'password',
-            client_id: SALESFORCE_AUTH.consumerKey,
-            client_secret: SALESFORCE_AUTH.consumerSecret,
-            username: SALESFORCE_AUTH.username,
-            password: `${SALESFORCE_AUTH.password}${SALESFORCE_AUTH.token}`,
-        },
-    });
+    const resultJson = await got
+        .post(AUTH_URL, {
+            form: {
+                grant_type: 'password',
+                client_id: SALESFORCE_AUTH.consumerKey,
+                client_secret: SALESFORCE_AUTH.consumerSecret,
+                username: SALESFORCE_AUTH.username,
+                password: `${SALESFORCE_AUTH.password}${SALESFORCE_AUTH.token}`,
+            },
+        })
+        .json();
 
     return new Salesforce(resultJson.instance_url, resultJson.access_token);
 }
 
 function checkStatus() {
-    return request({
-        url: `https://api.status.salesforce.com/v1/instances/${SALESFORCE_AUTH.instanceId}/status`,
-        json: true,
-        timeout: 3000,
-    });
+    const statusUrl = `https://api.status.salesforce.com/v1/instances/${SALESFORCE_AUTH.instanceId}/status`;
+    return got.get(statusUrl, { timeout: 3000 }).json();
 }
 
 module.exports = {
