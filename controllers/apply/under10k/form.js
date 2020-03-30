795af96d0375a2074d0b9362073c58461b404483
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2020-03-30 09:12:18 +0100

    Fix typo

--- controllers/apply/under10k/form.js
+++ controllers/apply/under10k/form.js
@@ -990,7 +990,7 @@ module.exports = function ({
             
             <div class="o-media--constrained u-padded u-tone-background-tint u-margin-bottom">
                 <a href="../help/bank-statement" target="_blank">
-                    <img src="/assets/images/apply/afaunder-10k-bank-statement-example-small-welsh.jpg"
+                    <img src="/assets/images/apply/under-10k-bank-statement-example-small-welsh.jpg"
                          alt="Enghraifft o gyfriflen banc rydym ei angen gennych"
                          class="o-media__figure o-media__figure-gutter"
                          width="300" />
