5b3393ddde1967e154b272ebe8c5ef465159a2f2
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-04-03 15:13:53 +0100

    Add shallow homepage hero images

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -18,10 +18,10 @@ router.get('/', async function (req, res, next) {
             featuredLinks: entry.featuredLinks,
             promotedUpdates: entry.promotedUpdates,
             heroImage: {
-                small: '/assets/images/home/superhero-small-v2.jpg',
-                medium: '/assets/images/home/superhero-medium-v2.jpg',
-                large: '/assets/images/home/superhero-large-v2.jpg',
-                default: '/assets/images/home/superhero-medium-v2.jpg',
+                small: '/assets/images/home/superhero-shallow-small.jpg',
+                medium: '/assets/images/home/superhero-shallow-medium.jpg',
+                large: '/assets/images/home/superhero-shallow-large.jpg',
+                default: '/assets/images/home/superhero-shallow-medium.jpg',
                 caption: 'Superstars Club',
             },
         });
