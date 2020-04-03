8eb617f7c3cd9ebb949790e1d449a8875510fe96
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-04-03 14:51:55 +0100

    Streamline filters test code

--- views/filters.test.js
+++ views/filters.test.js
@@ -1,53 +1,28 @@
 /* eslint-env jest */
 'use strict';
 
-const {
-    appendUuid,
-    getCachebustedPath,
-    numberWithCommas,
-    slugify,
-    widont,
-} = require('./filters');
+const filters = require('./filters');
 
-describe('appendUuid', () => {
-    it('should append a uuid', () => {
-        expect(appendUuid('hello-')).toMatch(/hello-.*/);
-    });
+test('append a uuid', () => {
+    expect(filters.appendUuid('hello-')).toMatch(/hello-.*/);
 });
 
-describe('getCachebustedPath', () => {
-    it('should get local url path cachebusted asset', () => {
-        const result = getCachebustedPath('stylesheets/style.css', false);
-        expect(result).toMatch(
-            /^\/assets\/build\/\w+\/stylesheets\/style.css$/
-        );
-    });
-
-    it('should get external url for cachebusted asset', () => {
-        const result = getCachebustedPath('stylesheets/style.css', true);
-        expect(result).toMatch(
-            /^\/assets\/build\/\w+\/stylesheets\/style.css$/
-        );
-    });
+test('get cachebusted asset', () => {
+    const result = filters.getCachebustedPath('stylesheets/style.css', false);
+    expect(result).toMatch(/^\/assets\/build\/\w+\/stylesheets\/style.css$/);
 });
 
-describe('numberWithCommas', () => {
-    it('should format a number with comma separators', () => {
-        expect(numberWithCommas(1548028)).toBe('1,548,028');
-    });
+test('should format a number with comma separators', () => {
+    expect(filters.numberWithCommas(1548028)).toBe('1,548,028');
 });
 
-describe('slugify', () => {
-    it('should slugify a string', () => {
-        expect(slugify('This is a test')).toBe('this-is-a-test');
-    });
+test('slugify a string', () => {
+    expect(filters.slugify('This is a test')).toBe('this-is-a-test');
 });
 
-describe('widont', () => {
-    it('should add a non-breaking-space to the last word of a string to prevent typographic widows', () => {
-        expect(widont('A string')).toBe('A&nbsp;string');
-        expect(widont('A slightly longer string')).toBe(
-            'A slightly longer&nbsp;string'
-        );
-    });
+test('prevent typographic widows', () => {
+    expect(filters.widont('A string')).toBe('A&nbsp;string');
+    expect(filters.widont('A slightly longer string')).toBe(
+        'A slightly longer&nbsp;string'
+    );
 });
