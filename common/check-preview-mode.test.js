6be7d173113d79905541113e56592ec4f35f967b
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-04-09 15:55:49 +0100

    Prettier auto-format

--- common/check-preview-mode.test.js
+++ common/check-preview-mode.test.js
@@ -1,11 +1,32 @@
 /* eslint-env jest */
 'use strict';
 
-const preview = require('./check-preview-mode');
+const checkPreviewMode = require('./check-preview-mode');
 
+test('checkPreviewMode', function () {
+    expect(
+        checkPreviewMode({
+            'x-craft-live-preview': 'abc',
+            'x-craft-preview': '123',
+            'token': '456',
+        })
+    ).toStrictEqual({
+        isLivePreview: 'abc',
+        isPreview: 'abc',
+        isShareLink: '456',
+    });
 
-test('preview', function () {
-    expect(preview({'x-craft-live-preview' : 'abc', 'x-craft-preview': '123', 'token': '456'})).toStrictEqual({isLivePreview: "abc", isPreview: "abc", isShareLink: "456"});
-    expect(preview({'x-craft-live-preview' : 'abc'})).toStrictEqual({isLivePreview: "abc", isPreview: "abc", isShareLink: undefined});
-    expect(preview({'x-craft-preview': '123', 'token': '456'})).toStrictEqual({isLivePreview: undefined, isPreview: "456", isShareLink: "456"});
+    expect(checkPreviewMode({ 'x-craft-live-preview': 'abc' })).toStrictEqual({
+        isLivePreview: 'abc',
+        isPreview: 'abc',
+        isShareLink: undefined,
+    });
+
+    expect(
+        checkPreviewMode({ 'x-craft-preview': '123', 'token': '456' })
+    ).toStrictEqual({
+        isLivePreview: undefined,
+        isPreview: '456',
+        isShareLink: '456',
+    });
 });
