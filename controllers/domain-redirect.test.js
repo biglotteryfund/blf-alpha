a295f677385abb9618761a54fb9fd4606509eed7
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-04-15 13:41:55 +0100

    Use _get helpers in domain-redirect test

--- controllers/domain-redirect.test.js
+++ controllers/domain-redirect.test.js
@@ -16,12 +16,15 @@ test('should redirect old domain to new domain', () => {
     const req = createRequestFor('apply.tnlcommunityfund.org.uk');
     const res = httpMocks.createResponse();
     domainRedirect(req, res, () => {});
-    expect(res.statusCode).toBe(301);
+    expect(res._getStatusCode()).toBe(301);
+    expect(res._getRedirectUrl()).toBe(
+        'https://www.tnlcommunityfund.org.uk/apply'
+    );
 });
 
 test('should not redirect new domain', () => {
     const req = createRequestFor('www.tnlcommunityfund.org.uk');
     const res = httpMocks.createResponse();
     domainRedirect(req, res, () => {});
-    expect(res.statusCode).toBe(200);
+    expect(res._getStatusCode()).toBe(200);
 });
