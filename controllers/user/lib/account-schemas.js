6b2686859aa0e3478882ee13c7762b0795f2bce3
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-04-20 16:59:25 +0100

    Alias latest joi as joiNext, upgrade to 17.x

--- controllers/user/lib/account-schemas.js
+++ controllers/user/lib/account-schemas.js
@@ -1,5 +1,5 @@
 'use strict';
-const baseJoi = require('@hapi/joi16');
+const baseJoi = require('@hapi/joiNext');
 const Joi = baseJoi.extend(require('./password-strength'));
 
 const MIN_PASSWORD_LENGTH = 10;
