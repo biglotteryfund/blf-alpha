44f124c6c5d32708de888f097d033e75980da797
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-04-21 11:22:13 +0100

    Add missing test for rangeLimit

--- controllers/apply/lib/joi-extensions/date-parts.test.js
+++ controllers/apply/lib/joi-extensions/date-parts.test.js
@@ -73,3 +73,25 @@ test('maxDate', function () {
         'Date must be on or before 2020-10-01'
     );
 });
+
+test('rangeLimit', function () {
+    const schema = Joi.object({
+        dateA: Joi.dateParts(),
+        dateB: Joi.dateParts().rangeLimit(Joi.ref('dateA'), {
+            amount: 7,
+            unit: 'days',
+        }),
+    });
+
+    const valid = schema.validate({
+        dateA: { day: 2, month: 10, year: 2020 },
+        dateB: { day: 9, month: 10, year: 2020 },
+    });
+    expect(valid.error).toBeNull();
+
+    const invalid = schema.validate({
+        dateA: { day: 2, month: 10, year: 2020 },
+        dateB: { day: 10, month: 10, year: 2020 },
+    });
+    expect(invalid.error.message).toContain('Date must be within range');
+});
