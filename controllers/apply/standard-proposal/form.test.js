933499e79813b2c0c273902a15f4d565dbcac036
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-04-29 11:07:05 +0100

    Passing test for stripping regions outside England

--- controllers/apply/standard-proposal/form.test.js
+++ controllers/apply/standard-proposal/form.test.js
@@ -50,6 +50,17 @@ test('require region when england is selected', function () {
     );
 });
 
+test('strip region outside of england', function () {
+    const data = mockResponse({
+        projectCountries: ['northern-ireland'],
+        projectRegions: ['midlands', 'north-west'],
+    });
+
+    const form = formBuilder({ data });
+
+    expect(form.validation.value).not.toHaveProperty('projectRegions');
+});
+
 test('strip other region selections when all-england is selected', function () {
     const form = formBuilder({
         data: mockResponse({
