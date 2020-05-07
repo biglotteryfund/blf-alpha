665b9f81f4607934894d90777e136cca16091b3e
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-05-07 16:22:08 +0100

    Add welsh language and tests

--- controllers/apply/dashboard/lib/get-notice.js
+++ controllers/apply/dashboard/lib/get-notice.js
@@ -20,7 +20,8 @@ module.exports = function getNotice(locale, pendingApplications = []) {
             title: localise({
                 en: oneLine`For funding under £10,000 in England, we're now only
                     accepting COVID-19 related applications`,
-                cy: `@TODO: i18n`,
+                cy: oneLine`Ar gyfer ariannu o dan £10,000 yn Lloegr, dim ond
+                    ceisiadau cysylltiedig â COVID-19 yr ydym yn eu derbyn`,
             }),
             body: localise({
                 en: oneLine`If you've started an application already,
@@ -28,7 +29,11 @@ module.exports = function getNotice(locale, pendingApplications = []) {
                     or organisation through the pandemic, we won't be
                     able to accept it. But you could decide to start
                     a new one that focuses on COVID-19 instead.`,
-                cy: `@TODO: i18n`,
+                cy: oneLine`Os ydych chi wedi cychwyn cais yn barod,
+                    ac nad yw'n gysylltiedig â chefnogi'ch cymuned
+                    neu sefydliad trwy'r pandemig, ni fyddwn yn gallu
+                    ei dderbyn. Ond fe allech chi benderfynu cychwyn
+                    un newydd sy'n canolbwyntio ar COVID-19 yn lle.`,
             }),
         };
     }
