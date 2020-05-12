3984e734a84e7c8ef1d6e063621c3192fc324d94
Mohammed Durvesh <Mohammed.Durvesh@tnlcommunityfund.org.uk>
2020-05-12 13:19:52 +0100

    Updated tests according to removal of enableNewCOVID19Flow flag.

--- controllers/apply/under10k/fields/your-idea.test.js
+++ controllers/apply/under10k/fields/your-idea.test.js
@@ -2,22 +2,11 @@
 'use strict';
 const { fieldYourIdeaPriorities } = require('./your-idea');
 
-test('show existing guidance text if enableNewCOVID19Flow is disabled', function () {
-    const field = fieldYourIdeaPriorities(
-        'en',
-        {},
-        { enableNewCOVID19Flow: false }
-    );
-    expect(field.explanation).toMatchSnapshot();
-});
-
 test('show COVID-19 guidance text in England', function () {
     const data = {
         projectCountry: 'england',
     };
-    const field = fieldYourIdeaPriorities('en', data, {
-        enableNewCOVID19Flow: true,
-    });
+    const field = fieldYourIdeaPriorities('en', data);
     expect(field.explanation).toMatchSnapshot();
 });
 
@@ -26,9 +15,7 @@ test('show COVID-19 guidance text outside England when project is responding to 
         projectCountry: 'scotland',
         supportingCOVID19: 'yes',
     };
-    const field = fieldYourIdeaPriorities('en', data, {
-        enableNewCOVID19Flow: true,
-    });
+    const field = fieldYourIdeaPriorities('en', data);
     expect(field.explanation).toMatchSnapshot();
 });
 
@@ -37,17 +24,14 @@ test('show regular guidance text outside England when project is not responding 
         projectCountry: 'scotland',
         supportingCOVID19: 'no',
     };
-    const field = fieldYourIdeaPriorities('en', data, {
-        enableNewCOVID19Flow: true,
-    });
+    const field = fieldYourIdeaPriorities('en', data);
     expect(field.explanation).toMatchSnapshot();
 });
 
 test('show combined guidance text as a fallback', function () {
     const field = fieldYourIdeaPriorities(
         'en',
-        {},
-        { enableNewCOVID19Flow: true }
+        {}
     );
     expect(field.explanation).toMatchSnapshot();
 });
