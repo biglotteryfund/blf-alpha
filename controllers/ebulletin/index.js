0d036a64fcc497d386682fae5da47528e37be412
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2020-05-12 15:15:09 +0100

    Refactor ebulletin logic to allow stakeholder signups [WIP] (#3331)
    * Refactor ebulletin logic to allow stakeholder signups
    
    * Remove debug line
    
    * Remove dynamic form action in favour of self-posting
    
    * Add some style tweaks to the make the form a bit more usable
    
    * Rename "stakeholder" to "policy" in public-facing code

--- controllers/ebulletin/index.js
+++ controllers/ebulletin/index.js
@@ -4,7 +4,12 @@ const express = require('express');
 const Sentry = require('@sentry/node');
 const omit = require('lodash/omit');
 
-const { newContact, buildValidLocations } = require('./lib/contact-schema');
+const {
+    newContact,
+    newStakeholder,
+    buildValidLocations,
+    buildValidSectors,
+} = require('./lib/contact-schema');
 const ebulletinService = require('./lib/ebulletin-service');
 
 const { noStore, csrfProtection } = require('../../common/cached');
@@ -17,30 +22,38 @@ const router = express.Router();
 function renderForm(req, res, data = null, errors = []) {
     res.render(path.resolve(__dirname, './views/ebulletin'), {
         title: req.i18n.__('toplevel.ebulletin.title'),
+        contactType: req.params.contactType,
         csrfToken: req.csrfToken(),
         validLocations: buildValidLocations(req.i18n),
+        validSectors: buildValidSectors(req.i18n),
         formValues: data,
         errors: errors,
     });
 }
 
 router
-    .route('/')
+    .route('/:contactType(policy)?')
     .all(noStore, csrfProtection)
     .get(renderForm)
     .post(async function handleEbulletinSignup(req, res) {
         const sanitisedBody = sanitiseRequestBody(omit(req.body, ['_csrf']));
 
-        const validationResult = validateSchema(
-            newContact(req.i18n),
-            sanitisedBody
-        );
+        let contactToUse = newContact(req.i18n);
+        let addressBookId = 148374;
+
+        if (req.params.contactType === 'policy') {
+            contactToUse = newStakeholder(req.i18n);
+            addressBookId = 249380;
+        }
+
+        const validationResult = validateSchema(contactToUse, sanitisedBody);
 
         if (validationResult.isValid) {
             try {
                 await ebulletinService.subscribe({
-                    addressBookId: '148374',
+                    addressBookId: addressBookId,
                     subscriptionData: validationResult.value,
+                    contactType: req.params.contactType,
                 });
                 res.locals.status = 'SUCCESS';
                 renderForm(req, res);
