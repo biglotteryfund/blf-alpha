ad22159f025c70ea049c28a881bee4c31cc3df16
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-05-18 11:12:14 +0100

    This guidance rather than application guidance
    To avoid confusion with existing application guidance.

--- controllers/apply/under10k/fields/terms.js
+++ controllers/apply/under10k/fields/terms.js
@@ -270,7 +270,7 @@ module.exports = {
             label: oneLine`You confirm that you have considered the State Aid
                 compliance requirements set out
                 <a href="/funding/thinking-of-applying-for-funding/who-can-apply/covid-19-state-aid-compliance-guidance">
-                    in our application guidance
+                    in this guidance
                 </a>`,
             options: [
                 {
@@ -304,7 +304,7 @@ module.exports = {
                 requirements at 3.1 of the UK's Covid-19 Temporary Framework Scheme (SA. SA.56841)
                 as explained in our
                 <a href="/funding/thinking-of-applying-for-funding/who-can-apply/covid-19-state-aid-compliance-guidance">
-                    in our application guidance
+                    in this guidance
                 </a>`,
             options: [
                 {
