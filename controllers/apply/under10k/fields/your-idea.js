4dd98176471f65196e64a38b5b2c3dbb9425f928
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-05-20 14:21:50 +0100

    Use wordCountText helper consistently

--- controllers/apply/under10k/fields/your-idea.js
+++ controllers/apply/under10k/fields/your-idea.js
@@ -26,6 +26,43 @@ module.exports = {
         const minWords = 50;
         const maxWords = 300;
 
+        function guidanceText() {
+            return localise({
+                en: `<p><strong>
+                    Here are some ideas of what to tell us about your project:
+                </strong></p>
+                <ul>
+                    <li>What you would like to do</li>
+                    <li>What difference your project will make</li>
+                    <li>Who will benefit from it</li>
+                    <li>How long you expect to run it for. This can be an estimate</li>
+                    <li>How you'll make sure people know about it</li>
+                    <li>How you plan to learn from it and use this
+                        learning to shape future projects</li>
+                    <li>Is it something new, or are you continuing something that
+                        has worked well previously? We want to fund both types of projects</li>
+                </ul>`,
+
+                cy: `<p><strong>
+                    Dyma rhai syniadau o’r hyn i ddweud wrthym am eich prosiect:
+                </strong></p>
+                <ul>
+                    <li>Beth y hoffech ei wneud?</li>
+                    <li>Pa wahaniaeth bydd eich prosiect yn ei wneud?</li>
+                    <li>Pwy fydd yn cael budd ohono?</li>
+                    <li>Am ba mor hir rydych yn disgwyl ei gynnal?
+                        Gall hyn fod yn amcangyfrif.</li> 
+                    <li>Sut y byddwch yn sicrhau fod pobl yn gwybod amdano?</li>
+                    <li>Sut rydych yn bwriadu dysgu ohono a defnyddio’r wybodaeth 
+                    hwn i siapio prosiectau yn y dyfodol?</li>
+                    <li>A yw’n rhywbeth newydd, neu ydych yn parhau â rhywbeth
+                        sydd wedi gweithio’n dda yn flaenorol? Rydym eisiau
+                        ariannu’r ddau fath o brosiectau.</li>
+    
+                </ul>`,
+            });
+        }
+
         return new TextareaField({
             locale: locale,
             name: 'yourIdeaProject',
@@ -33,48 +70,10 @@ module.exports = {
                 en: `What would you like to do?`,
                 cy: `Beth yr hoffech ei wneud?`,
             }),
-            explanation: localise({
-                en: `<p><strong>
-                Here are some ideas of what to tell us about your project:
-            </strong></p>
-            <ul>
-                <li>What you would like to do</li>
-                <li>What difference your project will make</li>
-                <li>Who will benefit from it</li>
-                <li>How long you expect to run it for. This can be an estimate</li>
-                <li>How you'll make sure people know about it</li>
-                <li>How you plan to learn from it and use this
-                    learning to shape future projects</li>
-                <li>Is it something new, or are you continuing something that
-                    has worked well previously? We want to fund both types of projects</li>
-            </ul>
-            <p><strong>
-                You can write up to ${maxWords} words for this section,
-                but don't worry if you use less.
-            </strong></p>`,
-
-                cy: `<p><strong>
-                Dyma rhai syniadau o’r hyn i ddweud wrthym am eich prosiect:
-            </strong></p>
-            <ul>
-                <li>Beth y hoffech ei wneud?</li>
-                <li>Pa wahaniaeth bydd eich prosiect yn ei wneud?</li>
-                <li>Pwy fydd yn cael budd ohono?</li>
-                <li>Am ba mor hir rydych yn disgwyl ei gynnal?
-                    Gall hyn fod yn amcangyfrif.</li> 
-                <li>Sut y byddwch yn sicrhau fod pobl yn gwybod amdano?</li>
-                <li>Sut rydych yn bwriadu dysgu ohono a defnyddio’r wybodaeth 
-                hwn i siapio prosiectau yn y dyfodol?</li>
-                <li>A yw’n rhywbeth newydd, neu ydych yn parhau â rhywbeth
-                    sydd wedi gweithio’n dda yn flaenorol? Rydym eisiau
-                    ariannu’r ddau fath o brosiectau.</li>
-
-            </ul>
-            <p><strong>
-                Gallwch ysgrifennu hyd at ${maxWords} gair i’r adran hon,
-                ond mae modd ysgrifennu llai.
-            </strong></p>`,
-            }),
+            explanation: stripIndents`${guidanceText()}${wordCountText(
+                locale,
+                maxWords
+            )}`,
             type: 'textarea',
             minWords: minWords,
             maxWords: maxWords,
@@ -109,7 +108,7 @@ module.exports = {
                         </li>
                         <li>organisations providing services and support
                             for vulnerable people, for which there will be
-                            increased demand as a result of the COVID-19 crisis 
+                            increased demand as a result of the COVID-19 crisis
                         </li>
                         <li>organisations which connect communities and support
                             communities to work together to respond to COVID-19.
@@ -283,6 +282,41 @@ module.exports = {
         const minWords = 50;
         const maxWords = 200;
 
+        function guidanceText() {
+            return localise({
+                en: `<p>
+                    We believe that people understand what's needed in their
+                    communities better than anyone. Tell us how your community 
+                    came up with the idea for your project. We want to know how
+                    many people you've spoken to, and how they'll be involved
+                    in the development and delivery of the project.
+                </p>
+                <p><strong>Here are some examples of how you could be involving your community:</strong></p>
+                <ul>
+                    <li>Having regular chats with community members, in person or on social media</li>
+                    <li>Including community members on your board or committee</li>
+                    <li>Regular surveys</li>
+                    <li>Setting up steering groups</li>
+                    <li>Running open days</li>
+                </ul>`,
+                cy: `<p>
+                    Rydym o’r gred fod pobl yn gwybod yr hyn sydd ei angen yn eu
+                    cymunedau yn well nag unrhyw un. Dywedwch wrthym sut feddyliodd
+                    eich cymuned am y syniad i’ch prosiect. Rydym eisiau gwybod
+                    faint o bobl rydych wedi siarad â nhw, a sut y byddant yn cael
+                    eu cynnwys yn y datblygiad a’r ddarpariaeth o’r prosiect.
+                </p>
+                <p><strong>Dyma rhai enghreifftiau o sut gallwch fod yn cynnwys eich cymunedau:</strong></p>
+                <ul>
+                    <li>Cael sgyrsiau rheolaidd ag aelodau’r gymuned, naill ai mewn person neu gyfryngau cymdeithasol</li>
+                    <li>Cynnwys aelodau o'r gymuned ar eich bwrdd neu bwyllgor</li>
+                    <li>Arolygon rheolaidd</li>
+                    <li>Sefydlu grwpiau llywio</li>
+                    <li>Cynnal diwrnodau agored</li>
+                </ul>`,
+            });
+        }
+
         return new TextareaField({
             locale: locale,
             name: 'yourIdeaCommunity',
@@ -314,46 +348,10 @@ module.exports = {
                 </ol>`,
                 }),
             },
-            explanation: localise({
-                en: `<p>
-                We believe that people understand what's needed in their
-                communities better than anyone. Tell us how your community 
-                came up with the idea for your project. We want to know how
-                many people you've spoken to, and how they'll be involved
-                in the development and delivery of the project.
-            </p>
-            <p><strong>Here are some examples of how you could be involving your community:</strong></p>
-            <ul>
-                <li>Having regular chats with community members, in person or on social media</li>
-                <li>Including community members on your board or committee</li>
-                <li>Regular surveys</li>
-                <li>Setting up steering groups</li>
-                <li>Running open days</li>
-            </ul>
-            <p><strong>
-                You can write up to ${maxWords} words for this section,
-                but don't worry if you use less.
-            </strong></p>`,
-                cy: `<p>
-                Rydym o’r gred fod pobl yn gwybod yr hyn sydd ei angen yn eu
-                cymunedau yn well nag unrhyw un. Dywedwch wrthym sut feddyliodd
-                eich cymuned am y syniad i’ch prosiect. Rydym eisiau gwybod
-                faint o bobl rydych wedi siarad â nhw, a sut y byddant yn cael
-                eu cynnwys yn y datblygiad a’r ddarpariaeth o’r prosiect.
-            </p>
-            <p><strong>Dyma rhai enghreifftiau o sut gallwch fod yn cynnwys eich cymunedau:</strong></p>
-            <ul>
-                <li>Cael sgyrsiau rheolaidd ag aelodau’r gymuned, naill ai mewn person neu gyfryngau cymdeithasol</li>
-                <li>Cynnwys aelodau o'r gymuned ar eich bwrdd neu bwyllgor</li>
-                <li>Arolygon rheolaidd</li>
-                <li>Sefydlu grwpiau llywio</li>
-                <li>Cynnal diwrnodau agored</li>
-            </ul>
-            <p><strong>
-                Gallwch ysgrifennu hyd at ${maxWords} gair i’r adran hon,
-                ond peidiwch â poeni os byddwch yn defnyddio llai.
-            </strong></p>`,
-            }),
+            explanation: stripIndents`${guidanceText()}${wordCountText(
+                locale,
+                maxWords
+            )}`,
             minWords: minWords,
             maxWords: maxWords,
             messages: [
