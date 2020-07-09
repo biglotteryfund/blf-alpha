a8b6d01bf7e5b88c8d01b53587acfe027e862b17
Matt Andrews <matt.andrews@tnlcommunityfund.org.uk>
2020-07-09 15:49:18 +0100

    Update Checkbox field type to allow optgroups as well as options

--- controllers/apply/lib/field-types/checkbox.test.js
+++ controllers/apply/lib/field-types/checkbox.test.js
@@ -28,6 +28,60 @@ test('valid field', function () {
     );
 });
 
+test('valid field with optgroups', function () {
+    const field = new CheckboxField({
+        locale: 'en',
+        name: 'example',
+        label: 'Example field',
+        options: [
+            { label: 'Option 1', value: 'option-1' },
+            { label: 'Option 2', value: 'option-2' },
+            { label: 'Option 3', value: 'option-3' },
+        ],
+        optgroups: [
+            {
+                label: 'Midlands',
+                options: [
+                    {
+                        value: 'east-midlands',
+                        label: 'East Midlands',
+                    },
+                    {
+                        value: 'west-midlands',
+                        label: 'West Midlands',
+                    },
+                ],
+            },
+            {
+                label: 'London',
+                options: [
+                    {
+                        value: 'north-london',
+                        label: 'North London',
+                    },
+                    {
+                        value: 'south-london',
+                        label: 'South London',
+                    },
+                ],
+            },
+        ],
+        messages: [{ type: 'base', message: 'Select an option' }],
+    });
+
+    expect(field.type).toBe('checkbox');
+    expect(field.displayValue).toBe('');
+
+    field.withValue(['west-midlands', 'south-london']);
+    expect(field.displayValue).toBe('West Midlands,\nSouth London');
+    expect(field.validate().error).toBeNull();
+
+    field.withValue(['bad-option']);
+    expect(field.validate().error.message).toEqual(
+        expect.stringContaining('must be one of')
+    );
+});
+
 test('optional field', function () {
     const field = new CheckboxField({
         locale: 'en',
@@ -52,7 +106,7 @@ test('must provide options', function () {
             label: 'Example field',
             messages: [{ type: 'base', message: 'Select an option' }],
         });
-    }).toThrowError('Must provide options');
+    }).toThrowError('Must provide either options or optgroups');
 });
 
 test('options must contain unique values', function () {
