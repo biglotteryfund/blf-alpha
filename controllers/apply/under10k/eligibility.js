59c09a90afa5a5b5e1722d55c8e8a656d0bb6a4d
MDurvesh <mohammed.durvesh@tnlcommunityfund.org.uk>
2020-10-22 16:46:15 +0100

    Eligibility checker - q2 + explanation updated

--- controllers/apply/under10k/eligibility.js
+++ controllers/apply/under10k/eligibility.js
@@ -21,7 +21,7 @@ module.exports = function ({ locale }) {
     const orgMinAgeLabel = localise(ORG_MIN_AGE.label);
 
     function question1() {
-        function unconnectedExplanation() {
+        function getExplanation() {
             if (enableSimpleV2) {
                 return localise({
                     en: oneLine`By unconnected, we mean not a relation by blood, marriage,
@@ -44,7 +44,7 @@ module.exports = function ({ locale }) {
                 en: oneLine`Does your organisation have at least two unconnected people on the board or committee?`,
                 cy: oneLine`Oes gan eich sefydliad o leiaf dau berson heb gysylltiad i’w gilydd ar y bwrdd neu bwyllgor?`,
             }),
-            explanation: unconnectedExplanation(),
+            explanation: getExplanation(),
             yesLabel: localise({ en: 'Yes', cy: 'Oes' }),
             noLabel: localise({ en: 'No', cy: 'Nac oes' }),
             errorMessage: localise({
@@ -69,34 +69,68 @@ module.exports = function ({ locale }) {
     }
 
     function question2() {
-        const question = localise({
-            en: oneLine`Are you applying for an amount between
+        function getQuestion() {
+            if (enableSimpleV2) {
+                return localise({
+                    en: oneLine`Are you applying for an amount between
                 £${MIN_BUDGET_TOTAL_GBP.toLocaleString()} and
                 £${MAX_BUDGET_TOTAL_GBP.toLocaleString()}
-                that you’ll spend in around in around 12 months?
+                that you’ll spend in around 12 months?
                 (or in 6 months for projects in England)`,
-            cy: oneLine`A ydych chi'n gwneud cais am swm rhwng
+                    cy: oneLine`A ydych chi'n gwneud cais am swm rhwng
                 £${MIN_BUDGET_TOTAL_GBP.toLocaleString()} a
                 £${MAX_BUDGET_TOTAL_GBP.toLocaleString()} y
                 byddwch chi'n ei wario mewn tua 12 mis?
                 (neu mewn 6 mis ar gyfer prosiectau yn Lloegr) `,
-        });
+                });
+            } else {
+                return localise({
+                    en: oneLine`Are you applying for an amount between
+                £${MIN_BUDGET_TOTAL_GBP.toLocaleString()} and
+                £${MAX_BUDGET_TOTAL_GBP.toLocaleString()}
+                that you’ll spend in around in around 12 months?
+                (or in 6 months for projects in England)`,
+                    cy: oneLine`A ydych chi'n gwneud cais am swm rhwng
+                £${MIN_BUDGET_TOTAL_GBP.toLocaleString()} a
+                £${MAX_BUDGET_TOTAL_GBP.toLocaleString()} y
+                byddwch chi'n ei wario mewn tua 12 mis?
+                (neu mewn 6 mis ar gyfer prosiectau yn Lloegr) `,
+                });
+            }
+        }
 
-        const explanation = localise({
-            en: oneLine`We know it's not always possible to complete a
+        const question = getQuestion();
+
+        function getExplanation() {
+            if (enableSimpleV2) {
+                return localise({
+                    en: oneLine`We know it's not always possible to complete a
+                  project in 12 months for lots of reasons. So we can
+                  consider projects which are slightly longer than this.
+                  But groups in England need to spend the funding in 6 months.
+                  We will also consider applications for one-off events.`,
+                    cy: oneLine``,
+                });
+            } else {
+                return localise({
+                    en: oneLine`We know it's not always possible to complete a
                   project in 12 months for lots of reasons. So we can
                   consider projects which are slightly longer than this.
                   But groups in England need to spend the funding in 6 months.
                   We will also consider applications for one-off events
                   such as a festival, gala day or conference`,
-            cy: oneLine`Rydym yn gwybod nad yw bob amser yn bosibl
+                    cy: oneLine`Rydym yn gwybod nad yw bob amser yn bosibl
                   cwblhau prosiect mewn 12 mis am lawer o resymau.
                   Felly gallwn ystyried prosiectau sydd ychydig yn
                   hirach na hyn. Ond mae angen i grwpiau yn Lloegr
                   wario'r arian mewn 6 mis. Byddwn hefyd yn ystyried
                   ceisiadau am ddigwyddiadau unwaith yn unig fel gŵyl,
                   diwrnod gala neu gynhadledd.`,
-        });
+                });
+            }
+        }
+
+        const explanation = getExplanation();
 
         return {
             question: question,
