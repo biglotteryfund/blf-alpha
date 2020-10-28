ee6e7a1af7c9990d285bb6dac311760ea169ed24
MDurvesh <mohammed.durvesh@tnlcommunityfund.org.uk>
2020-10-28 13:45:46 +0000

    max amount changed to 999999999 - highest number before ten characters to match error messages

--- controllers/apply/lib/field-types/currency.js
+++ controllers/apply/lib/field-types/currency.js
@@ -16,7 +16,7 @@ class CurrencyField extends Field {
 
     defaultSchema() {
         const minAmount = this.minAmount || 0;
-        const maxAmount = this.maxAmount || 1000000000;
+        const maxAmount = this.maxAmount || 999999999;
         const baseSchema = Joi.friendlyNumber()
             .integer()
             .min(minAmount)
