2bf6e7846a4ba531be396c9f9595007f546220c6
MDurvesh <mohammed.durvesh@tnlcommunityfund.org.uk>
2020-11-06 16:55:32 +0000

    spec updated

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -8,7 +8,7 @@ const sample = require('lodash/sample');
 const sampleSize = require('lodash/sampleSize');
 const sum = require('lodash/sum');
 const times = require('lodash/times');
-const { enableStandardV2, enableSimpleV2 } = require('../../common/secrets');
+const { enableStandardV2, enableSimpleV2 } = true;
 
 function acceptCookieConsent() {
     return cy.get('.cookie-consent button').click();
