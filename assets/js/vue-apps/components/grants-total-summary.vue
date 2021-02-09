371d823b38242aff1dfb6fd0a99806e42b395b55
MDurvesh <mohammed.durvesh@tnlcommunityfund.org.uk>
2021-02-09 14:00:50 +0000

    aria-live region specified for grants total

--- assets/js/vue-apps/components/grants-total-summary.vue
+++ assets/js/vue-apps/components/grants-total-summary.vue
@@ -1,5 +1,5 @@
 <template functional>
-    <span role="alert">
+    <span role="alert" aria-live="polite">
         {{ props.totalResults.toLocaleString() }} {{ props.copy.resultsCount }}
         <template v-if="props.totalAwarded">
             {{ props.copy.totalCount }} £{{
