f176333d24a04b9c08e51e49e20587042e7d7c4b
Mark Smith <mark.smith@tnlcommunityfund.org.uk>
2021-02-19 11:49:02 +0000

    Removed console log.

--- assets/js/common/videos.js
+++ assets/js/common/videos.js
@@ -3,7 +3,6 @@ import $ from 'jquery';
 function changeIframeToLink() {
     // Find all iframes with an src of youtube, split out the link, take the video ID and create a link.
     $("iframe[src*='youtube']").each(function() {
-        console.log($(this));
         let link = 'http://www.youtube.com/watch?v=' + $(this).attr('src').split('/')[4];
         $(this).parent().append('<a href="' + link + '">' + link + '</a>').css('padding-top', '0%'); // Add the link to the parent and
         $(this).remove();
