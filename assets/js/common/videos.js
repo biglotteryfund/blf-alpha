4b8ef198645bf617c978a6fe0b5a67ac41b4f2e3
Mark Smith <mark.smith@tnlcommunityfund.org.uk>
2021-02-19 11:36:18 +0000

    Added JS to turn YouTube videos into links when a user has not accepted all cookies.

---
+++ assets/js/common/videos.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+
+function changeIframeToLink() {
+    // Find all iframes with an src of youtube, split out the link, take the video ID and create a link.
+    $("iframe[src*='youtube']").each(function() {
+        console.log($(this));
+        let link = 'http://www.youtube.com/watch?v=' + $(this).attr('src').split('/')[4];
+        $(this).parent().append('<a href="' + link + '">' + link + '</a>').css('padding-top', '0%'); // Add the link to the parent and
+        $(this).remove();
+    });
+}
+
+function init() {
+    // If the user has only selected to have essential cookies, turn all iframe YouTube videos to links.
+    if(localStorage.getItem('tnlcommunityfund:cookie-consent') != 'all') {
+        changeIframeToLink();
+    }
+}
+
+export default {
+    init,
+};
