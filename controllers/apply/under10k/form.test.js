19225aaff2e417be70411510cc81813cbe9e0a64
Shayan03 <shayan.mohammadi@tnlcommunityfund.org.uk>
2022-03-02 13:11:30 +0000

    Fix test

--- controllers/apply/under10k/form.test.js
+++ controllers/apply/under10k/form.test.js
@@ -507,8 +507,8 @@ test('maintain backwards compatibility for date schema', function () {
         projectCountry: 'scotland',
         projectLocation: 'fife',
         supportingCOVID19: 'no',
-        projectStartDate: { day: 25, month: 4, year: 2022 },
-        projectEndDate: { day: 3, month: 7, year: 2022 },
+        projectStartDate: { day: 25, month: 6, year: 2022 },
+        projectEndDate: { day: 3, month: 9, year: 2022 },
     });
 
     const form = formBuilder({
@@ -519,11 +519,11 @@ test('maintain backwards compatibility for date schema', function () {
 
     // Maintain backwards compatibility with salesforce schema
     const salesforceResult = form.forSalesforce();
-    expect(salesforceResult.projectStartDate).toBe('2022-04-25');
-    expect(salesforceResult.projectEndDate).toBe('2022-07-03');
+    expect(salesforceResult.projectStartDate).toBe('2022-06-25');
+    expect(salesforceResult.projectEndDate).toBe('2022-09-03');
     expect(salesforceResult.projectDateRange).toEqual({
-        startDate: '2022-04-25',
-        endDate: '2022-07-03',
+        startDate: '2022-06-25',
+        endDate: '2022-09-03',
     });
 });
 
