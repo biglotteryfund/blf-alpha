d32d912fe7f93c76c243c1b3d8823ae64afbac33
Mark Smith <Mark.Smith@tnlcommunityfund.org.uk>
2023-03-01 10:50:59 +0000

    Added ReciteMe

--- common/csp-directives.js
+++ common/csp-directives.js
@@ -26,6 +26,8 @@ module.exports = function cspDirectives() {
         '*.bootstrapcdn.com',
         '*.soundcloud.com',
         'emails-tnlcommunityfund.org.uk',
+        'api.reciteme.com',
+        'stats.reciteme.com',
         new URL(CONTENT_API_URL).host,
     ];
 
